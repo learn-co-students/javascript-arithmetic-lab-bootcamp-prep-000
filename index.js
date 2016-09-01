242e3fc0d4eb082a0387df92aa518117695ca438
Menachem Abraham <menachemabraham@gmail.com>
2016-09-01 22:22:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(n, m){
+  return n + m;
+}
+
+function subtract(n, m){
+  return n - m;
+}
+
+function multiply(n, m){
+  return n * m;
+}
+
+function divide(n, m){
+  return n / m;
+}
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(str){
+  return parseInt(str, 10);
+}
+
+function preserveDecimal(str){
+  return parseFloat(str);
+}
