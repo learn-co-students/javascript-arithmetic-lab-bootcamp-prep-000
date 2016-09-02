a68a1e8e3ef1d85595cb17e68ba53b9293cc6e26
Stan Aretinskiy <sr575sr@gmail.com>
2016-09-02 04:10:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-var a, b
 function add(a, b) { // function 1
   return a + b
 }
-var a, b
+
 function subtract(a, b) {  // function 2
   return a - b
 }
-var a, b
+
 function multiply(a, b) {  // function 3
   return a * b
 }
-var a, b
+
 function divide(a, b) {  // function 4
   return a / b
 }
-var a, b
+
 function inc(a) { // function 5
   return a + 1
 }
-var a, b
+
 function dec(a) {  // function 6
   return a - 1
 }
-var a, b
-function makeInt(a) {  // function 7
-  return parseInt(a)
-}
-var a, b
+
 function makeInt(a) {  // function 8
   return parseInt(a, 10)
 }
-var a, b
+
 function preserveDecimal(a) {  // function 9
   return parseFloat(a)
 }
