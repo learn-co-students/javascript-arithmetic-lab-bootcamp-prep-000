68191e9ab40fbc9166122cc30a273ffe632e456e
Brian Brazil <briancbrazil@gmail.com>
2016-09-05 21:02:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+  return (num1 + num2);
+}
+
+function subtract(a, b) {
+  return (a - b);
+}
+
+function multiply(num1, num2) {
+  return (num1 * num2);
+}
+
+function divide(num1, num2) {
+  return (num1 / num2);
+}
+
+function inc(n) {
+  return ++n;
+}
+
+function dec(n) {
+  return --n;
+}
+
+
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
