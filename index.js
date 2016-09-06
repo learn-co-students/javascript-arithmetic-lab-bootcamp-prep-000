09653dab01df1616f6b5b9298bca4b407659d649
Vnay Kumr <chvnay@gmail.com>
2016-09-06 11:38:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a , b)
+{
+return a + b
+}
+function subtract(a,b)
+{
+return a-b
+}
+function multiply(a,b)
+{
+return a*b
+}
+function divide(a,b)
+{
+return a/b
+}
+function inc(n)
+{
+return n+1
+}
+function dec(n)
+{
+  return n-1
+}
+function makeInt(n)
+{
+switch(n)
+{
+  case n:
+  case n < 100:
+  return parseInt(n, 10)
+  default:
+  return NaN
+}
+}
+function preserveDecimal(n)
+{
+  switch(n)
+  {
+    case n:
+    return parseFloat(n)
+    default:
+    return NaN
+}
+}
