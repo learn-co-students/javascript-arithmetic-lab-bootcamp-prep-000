aa5fd0dcdc6e4b0d9102251ac748f33ca882a6f0
Carolyn Negri <caro.a.negri@gmail.com>
2016-09-10 18:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -22,16 +22,14 @@ function dec(n) {
   return(`${n -= 1}`)
 }
 
-function makeInt(n) {
-  parseInt('n')
-  return(n)
-}
-
-function makeInt(n) {
-  parseInt(n, 10)
-}
-
 function preserveDecimal(n) {
-  parseFloat('n')
-  return(n)
+  return parseFloat(n)
+}
+
+function makeInt(n) {
+  return parseInt('n', 10)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
 }
