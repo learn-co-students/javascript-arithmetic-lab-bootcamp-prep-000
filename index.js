15ce6eaea105a81082561ed3cbc80337ea7495c5
Ethan Ryan <ethanryan@gmail.com>
2016-09-10 00:36:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+
+add();
+subtract();
+multiply();
+divide();
+inc();
+dec();
+makeInt();
+preserveDecimal();
+
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function inc (n) {
+   return n += 1;
+}
+
+function dec (n) {
+   return n -= 1;
+ }
+
+ function makeInt (n) {
+   return parseInt(n, 10)
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n)
+}
