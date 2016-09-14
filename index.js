8f38422c006e45b9bd3d1f00c1a70a3a49466054
Clifton Jadoo <cliffjadoo@gmail.com>
2016-09-14 02:39:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b){
+  return a + b;
+}
+
+add(5,4);
+
+function subtract(a, b){
+  return a - b;
+}
+
+subtract(4,3);
+
+function multiply(a,b ){
+  return a*b;
+}
+
+multiply(3,3);
+
+function divide(a, b) {
+  return a/b;
+}
+
+divide(3,3);
+
+function inc(n){
+  return n += 1;
+}
+
+inc(4);
+
+function dec(n) {
+  return n-= 1 ;
+}
+
+dec(3);
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+makeInt(4);
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10);
+}
+
+preserveDecimal(1);
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
