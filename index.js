57f458c552e23d27cf1a6d3fba6671fbae1c9dc9
Ian  Bumatay <ianbumatay.09@gmail.com>
2016-09-15 02:57:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var a = 2
+var b = 3
+
+function add(a,b){
+  return a + b
+}
+
+var a = 10
+var b = 5
+
+function subtract(a,b){
+  return a - b
+}
+
+var a = 8
+var b = 20
+
+function multiply(a,b){
+  return a * b
+}
+
+var a = 5
+var b =10
+
+function divide(a,b){
+  return a / b
+}
+
+
+
+funtion inc(n){
+  return 5 ++
+}
