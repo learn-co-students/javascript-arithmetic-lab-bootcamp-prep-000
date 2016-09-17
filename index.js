4a7d8d3909dc9be03874f30d77e09e30ee627a88
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2016-09-17 21:15:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+ function multiply(a, b){
+   return a * b
+ }
+
+ function divide(a, b){
+   return a / b
+ }
+
+function inc(n){
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n){
+  parseInt('2', 10)
+  return n
+}
+
+ function preserveDecimal(n){
+   parseFloat(2.22)
+   return n
+ }
