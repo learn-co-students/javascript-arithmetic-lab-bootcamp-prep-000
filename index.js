f4c236b67c55e5569fef164f271a5a7a5e1407ad
Kevin Crystal <kevinacrystal@gmail.com>
2016-09-25 02:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,15 @@ function divide(a, b){
   return(a/b)
 }
 function inc(n){
-  n++
-  return(n)
+  return(n+=1)
 }
 function dec(n) {
-  n--
-  return(n)
-
+  return(n-=1)
 }
 function makeInt(n) {
   return(parseInt(n,10))
 }
 function preserveDecimal(n) {
-  parseFloat(n)
-  return(n)
+  return(parseFloat(n))
+
 }
