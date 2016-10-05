d2406e5ec96c59b3b89b0f2b7b343ae67dedfd6c
Herbert Tejada <herberthtejada@gmail.com>
2016-10-05 18:07:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var a = 15;
+var b = 10;
+var n = 100;
+function add(a,b){
+  return a + b;
+}
+add();
+
+function subtract(a,b){
+  return a - b;
+}
+subtract();
+
+function multiply(a,b){
+  return a * b;
+}
+multiply();
+
+function divide(a,b){
+  return a / b;
+}
+divide();
+
+function inc(n){
+  return ++n ;
+}
+inc();
+
+function dec(n){
+  return --n ;
+}
+dec();
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+makeInt();
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+preserveDecimal();
