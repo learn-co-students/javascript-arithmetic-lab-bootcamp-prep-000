cd73147f176b2249ffa742f1172ce6c5dae2fdf6
Azekah Griffiths <azekah.griffiths@icloud.com>
2016-10-10 23:36:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b) {
+  var sum = a + b;
+  return sum
+}
+
+function subtract(a,b) {
+  var minus = a - b;
+  return minus
+}
+
+function multiply(a,b) {
+  var mult = a*b;
+  return mult
+}
+
+function divide(a,b) {
+  var div = a/b;
+  return div
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
