7d134bf1604b70314c53ec1c2d03dd28adf061a4
Miranda Clay <mjc29827@uga.edu>
2016-10-13 23:49:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var a = 2
+var b = 7
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a/b
+}
+var n = 1
+function inc(n) {
+  n++;
+  return n
+}
+function dec(n) {
+  --n
+  return n 
+}
