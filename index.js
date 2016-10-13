0016e78df81c3724b2abf700554df286dcec69f5
IFEANYICHUKWU UDOCHU <ifeanyi.udochu@gmail.com>
2016-10-13 17:18:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
