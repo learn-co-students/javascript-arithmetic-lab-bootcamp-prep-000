1bf4ecc4f0a878e44a1210a55fa4e4d4ae1f3bc4
Zhanxi Ni <ni.zhanxi@gmail.com>
2016-10-14 18:22:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+  return a+b;
+}
+
+function substract (a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide (a,b) {
+  return a/b;
+}
+
+function inc(n) {
+  return n+=1;
+}
+
+function dec(n) {
+  return n-=1;
+}
+
+function makeInt(n) {
+  return parseFloat(n,10);
+}
