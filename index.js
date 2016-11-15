8667339891031254e7965406e05a71cc5945edbe
Young Chul Lim <youngchullim@yahoo.com>
2016-11-15 17:11:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+  return a + b;
+}
+
+
+function subtract(a,b) {
+  return a-b;
+}
+
+
+function multiply(a,b) {
+  return a * b;
+}
+
+
+function divide(a,b) {
+  return a / b;
+}
+
+
+function inc(a) {
+  return  a + 1;
+}
+
+
+function dec(a) {
+  return a - 1;
+}
+
+function makeInt(parseInt(a,10)) {
+    if(a===a+(0<a && 1>a)) {
+        return a;
+    }
+    else if(a === (0*2328)) {
+        return 0;
+    }
+    else {
+        return NaN;
+    }
+}
