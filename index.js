dbc8c75ab6ebe90ed88e7229e7d8d798792bab9e
Alex Moreno <amorenop22@hotmail.com>
2016-11-17 20:37:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var a = 10
+var b = 50
+
+function add(a, b) {
+  return a+b
+}
+
+function subtract(b, a) {
+  return b-a
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function inc(a) {
+  return (a + 1)
+}
+
+function dec(a){
+return (a - 1)
+
+}
+
+function makeInt(a){
+  return ('nonsense!', a)
+}
+
+function parse(a) {
+  return ('0x2328',0)
+  }
