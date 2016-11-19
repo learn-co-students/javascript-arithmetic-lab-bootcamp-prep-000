2af9bf93baee72df90ccf0b264a7c9cce8d70beb
Allan (Wei Der) Peng <allanwdpeng@gmail.com>
2016-11-19 08:09:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a + b
+} //add two numbers and returns the result
+
+function subtract(a,b){
+  return a - b
+}//subtract two numbers and returns the result
+
+function multiply(a,b){
+  return a * b
+}//multiply two numbers and returns the result
+
+function divide(a,b){
+  return a / b
+}//divide two numbers and returns the result
+
+function inc(n){
+  return ++n
+}//increments n and return the result
+
+function dec(n){
+  return --n
+}//decrements n and return the result
+
+function makeInt(n){
+  return parseInt(n, 10)
+}//parses n as an integer and returns the parsed integer
+
+function preserveDecimal(n){
+  return parseFloat(n, 10)
+}//parses n as a floating point number and returns the parsed number
