9bdb59e557d53520013bd1799bc36e09ff054b8b
Raquel Velasquez <goddessra@icloud.com>
2016-11-26 08:00:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n - 1
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+  }
+
+function makeInt(assumesbase10) {
+  return parseInt(assumesbase10, 10)
+}
+
+function preserveDecimal(n) {
+  parseFloat('n.123')
+  return (n)
+}
