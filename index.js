df77dadcc980b8b3fd53da37e87703411a34be53
Jessica Quiroga <jqnotjq@gmail.com>
2016-11-29 18:53:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  1 + 2
+  return 
+}
+
+function subtract(a, b) {
+  2 - 1
+}
+
+/*
+function multiply()
+
+function divide()
+
+function inc(n)
+
+function dec(n)
+
+function makeInt(n)
+
+function perserveDecimal(n)
+*/
