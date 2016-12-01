c586933c58db70d2c2a7a13f2b567d01d2e77653
Aidan Bane <aidanfbane@gmail.com>
2016-12-01 06:56:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+  return ++a;
+}
+
+function dec(x) {
+  return --x;
+}
+
+function makeInt(n) {
+  return parseInt(n, '10');
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
