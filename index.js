7c015bd01ef223412c17843f86929c16212e5cd3
khatera dehzad <khater.d@gmail.com>
2016-12-01 01:31:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+  return a+b
+}
+
+function subtract(a,b){
+  var result = a - b
+  return result
+}
+
+function multiply(a,b){
+  var result = a*b
+  return result
+}
+
+function divide(a,b){
+  var result = a / b
+  return result
+}
+
+function inc(n){
+  var result = ++n
+  return result
+}
+
+function dec(n){
+  var result = -- n
+  return result
+}
+
+function makeInt(n){
+var integer = parseInt (n , 10)
+return integer
+}
+
+function preserveDecimal(n){
+  var decimal= parseFloat (n)
+  return decimal
+}
