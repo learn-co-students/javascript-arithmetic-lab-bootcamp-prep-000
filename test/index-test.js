3b55b84667d51188207ddca2de5a25fbc795b290
gabor santa <gabor.santa@gmail.com>
2016-12-03 18:18:12 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,50 +5,35 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
+function add(a, b) {
+  return a + b
+}
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
+function subtract(a, b) {
+  return a - b
+}
 
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
+function multiply(a, b) {
+  return a * b
+}
 
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
+function divide(a, b) {
+  return a / b
+}
 
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
+function inc(n) {
+  return n += 1
+}
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
+function dec(n) {
+  return n -= 1
+}
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
+function makeInt(n) {
+  return parseInt(n, 10)
+}
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toEqual(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
