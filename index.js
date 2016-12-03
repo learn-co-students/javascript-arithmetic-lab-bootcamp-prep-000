92eee7a5bd6a9b34e2431b20c4a74f752ef2cd45
Scott Young <young.scottw@gmail.com>
2016-12-03 20:49:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+//Adds a and b, returns sum
+function add(a, b){
+  return a+b
+}
+
+//Subtracts b from a, returns result
+function subtract(a,b){
+  return a-b
+}
+
+//Multiplies a and b, returns result
+function multiply(a,b){
+  return a*b
+}
+
+//Divides a by b, returns result
+function divide(a,b){
+  return a/b
+}
+
+//Increments n and returns result
+function inc(n){
+  return n+1
+}
+
+//Decrements n and returns result
+function dec(n){
+  return n-1
+}
+
+//Parses n as an integer and returns the integer
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+//Preserves n's decimals and returns a parsed number
+function preserveDecimal(n){
+  return parseFloat(n)
+}
