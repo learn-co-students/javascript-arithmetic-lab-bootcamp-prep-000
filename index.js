19a44cd0f3a940a5c1d438d5693458f18f8b12c7
Shelly Xu <xushell@gmail.com>
2016-12-08 02:20:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return a+b;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply (a,b){
+  return a*b;
+}
+
+function divide (a,b){
+  var divide= a/b;
+  return divide;
+}
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
