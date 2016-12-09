d3df368c92e5506e225e6b86888c361603d90705
Caroline Kasterine <ckasterine@gmail.com>
2016-12-09 05:20:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 
-function add() {
-  return 1 + 80
+function add(a, b) {
+  return a + b
 }
 
-function subract() {
-  return 60 - 40
+function subtract(a, b) {
+  return a - b
 }
 
-function multiply() {
-  return 2 * 3.4
+function multiply(a, b) {
+  return a * b
 }
 
-function divide() {
-  return 5.0 / 2.5
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+ return n += 1
+}
+
+function dec(n) {
+ return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
