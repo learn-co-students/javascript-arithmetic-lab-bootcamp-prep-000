398b8ddbab53c98c8870a8a273f14cfffff5b582
Yaakov Beiss <yaakovbeiss@gmail.com>
2016-12-09 16:04:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(number1,number2) {
+  return number1 + number2
+}
+function subtract(number1, number2) {
+  return number1 - number2
+}
+function divide(number1, number2) {
+  return number1 / number2
+}
+function multiply(number1, number2) {
+  return number1 * number2
+}
+function inc(n) {
+  return n += 1
+}
+function dec(n) {
+  return n -= 1
+}
+function makeInt(number) {
+  return parseInt(number, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
