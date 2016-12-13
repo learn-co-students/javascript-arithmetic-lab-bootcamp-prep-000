b6f1083a98cb4855719a3594341a11640a1cc641
Andre M <andre.mueller@zoho.com>
2016-12-13 23:37:15 +0000

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,14 @@ function divide(a, b) {
   return a/b
 }
 function inc(n) {
-  n++
-  return n
+  return n += 1
 }
 function dec(n) {
-  n--
-  return n
+  return n -= 1
 }
 function makeInt(n) {
-  parseInt(n, 10)
-  return n
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
-  parseFloat(n)
-  return n
+  return parseFloat(n)
 }
