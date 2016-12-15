067d9517b31656f1a4d220e07a2182016c38a35f
Leibel Hecht <leibelhecht@gmail.com>
2016-12-15 07:58:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function add(a, b){
+function add(a,b){
   return a + b
 }
-function subtract(a, b){
+function subtract(a,b){
   return a - b
 }
-function multiply(a, b){
+function multiply(a,b){
   return a * b
 }
-function divide(a, b){
-  return a / b
+function divide(a,b){
+  return a/b
 }
 function inc(n){
-  return n += 1
+  return n+1
 }
 function dec(n){
-  return n -= 1
+  return n-1
 }
 function makeInt(n){
   return parseInt(n, 10)
 }
 function preserveDecimal(n){
-  return parseFloat(n)
+  return parseFloat(n) 
 }
