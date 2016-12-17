e7ae2c33a4c179ad6bded1d0a4000295c2babfed
Julio Guzman <julio_guzman@ymail.com>
2016-12-17 19:21:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+// Adds two integers.
+function add(x,y) {
+  return x + y
+}
+
+// subtracts two integers.
+function subtract(x,y) {
+  return x - y
+}
+
+// Multiplies two integers
+function multiply(x,y) {
+  return x * y
+}
+
+// Divides two integers.
+function divide(x,y) {
+  return x / y
+}
+
+// Increments
+function inc(n) {
+  return ++n
+}
+
+// decrements.
+function dec(n) {
+  return --n
+}
+
+// Parses (n) as an integer and returns the parsed integer.
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+// Perseves (n) as a decimal and returns the parsed number.
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
