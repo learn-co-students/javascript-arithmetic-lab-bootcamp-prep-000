cec444ee3ac0e9a321d4ff036ac8664b9e3d1143
zeina sadaldeen <zeina.sadal.deen@gmail.com>
2016-12-17 20:01:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+//add(a, b) adds two numbers and returns the result
+function add(a,b){
+  return a+b;
+}
+//subtract(a, b) subtracts b from a and returns the result
+function subtract(a,b){
+  return a-b;
+}
+//multiply(a, b) multiplies two numbers and returns the result
+function multiply(a,b){
+  return a*b;
+}
+
+//divide(a, b) divides a by b and returns the result
+
+function divide(a,b){
+  return a/b;
+}
+
+//inc(n) increments n and returns the result
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+//makeInt(n) parses n as an integer and returns the parsed integer
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+//makeInt(n) assumes base 10
+
+//makeInt(n) returns NaN as appropriate
+
+
+
+//preserveDecimal(n) preserves n's decimals (it parses n as a floating point number) and returns the parsed number
+
+function preserveDecimal(n){
+  return parseFloat(n,10.0);
+}
+
+//preserveDecimal(n) returns NaN as appropriate
