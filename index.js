da6aedd0a55747638559ebe03df57e25ca172b9a
Lily Ling <lilyyling@gmail.com>
2016-12-18 06:04:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+add(1,2)
+subtract(6,3)
+multiple(2,3)
+divide(6,2)
+
+var = 10
+inc(number)
+dec(number)
+function makeInt()
+  parseInt('2',10)
+
+function preserveDecimal()
+  parseFloat('2.222',10)
+  
