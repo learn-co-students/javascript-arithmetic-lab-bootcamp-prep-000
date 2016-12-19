d8f6b30b28a67cbd1e8e1e9d77a580259c8d1026
Anthony Hutchinson <nampee1@aol.com>
2016-12-19 23:50:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b){
+  return a + b
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+function inc(a){
+  return a + 1
+}
+function dec(a){
+  return a - 1
+}
+function makeInt(a, isNaN){
+  return parseInt(a, 10)
+  return parseInt(isNaN, 10)
+}
+function preserveDecimal(isNaN){
+  //return parseFloat('2.222')
+  return parseFloat(isNaN)
+}
