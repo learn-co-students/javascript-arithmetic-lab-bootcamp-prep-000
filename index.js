b68791dc33d8aadf8d940dbc021a34b87598d84c
Duilio Bini <duiliobini@gmail.com>
2016-12-25 21:19:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function add(a,b) {return a+b}
+function subtract(a,b) {return a-b}
+function multiply(a,b) {return a*b}
+function divide(a,b) {return a/b}
+function inc(n) {return ++n}
+function dec(n) {return --n}
+function makeInt(n){
+  parseInt(n,"10")
+  return n
+}
+function preserveDecimal(n) {paresefloat("n") return n}
