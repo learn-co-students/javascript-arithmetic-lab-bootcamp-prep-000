a916adf2e7154b63c22bd81d126a557c19fcfcb3
Sean Savageau <seansavagio@gmail.com>
2016-12-27 03:20:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  var number = n
+  number++
+  return number
+}
+
+function dec(n) {
+  var number = n
+  number--
+  return number
+}
+
+function makeInt(n) {
+  parseInt('n', 10)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat('n', 10)
+  return n
+}
