bd31a0ff0114cd5fd6703ff09e1b1824b07132fe
Tranee Peevy <peevy.t@gmail.com>
2017-01-01 01:57:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-var number = 10
-
-function add5 ()
+function add(a, b)
 {
-  number += 5
-} 
+  return a + b
+}
+
+function subtract(a, b)
+{
+  return a - b
+}
+
+function multiply(a, b)
+{
+  return a * b
+}
+
+function divide(a, b)
+{
+  return a / b
+}
+
+function inc(n)
+{
+  return n += 1
+}
+
+function dec(n)
+{
+  return n -= 1
+}
+
+function makeInt(n)
+{
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
+}
