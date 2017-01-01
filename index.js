247e115f0def0a2445debf97165a0f9bf0c2933a
John Graniero <jgrani6167@aol.com>
2017-01-01 02:59:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b){
+    return a + b;
+}
+add(12,9);
+
+function subtract(a,b){
+    return a - b;
+}
+subtract(12,9);
+
+function multiply(a,b){
+    return a * b;
+}
+multiply(12,9);
+
+function divide(a,b){
+    return a / b;
+}
+divide(12,9);
+
+function inc(a){
+return ++a;
+}
+inc(7);
+
+function dec(a){
+return --a;
+}
+dec(7);
+
+function makeInt(n,b){
+ return parseInt(n,b)
+  }
+  makeInt(344,10);
+
+  function makeInt(a,b){
+ return parseFloat(a,b)
+  }
+  makeInt(0,10);
+
+  function preserveDecimal(a){
+    return parseFloat(a);
+  }
+     preserveDecimal(2.222);
+
+
+  function preserveDecimal(a){
+    return parseFloat(a)
+  }
+     preserveDecimal(2.222);
