483e4ebb340b140b295c15a806fcd3e585a6bc78
Esdras  Etrenne <esdras.pierre91@gmail.com>
2017-01-03 08:59:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+  return a + b //next 4 functions are simple arithmetic
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  return ++n //increase to get one more than original number
+}
+
+function dec(n){
+  return --n //decend to get one less than the original number
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n,10)
+}
