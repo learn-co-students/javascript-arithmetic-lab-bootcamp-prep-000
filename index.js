239748fd88fc6c75fd672452fdef82d0c03f0626
David Kennedy <dkennedychicago@gmail.com>
2017-01-10 18:43:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+// 1.
+function add(a, b) {
+  return a + b
+}
+// 2.
+function subtract(a, b) {
+  return a - b
+}
+// 3.
+function multiply(a, b) {
+  return a * b
+}
+// 4.
+function divide(a, b) {
+  return a / b
+}
+// 5.
+function inc(n) {
+  return ++n
+}
+// 6.
+function dec(n) {
+  return --n
+}
+// 7.
+function makeInt(n) {
+  return parseInt(n)
+}
+// 8.
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+// 9.
+function preserveDecimal(n) {
+  return parseInt(n)
+}
+// 10.
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+// 11.
