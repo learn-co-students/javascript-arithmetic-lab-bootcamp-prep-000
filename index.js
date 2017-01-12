7a543cce784c8d00a5aa928887492f3e3b35a149
Nabil Yafai <nabilyafai90@gmail.com>
2017-01-12 04:03:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+return a+b
+}
+function subtract(a,b){
+return a-b
+}
+function multiply(a,b){
+return a*b
+}
+function divide(a, b) {
+return a/b
+}
+function inc(n){
+var increment = n +=1
+return increment
+}
+function dec(n){
+var decrement = n-=1
+return decrement
+}
+function makeInt(n){
+var theInterger = parseInt(n,10)
+return theInterger
+}
+
+function preserveDecimal(n){
+var theFloater = parseFloat(n)
+return theFloater
+
+}
