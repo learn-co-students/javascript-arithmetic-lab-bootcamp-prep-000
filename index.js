05e352b0ab4b0f26944c5ecfb3faf586bd70e0b7
Danielle Croxton <danikrist@gmail.com>
2017-01-14 05:16:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+var n
+  return ++n
+}
+
+function dec(n) {
+var n
+  return --n
+}
+
+function makeInt(n) {
+  var n
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  var n
+  return parseFloat(n)
+}
