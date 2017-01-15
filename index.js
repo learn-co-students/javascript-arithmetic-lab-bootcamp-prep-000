33d4e6276e00a42e3c524c9a3365940ae0f1d133
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-01-15 21:29:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b
+}
+add (1, 4)
+
+function subtract(a, b) {
+  return a - b
+}
+subtract(10, 5)
+
+function multiply(a, b) {
+  return a * b
+}
+multiply(5,5)
+
+function divide(a, b) {
+  return a / b
+}
+divide(20, 4)
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
