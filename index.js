6ce5fa1f4c6df94b7123b0ec70aade6d388521ff
Charles Di Renzo <charlesdirenzo@gmail.com>
2017-01-16 20:08:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(){
+
+
+}
+
+function subtract(){
+
+
+}
+
+function multiply(){
+
+
+}
+
+function divide(){
+
+
+}
+
+function inc(n){
+
+
+}
+
+function dec(n){
+
+
+}
+
+function makeInt('2', 10){
+
+
+}
+
+function preserveFloat(){
+
+
+}
