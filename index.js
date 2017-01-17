9840b33bbc486862c22da7dbbd819e293d70ceb3
Santita Seesod <ant.csod@gmail.com>
2017-01-17 23:02:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var a = number * 1000
+var b = number * 1000
+var n = number
+
+function add(a,b) {
+  return a + b
+} 
+
+function subtract(a,b) {
+  return a - b 
+}
+
+function multiply(a,b) {
+  return a * b 
+}
+
+function divide(a,b) {
+  return a / b 
+}
+
+function inc(n) {
+  return a +=
+}
+
+function dec(n) {
+  return a -=
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
