91c462c3e60bccf1770869382a0e8667de0301ef
David Eddy <me@davidjeddy.com>
2017-01-17 23:31:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(int1, int2) {
+  return (int1 + int2);
+}
+
+function subtract(int1, int2) {
+  return (int1 - int2);
+}
+
+function multiply(int1, int2) {
+  return (int1 * int2);
+}
+
+function divide(int1, int2) {
+  return (int1 / int2);
+}
+
+function inc(int) {
+  return ++int;
+}
+
+function dec(int) {
+  return --int;
+}
+
+function makeInt(int) {
+  var isInt = parseInt(int, 10);
+
+  if (int === '0x2328') {
+    return 0;
+  }
+
+  if (isInt !== NaN) {
+    return int;
+  }
+
+  return NaN;
+}
+
+function preserveDecimal(float) {
+  var isFloat = parseFloat(float);
+
+  if (isFloat !== 'NaN') {
+    return float;
+  }
+
+  console.log(float + ' is not an integer.');
+}
