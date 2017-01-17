b1c6b38493764e69d3334803c3df0e07e5760844
David Calle <davidcalle7229@outlook.com>
2017-01-17 19:05:42 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function dec(a){
 function makeInt(a){
   return parseInt(a, 10);
 }
-function preserveDecimal(a){
-  return parseFloat(a);
+function preserveDecimal(b){
+  return parseFloat(b);
 }
