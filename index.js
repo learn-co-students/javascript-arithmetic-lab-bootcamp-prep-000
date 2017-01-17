a3d76c4e48ade16840916d6260839fce6f19e504
Victor Kolatau <pitbulldogy@gmail.com>
2017-01-17 20:34:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract (a,b) {
+  return a - b;
+}
+
+function multiply (a,b) {
+  return a * b;
+}
+
+function divide (a,b) {
+  return a / b;
+}
+
+function inc(n){
+var number = n;
+n++;
+return n;
+}
+
+function dec(n){
+var number = n;
+n--;
+return n;
+}
+
+function makeInt(n){
+  parseInt(n, n);
+  return n;
+}
+
+function makeInt(n){
+  parseInt(n, '10');
+  return n;
+}
+
+function preserveDecimal(n) {
+  parseFloat('n');
+  return n;
+}
