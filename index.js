27b48e12798edffdb94a3dea5639b5b7edcee318
Bashir Awaty <bashir.awaty@hotmail.com>
2017-01-20 19:30:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  parseInt('476', 10);
+
+}
+function makeInt(n) {
+  parseInt('2', 10);
+  return 2;
+
+}
+
+function makeInt(n) {
+  parseInt('0*3333');
+  return 0;
+
+}
+
+
+function preserveDecimal(n){
+  parseFolat('2.222');
+  }
