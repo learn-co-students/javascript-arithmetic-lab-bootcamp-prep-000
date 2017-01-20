6686376f6475704434745286859da7bec2e007ba
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-01-20 22:22:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var a,b;
-function add(a,b)
-function subtract(a,b)
+var a, b
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+var n
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+ }
+ function preserveDecimal(n) {
+   return parseFloat(n)
+ }
