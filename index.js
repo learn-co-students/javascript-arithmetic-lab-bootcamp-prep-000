adc0b95ff15e77aa0d4eb70c877533755f8963e2
Charisa Lackrone <charisaanne@gmail.com>
2017-01-20 02:42:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  var x = (a + b)
+    return x
+}
+
+function subtract(a, b) {
+  var x = (a - b)
+    return x
+}
+
+function multiply(a, b) {
+  var x = (a * b)
+    return x
+}
+
+function divide(a, b) {
+  var x = (a / b)
+    return x
+}
+
+function inc(a) {
+  var n = a
+    return n += 1
+}
+
+function dec(a) {
+    return a -= 1
+}
+
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
