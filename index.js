6756bd3cf5af6f17342311677353970b5126bac1
Chris Beyer <beyercorpuz@gmail.com>
2017-01-23 19:18:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-var number = 10
- 
-function add5(a,b) {
-  number += 5
+function add(a,b) {
+  return a+b
 }
- 
-function divideBy3() {
-  number /= 3
+function subtract (a,b){
+return a-b
 }
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
+
+function multiply(a,b){
+  return a*b
+}
+function inc(n){
+return ++n
+}
+function divide(a,b){
+return a/b
+
+}
+function dec(n){
+  return --n
+}
+function makeInt(n){
+  return n
+}
+function makeInt(dog){
+return parseInt(dog, 10);
+}
+
+function preserveDecimal(n){
+
+return n
+
+
+}
+
+
+
+
+
+// NOTE:
+// In JS we can *define* a function and we can *call* a function
+
+// divideBy3()
+//
+// console.log(number) // 3.333333333335
+//
+// add5()
+//
+// console.log(number) // 8.333333333335
+//
+// // reset number
+// number = 10
+//
+// add5()
+//
+// console.log(number) // 15
+//
+// divideBy3()
+//
+// console.log(number) // 5
