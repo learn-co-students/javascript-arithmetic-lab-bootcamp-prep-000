eefa88bdafb8d53591a7bff3e47bead63456001f
Murali Rai <murali.rai@gmail.com>
2017-01-25 23:21:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function makeInt(n){
+     return parseInt(n,10)
+  }
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}  
+
+function add(a,b)
+{
+  return a + b   
+}
+
+function subtract(a,b)
+{
+  return a - b
+}
+
+function multiply(a,b){
+  return a*=b
+}
+
+function divide(a,b)
+{
+  return a/b
+}
+
+function inc(a){
+  return ++a
+}
+
+function dec(a){
+  return --a
+}
