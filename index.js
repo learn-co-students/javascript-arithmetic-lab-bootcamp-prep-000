48d5a2074b79014e3ad48e5e77e8676170b79394
Rinda Ko <rinda.ko@gmail.com>
2017-01-26 04:04:41 +0000

    Done.

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function dec(number){
   return number-=1;
 }
 
-function makeInt(n){
-  return parseInt(n, 10);
+function makeInt(arg){
+  return parseInt(arg, 10);
 }
 
-function preserveDecimal(n){
-  return parseFloat(n);
+function preserveDecimal(arg){
+  return parseFloat(arg);
 }
