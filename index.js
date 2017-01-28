9b83559c7d5bdf48fd524bba71bded00313d4356
Nicole Therien <nicoletherien@yahoo.com>
2017-01-28 22:29:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a,b) {return a+b} //adds a+b
+
+function subtract(b,a) {return b-a} //subtract b-a
+
+function multiply(a,b) {return a*b} //multiply a*b
+
+function divide(a,b) {return a/b} //divides a/b
+
+function inc(n) {return n+1}
+
+function dec(n) {return n-1}
+
+function makeInt(n) {return parseInt(n, 10)}
+
+function preserveDecimal(n) {return parseFloat(n)}
