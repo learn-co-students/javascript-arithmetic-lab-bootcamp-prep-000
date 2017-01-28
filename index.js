cc017a1c32848d6da6df4d9e1d8c9f8244f35c4e
NInad Nandgadkar <nnandga1@binghamton.edu>
2017-01-28 21:00:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add (a,b)
+{
+  var c = a + b
+   return c 
+}
+
+function subtract(a,b)
+{
+  var c = a - b
+  return c
+}
+
+function multiply(a,b)
+{
+  var c = a * b
+  return c  
+}
+
+function divide(a,b)
+{
+  var c = a / b
+  return c  
+}
+
+function inc(a)
+{
+  return  (++a)
+}
+
+function dec(a)
+{
+  return --a
+}
+
+function makeInt(a)
+{
+  return parseInt(a,10)
+  
+}
+
+function preserveDecimal(a)
+{
+  return parseFloat(a)
+}
