ab9669462998feb45a890e05a8fa0f6250e486b0
Charlay Davenport <charlayd1993@gmail.com>
2017-01-29 20:13:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function add(a,b) {
+function add(a,b)
+debugger; {
   return a + b
 }
 
@@ -23,9 +24,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt('0x2328',10)
+  return parseInt(n,10) 
 }
 
 function preserveDecimal(n) {
-  return parseFloat('2.222')
+  return parseFloat(n)
 }
