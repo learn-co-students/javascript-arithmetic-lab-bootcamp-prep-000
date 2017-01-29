03511c1242ad73e4d7fda16ac407f3013aa9da78
Charlay Davenport <charlayd1993@gmail.com>
2017-01-29 19:40:35 +0000

    Done.

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ function subtract(a,b) {
   return a - b
 }
 
-function multipy(a,b) {
+function multiply(a,b) {
   return a*b
 }
 
 function divide(a,b) {
   return a/b
+}
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n - 1
 }
