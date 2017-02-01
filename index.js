d951d000b6ffa3dacd6f1fbacabd305331b8036f
daniel byun <byunsta@gmail.com>
2017-02-01 02:15:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,81 @@
+function add(a, b) {
+
+var a;
+var b;
+
+var result = a + b;
+
+return result;
+
+}
+
+function subtract(a, b) {
+
+var a;
+var b;
+
+var result = a - b;
+return result;
+
+}
+
+function multiply(a, b) {
+
+var a;
+var b;
+
+var result = a * b;
+return result;
+
+}
+
+function divide(a, b) {
+
+var a;
+var b;
+
+var result = a / b;
+return result;
+
+}
+
+function inc(n){
+
+  var n;
+
+  n++;
+
+  return n;
+
+}
+
+function dec(n){
+
+  var n;
+
+  n--;
+
+  return n;
+
+}
+
+
+function makeInt(n){
+
+    var n;
+
+    var x = parseInt(n, 10);
+
+    return x;
+
+}
+
+function preserveDecimal(n){
+
+  var n;
+
+  var x = parseFloat(n);
+
+  return x;
+
+}
