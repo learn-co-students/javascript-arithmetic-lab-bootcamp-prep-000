8c032f297e7c8bb51027258ff0dbc17474e57d67
Amanda Chen <amandachen13@gmail.com>
2017-02-02 03:55:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+function subtract(a, b) {
+  var diff = a - b;
+  return diff;
+}
+
+function multiply(a, b) {
+  var prod = a * b;
+  return prod;
+}
+
+function divide(a, b) {
+  var quot = a / b;
+  return quot;
+}
+
+function inc(n) {
+  n += 1;
+  return n;
+}
+
+function dec(n) {
+  n -= 1;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
