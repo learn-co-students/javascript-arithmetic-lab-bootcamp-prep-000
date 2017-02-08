47614b20759238994dff213fa4687c354607fc00
Vanessa Carrasco <vcarr2010@gmail.com>
2017-02-08 02:42:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function add (a, b){
+    return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+function inc(n){
+  return n += 1 //increase by 1
+}
+
+function dec(n){
+  return n -= 1 //decreaase by 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)//assume base 10, return as Int
+}
+function preserveDecimal(n) {
+  return parseFloat(n)//return decimal
+}
