336411768dca99506f58052affa4bc786639020d
Jordan Schultz <jpas97@gmail.com>
2017-02-08 01:25:01 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,29 +5,57 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
+function add(a, b) {
+  return a + b
+}
+
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
+
+function subtract(a, b) {
+  return a-b
+}
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
 })
 
+function multiply(a, b){
+  return a*b
+}
+
 it('multiply(a, b) multiplies two numbers and returns the result', function() {
   expect(multiply(a, b)).toEqual(a * b)
 })
+
+function divide(a, b) {
+  return a / b
+}
 
 it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
 })
 
+function inc(a) {
+  return a += 1
+}
+
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
 
+function dec(a) {
+  return a -= 1
+}
+
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
 })
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
 
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
@@ -42,6 +70,10 @@ describe('makeInt(n)', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
   })
 })
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
 
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
