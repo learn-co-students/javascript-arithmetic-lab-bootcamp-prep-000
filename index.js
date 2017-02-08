2de9aa57c5ab776cca5e4d80f7394810a36f0e36
Alex Calice <macciak@gmail.com>
2017-02-08 06:50:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+  return a+b;
+}
+add(10,20)
+function divide(a,b) {
+  return a/b;
+}
+divide(10,20)
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract(10,20)
+
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(10,20)
+
+function inc(n) {
+  return ++n;
+}
+inc(2)
+
+function dec(n) {
+  return --n;
+}
+dec(2)
+
+function makeInt(n) {
+  return parseInt(n,10);
+}
+makeInt('24.2')
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal(20.3)
+
