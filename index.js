40bb12171b9fccdda8430a571c411c8c5589f9c8
Tom Choe <choe_tom@yahoo.com>
2017-02-09 18:59:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var a = 1
+var b = 2
+
+function add(a, b) {
+  return a+=b
+}
+var a = 1
+    b = 2
+
+function subtract(a, b) {
+  return a-=b
+}
+
+function multiply(a, b) {
+  return a*=b
+}
+
+function divide(a, b) {
+  return a/=b
+}
+
+var n = 2
+
+function inc(n) {    //increment
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
