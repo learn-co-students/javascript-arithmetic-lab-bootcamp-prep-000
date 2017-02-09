f94543ee2ed935c870badd36c9bf5ca3a33a0aa1
Bruno Verussa <b.verussa@gmail.com>
2017-02-09 01:58:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(x){
+  return x+= 1;
+}
+
+function dec(x){
+  return x-= 1;
+}
+
+function makeInt(x){
+  return parseInt(x, 10);
+}
+
+function preserveDecimal(x){
+  return parseFloat(x);
+}
