9568d760b5bb4709eb24b3f4dae0d01c70c7b1b1
Rachel Walwood <walwoodr@gmail.com>
2017-02-13 19:12:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+  var c = a + b;
+  return c;
+}
+
+function subtract(a, b) {
+  var c = a - b;
+  return c;
+}
+
+function multiply(a, b) {
+  var c = a * b;
+  return c;
+}
+
+function divide(a, b) {
+  var c = a / b;
+  return c;
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  var c = parseInt(n, 10);
+  return c;
+}
+
+function preserveDecimal(n) {
+  var c = parseFloat(n);
+  return c;
+}
