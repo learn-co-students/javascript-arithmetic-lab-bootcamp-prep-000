844113b5364d26aacded2d0f3f2338ca2239b568
Jacqlene Sifre <jsifre01@gmail.com>
2017-02-14 21:54:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var number
+
+function add(a, b) {
+  return a + b
+}
+
+add(3, 5)
+
+function subtract(a, b) {
+  return a - b
+}
+
+subtract(5, 3)
+
+function multiply(a, b) {
+  return a * b
+}
+
+multiply(3, 5)
+
+function divide(a, b) {
+  return a / b
+}
+
+divide(15, 3)
+
+function inc(a) {
+  return a += 1
+}
+
+inc(3)
+
+function dec(a) {
+  return a -= 1
+}
+
+dec(3)
+
+function makeInt() {
+  return parseInt('3', 10)
+}
+
+makeInt()
