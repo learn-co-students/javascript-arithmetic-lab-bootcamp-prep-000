c7d12d3f851a91a186197f59a74a12a42e894c1d
Lia HUANG <lisa.yc.huang@gmail.com>
2017-02-16 23:39:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a+b;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n){
+  return n+1;
+}
+
+function dec(n){
+  return n-1;
+}
+
+function makeInt(str){
+  return parseInt(str,10);
+}
+
+function preserveDecimal(str){
+  return parseFloat(str);
+}
