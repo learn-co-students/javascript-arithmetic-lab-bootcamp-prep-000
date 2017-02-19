1efc4dc7f744ba7e8c1f754217059cb64fbe3769
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2017-02-19 23:51:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+fucntion inc(n){
+
+}
+function dec(n){
+
+}
+function makeInt(n){
+
+}
+function preserveDecimal(n){
+
+}
