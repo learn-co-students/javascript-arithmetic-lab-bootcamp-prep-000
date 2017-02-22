e4db1079e65da7eb1d3cae5eefe9c7039ec0eae2
Jack he <jackhe678@gmail.com>
2017-02-22 21:00:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+let add = (a, b) => a + b
+
+let subtract = (a, b) => a - b
+
+let multiply = (a, b) => a * b
+
+let divide = (a, b) => a / b
+
+let inc = n => n += 1
+
+let dec = n => n -= 1
+
+let makeInt = n => parseInt(n, 10)
+
+let preserveDecimal = n => n
