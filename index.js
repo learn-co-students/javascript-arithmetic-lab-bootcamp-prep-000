a75235d1d0541e76b5b2bfc7368382f9bd0f5f7c
Shaikat  Haque <shaikathaque4@gmail.com>
2017-02-26 07:47:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a+b
+}
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function inc(n) {
+  return parseInt(++n, 10)
+}
+
