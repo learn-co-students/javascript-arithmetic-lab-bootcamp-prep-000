9d1d7219c420069ef33b37ee9055d899319a5289
Mandy Sparber <sparber.mandy@gmail.com>
2017-02-28 15:47:37 +0000

    Done.

--- index.js
+++ index.js
@@ -15,22 +15,20 @@ function divide(a, b){
 }
 
 function inc(n){
-  n++
+  n += 1
   return n
 }
 
 function dec(n){
-  n--
+  n -= 1
   return n
 }
 
 function makeInt(n){
-  
   return parseInt(n,10)
 }
 
 function preserveDecimal(n){
-  parseFloat(n)
-  return n
+  return parseFloat(n)
 }
 
