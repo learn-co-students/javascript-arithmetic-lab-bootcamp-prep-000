11c026b7a82d97f815d1b99cdde06969a3871f46
Timothy O'Brien <timothy.p.obrien@gmail.com>
2017-03-06 23:52:23 +0000

    Done.

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ function divide(a, b){
 }
 
 function inc(n){
-  return n++;
+  return n += 1;
 }
 
 function dec(n){
-  return n--;
+  return n -+ 1;
 }
 
 function makeInt(n){
-  
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n){
-  
+  return parseFloat(n);
 }
 
