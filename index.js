b2610ad9c257ab9a2cf4d249db5845701f9aba63
W L <wlei2@binghamton.edu>
2017-03-06 14:01:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+// Function that adds two numbers and returns the result.
+function add(a,b){
+  return (a+b)
+}
+// Function that subtracts b from a and returns the result.
+function subtract(a,b){
+  return (a-b)
+}
+// Function that multiplies two numbers and returns teh result.
+function multiply(a,b){
+  return (a*b)
+}
+// Function that divides a by b and returns the result.
+function divide(a,b){
+  return (a/b)
+}
+// Function that increments a number and returns the result.
+function inc(n){
+  n++
+  return n
+}
+// Function that decrements a number and returns the result.
+function dec(n){
+  n-- 
+  return n
+}
+// Function that parses a number as an integer and returns it.
+function makeInt(n){
+  return parseInt("n",10)
+}
+// Function that parses a number as a floating point number and returns it. 
+function preserveDecimal(n){
+  return parseFloat("n")
+}
