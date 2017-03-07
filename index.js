8163b4f357ccd3f8a3855442c5626a3f033d8e57
fernanda lehaci <fernanda.lehaci@gmail.com>
2017-03-07 04:25:57 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function divide(a,b) {
   return a/b
 }
 function inc(n) {
-  return n++
+  return ++n
 }
 function dec(n) {
-  return n--
+  return --n
 }
-makeInt(n)
-  {return parseInt(n,10)
+function makeInt(n){
+  return parseInt(n,10)
 }
 function preserveDecimal(n) {
   return parseFloat(n)
