d27289ebdf462fdf778d00825097872b9416af2c
Timothy Cevallos <timcev@gmail.com>
2017-03-09 14:01:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b){
+return ( a + b );
+}
+function subtract(a,b){
+  return ( a - b);
+}
+function multiply(a,b){
+  return (a * b);
+}
+function divide(a,b){
+  return ( a / b );
+}
+function inc(a){
+  return (a + 1);
+}
+function dec(a){
+  return (a -1);
+}
+function makeInt(n){
+  return parseInt('n', 10);
+
+}
+function preserveDecimal(n){
+  n = 2.222
+  return parseFloat('n');
+}
