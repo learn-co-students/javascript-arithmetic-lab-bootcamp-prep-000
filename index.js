841befe28d0f0cc0b8d1ce004284695c662da0f4
Justin Davidoff <jdavidoff1992@gmail.com>
2017-03-09 21:41:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function inc(a){
+  a++
+  return a
+}
+
+function dec(a){
+  a--
+  return a
+}
+
+function makeInt(n){
+  var y = parseInt(n,10)
+  return y
+}
+
+function preserveDecimal(n){
+  var y = parseFloat(n)
+    return y
+}
