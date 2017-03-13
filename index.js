10d4ac89fe0537d30fc956fb7ef53a390291dffd
Matthew McAnelly <mail@matthewmcanelly.com>
2017-03-13 01:12:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+// Practice creating basic math functions in JavaScript
+
+// Adds two numbers and returns the result
+function add(first, second) {
+  return first + second
+}
+
+// Subtracts a second number from the first and returns the result
+function subtract(first, second) {
+  return first - second
+}
+
+// Multiplies two numbers and returns the result
+function multiply(first, second) {
+  return first * second
+}
+
+// Divides the second number from the first and returns the result
+function divide(first, second) {
+  return first / second
+}
+
+// Increments a number and returns the result
+function inc(number) {
+  return ++number
+}
+
+// Decrements a number and returns the result
+function dec(number) {
+  return --number
+}
+
+// Parses argument as an integer in base 10 and returns the result
+function makeInt(number) {
+  return parseInt(number, 10)
+}
+
+// Parses argument as a floating point number and returns the result
+function preserveDecimal(number) {
+  return parseFloat(number)
+}
+
