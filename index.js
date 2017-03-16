038c6578cda8f399a34845f30efdb67c057cbc7b
Darius Marte <darius.marte66@gmail.com>
2017-03-16 15:53:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function add(a,b){
+  return a + b
+}
+ function subtract(a,b){
+   return a - b
+ }
+ function multiply(app,genius){
+   return app * genius
+ }
+ function divide(a,b){
+   return a/b
+ }
+ function inc(n){
+   return n +=1
+ }
+
+ function dec(n){
+   return n -=1
+ }
+function makeInt (n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
