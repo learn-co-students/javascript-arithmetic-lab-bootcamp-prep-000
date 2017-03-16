af660a121aa73d54a68e8d2d2e64ee06cd479f22
Russell Woods <rkwoods@gmail.com>
2017-03-16 01:08:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  add = a + b;
+  return add
+}
+
+function subtract(a, b) {
+  subtract = a - b;
+  return subtract
+}
+
+function multiply(a, b) {
+  multiply = a * b;
+  return multiply
+}
+
+function divide(a, b) {
+  divide = a / b;
+  return divide
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
+}
