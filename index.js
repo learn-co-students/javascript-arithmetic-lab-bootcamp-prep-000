6796af04028660bfd36d53d9bf01ff21ed30d0ac
johnshelb <jsnd11@gmail.com>
2017-03-16 15:38:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 function add(a,b){
-  return a + b
+return a + b
 }
+
 function subtract(a,b){
-  return a-b
+return a - b
 }
+
 function multiply(a,b){
-  return a * b
+return a * b
 }
+
 function divide(a,b){
-  return a/b
+return a/b
 }
-function inc(n){
-  return n += 1
+
+function inc(a){
+ a++
+ return a
 }
-function dec(n){
-  return n -= 1
+
+function dec(a){
+ a--
+ return a
 }
+
 function makeInt(n){
-  return parseInt(n,10)
+return parseInt(n, 10)
 }
+
 function preserveDecimal(n){
-  return parseFloat(n)
+return parseFloat(n)
 }
