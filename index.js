f043ca5565f26f1b17cca916f5f8e4810e670eff
Alexander Yehlen <ayehlen@gmail.com>
2017-03-17 21:59:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (a, b){
+  return a + b
+}
+
+function subtract (a, b){
+  return a - b
+}
+
+function multiply (a, b){
+  return a * b
+}
+
+function divide (a, b){
+  return a / b
+}
+
+function preserveDecimal(n){
+  n += .5
+  return n
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n){
+  n -= 1
+  return n
+}
+
+function makeInt(n){
+  parseInt(n, 10)
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  parseFloat(n)
+  return parseFloat(n)
+}
