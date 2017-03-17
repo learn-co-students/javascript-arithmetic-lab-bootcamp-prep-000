ce596e6e14a5e27891c6df5d2604d6ad6d53520c
safar zitoun zine eddine <ubiclaw.1@gmail.com>
2017-03-17 09:57:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b){
+ return a+b;
+}
+function substract(a,b){
+ return b-a;
+}
+function multiply(a,b){
+ return a*b;
+}
+function divide(a,b){
+ return a/b;
+}
+function inc(a){
+ return ++a;
+}
+function dec(a){
+ return --a;
+}
+function makeInt(a){
+ return parseInt(a,10);
+}
+function preserveDecimal(a){
+ return parseFloat(a,10);
+}
