d12fa4a85831b3685ffb68fcf40e1aa3abaaeeb5
Roman Mulladzhanov <romanmullady@gmail.com>
2017-03-21 23:44:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+   return a+b
+}
+add(1631,4)
+
+function subtract(a,b){
+  return a-b
+}
+subtract(6,4)
+
+function multiply(a,b){
+  return a*b
+}
+multiply(6,5)
+
+function divide(a,b){
+  return a/b
+}
+divide(30,5)
+
+function inc(n){
+  return ++n
+}
+inc(8)
+
+function dec(n){
+  return --n
+}
+dec(9)
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+makeInt('4')
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+preserveDecimal('NaN')
