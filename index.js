84670fab999d907349e79069741d04356f63003d
Md Abu  Zafar <mdabuzafar78@gmail.com>
2017-03-21 11:47:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+// add
+function add(a, b) {
+   return a + b;
+}
+
+// subtract
+function subtract (a, b){
+    return a - b;
+}
+
+// multiply
+function multiply (a , b) {
+    return a * b;
+}
+
+// divide
+function divide (a, b){
+    return a / b;
+}
+
+function inc(c) {
+    return ++c;
+}
+
+function dec(c) {
+    return c -= 1;
+}
+
+function makeInt(n) {
+    var n = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
