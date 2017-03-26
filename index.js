2cf43dc979cee668d22b4b9bad673bfae7fba3e1
Clayton Hebenik <clayton.hebenik@gmail.com>
2017-03-26 19:04:52 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function dec(n) {
   return n -= 1;
 }
 function makeInt(n) {
-  parseInt(`$n`, 10);
+  return parseInt(n, 10);
 }
 function preserveDecimal(n) {
-  parseFloat(n);
+  return parseFloat(n);
 }
