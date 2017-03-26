e7e1df3ed18c439d2e2653d79dc41c1a2d65b64d
Kelsey Fecho <kfecho30@gmail.com>
2017-03-26 02:18:46 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function divide(a,b) {
 }
 
 function inc(n){
-  return n++;
+  return n +=1;
 }
 
 function dec(n) {
-  return n--;
+  return n -=1;
 }
  function makeInt(n) {
   return parseInt(n,10);
  }
- function perserveDecimal(n) {
+ function preserveDecimal(n) {
    return parseFloat(n);
  }
