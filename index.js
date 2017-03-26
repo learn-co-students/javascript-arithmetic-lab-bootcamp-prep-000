5bd6c7f5b74b3c65c678e5197113c745f097aef7
Anthony Verdesca <verdesca2@gmail.com>
2017-03-26 01:56:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+return (a+b)
+}
+
+function subtracts(a,b){
+return (a-b)
+}
+
+function multiply(a,b){
+return (a*b)
+}
+
+function divide(a,b){
+return (a/b)
+}
+
+function inc(number){
+return (number +=3)
+}
+
+function dec(number){
+return (number -= 2)
+}
+
+function mult(number){
+return (number*= 10)
+}
+
+function makeInt(n){
+return (parseInt(n))
+}
