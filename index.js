82c6611cb50d49372074e12ec36b780fbc31a834
Joe Cutajar <jcutajar12@gmail.com>
2017-03-31 20:09:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(x,y){
+  var sum = x + y
+  return sum;
+  
+}
+
+function subtract(x,y) {
+  var sub = x - y
+  return sub;
+}
+
+function multiply(x,y) {
+  var mult = x * y
+  return mult;
+}
+
+function divide(x,y) {
+  var divd = x/y
+  return divd;
+}
+
+function inc(n){
+    var sumup = ++ n
+    return sumup;
+}
+
+function dec(n){
+  var down = -- n 
+  return down;
+}
+
+function makeInt(n) {
+  var par = parseInt(n, 10)
+  return par;
+}
+
+function preserveDecimal(n){
+  var flot = parseFloat(n)
+  return flot;
+}
