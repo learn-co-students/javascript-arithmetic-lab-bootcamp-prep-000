74a6b4835779e3ae473e7294afa2fea9d14be7e8
Denis Kruglenko <denisvkr@gmail.com>
2017-03-31 17:43:59 +0000

    Done.

--- index.js
+++ index.js
@@ -25,10 +25,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt ("n", 10);
-  return parseInt;
+ return parseInt (n, 10);
 }
 
 function preserveDecimal(n) {
-  parseFloat("n");
+  return parseFloat(n);
 }
