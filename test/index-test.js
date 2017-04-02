9cb2d9f037cc02de4f46289f83c6ef8a7791ddfd
Ron Cuan <ronald.cuanzon@gmail.com>
2017-04-02 06:32:06 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,6 +4,35 @@ beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
 })
+//-------------FUnction 1--------------
+function add(a, b){
+  return a + b;
+}
+//-------------Function 2--------------
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+function divide (a, b){
+  return a/b;
+}
+
+function inc(n){
+  return n += 1;
+}
+
+function dec(n){
+  return n -= 1;
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  return parseFloat(n, 9)
+}
 
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
