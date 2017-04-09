0c3270470d68c87598a3487d1c511e66a9539559
Justina Tran <justinavtran@yahoo.com>
2017-04-09 19:34:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = 23
+var b = 5
+
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+var n = 500
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
