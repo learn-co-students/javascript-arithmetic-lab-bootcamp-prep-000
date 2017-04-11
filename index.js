02382fedbf3ad898f64585d21d3c36447b7ebf30
Anton Parayno <dantoni09@yahoo.com>
2017-04-11 02:08:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b){
+  var sum = a+b
+  return sum
+}
+
+function subtract(a,b){
+  var diff = a - b
+  return diff
+}
+
+function multiply(a,b){
+  var product = a*b
+  return product
+}
+
+function divide(a,b){
+  var quotient = a / b
+  return quotient
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n){
+  n -= 1
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
