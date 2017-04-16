91a35aca8e4eb615975633f32c211b3e4ef6a522
Alexandra Wright <superbiscuit@gmail.com>
2017-04-16 20:48:27 +0000

    Done.

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ function subtract(a, b){
 }
 
 function multiply(a,b){
-return   a*b
+  return a * b
 }
 
 function divide(a,b){
-  return a/b
+  return a / b
+
 }
 
 function inc(n){
-  return n + 1
+return ++n
 }
 
 function dec(n){
-  return n - 1
+return --n
 }
 
-function makeInt(n){
-  return parseInt(n,10)
+function makeInt(n, 10){
+return parseInt(n)
 }
 
 function preserveDecimal(n){
-  return parseFloat(n)
+return parseFloat(n)
 }
