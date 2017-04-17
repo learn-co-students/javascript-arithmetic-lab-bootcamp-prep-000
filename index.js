aec7ebb9f29e6c3e87f5f8bf954e2ca8f3a6a81b
Jay Sargent <sargentjay11@gmail.com>
2017-04-17 19:33:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  var sum;
+  sum = a + b;
+
+  return sum;
+}
+function subtract(a, b) {
+  var difference;
+  difference = a - b;
+
+  return difference;
+}
+function multiply(a, b) {
+  var product;
+  product = a * b;
+
+  return product;
+}
+function divide(a, b) {
+  var quotient;
+  quotient = a / b;
+
+  return quotient;
+}
+function inc(n) {
+  n++;
+  return n;
+}
+function dec(n) {
+  n--;
+  return n;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
