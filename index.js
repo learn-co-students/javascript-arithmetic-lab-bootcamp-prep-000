5ca31effa2f826f7d894b5e1227a182c08467ef8
Arnish Shakil <arnish.shakil@gmail.com>
2017-04-21 19:27:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var a = 5
+var b = 6
+
+
+function add(a, b)
+{
+  return a + b
+}
+
+function subtract(a, b)
+{
+  return a - b
+}
+
+function multiply(a, b)
+{
+  return a * b
+}
+
+function divide(a, b)
+{
+  return a/b
+}
+
+function inc(n)
+{
+  return ++n
+}
+
+function dec(n)
+{
+  return --n
+}
+
+function makeInt(n)
+{
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n)
+{
+  parseFloat('n')
+  return n
+}
