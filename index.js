54065c704cedd47df63dd28ddd15141b7791aac4
Jennifer Watson <jennifer.watson@me.com>
2017-04-26 14:33:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return (a + b);
+}
+
+function subtract(a, b) {
+  return (a - b);
+}
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+function divide(a, b) {
+  return (a / b);
+}
+
+function inc(n) {
+  var a = n;
+  a++;
+  return a;
+}
+
+function dec(n) {
+  var a = n;
+  a--;
+  return a--;
+}
+
+function makeInt(a) {
+  return parseInt(a,10);
+}
+
+function preserveDecimal(n) {
+  var a = n;
+  return parseFloat(a);
+}
