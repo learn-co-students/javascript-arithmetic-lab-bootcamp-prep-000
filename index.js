53f5e133f665eb632f000c5abb77fd6fdec7fa38
Devon Boen <devonjboen@gmail.com>
2017-05-02 23:25:25 +0000

    devonjboen

---
+++ index.js
@@ -0,0 +1,38 @@
+var a, b
+a = 14
+b = 8
+function add(a,b) {
+return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(a) {
+  return a + 1;
+}
+
+function dec(a) {
+  return a - 1;
+}
+
+
+function makeInt(a) {
+return parseInt(a, 10);
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a,10);
+}
+
+
+
