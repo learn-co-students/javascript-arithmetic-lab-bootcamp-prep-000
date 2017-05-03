cb69600871f9094979195396b21d73bdb64042f2
Sho Miyata <sho.miyata.1@gmail.com>
2017-05-03 03:02:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+//add(a, b) adds two numbers and returns the result
+function add(a, b){
+  return(a+b)
+}
+
+function multiply(a, b){
+  return(a*b)
+}
+
+function divide(a, b){
+  return(a/b)
+}
+
+function inc(n){
+  return(n+1)
+}
+
+function dec(n){
+  return(n-1)
+}
+
+function makeInt(n){
+  return(parseInt(n,10))
+}
+
+function preserveDecimal(n){
+  return(parseFloat(n))
+}
+
+function subtract(a,b){
+  return(a-b)
+}
