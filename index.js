c8b463fd723745d16c35c81d26035063dd3d4a51
Robert Gonzalez <robertgonzalez@utexas.edu>
2017-05-08 00:39:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+var n =  a + b;
+return n
+};
+function subtract(a, b) {
+var n =  a - b;
+return n
+};
+function multiply(a, b) {
+var n =  a * b;
+return n
+};
+function divide(a, b) {
+var n =  a / b;
+return n
+};
+function inc(n) {
+var n =  n+=1;
+return n
+};
+function dec(n) {
+var n = n-=1;
+return n
+};
+function makeInt(n) {
+var n = parseInt(n, 10);
+  return n
+};
+function preserveDecimal(n) {
+var n = parseFloat(n);
+return n
+};
