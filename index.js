ec16f0dd8024bff07ceaf6d63a695d0f0d611a5f
Brent  Ransom <sadumedusa@gmail.com>
2017-05-09 11:34:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function preserveDecimal(n){
+  parseFloat('nonsense')
+}
+
+function preserveDecimal(n){
+  parseFloat('2.222')
+
+}
+
+function makeInt(n){
+  parseInt('nonsense')
+}
+
+function makeInt(n){
+  parseInt('2')
+}
