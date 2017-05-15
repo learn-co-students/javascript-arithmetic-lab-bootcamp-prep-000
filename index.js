2af8e5abc69e2b2d1c349be9eebdb1e552b82b20
shawn mavunga <shawn.b.mavunga@gmail.com>
2017-05-15 01:13:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+//add function//
+function add(a, b) {
+  return a + b
+};
+//subtract//
+function subtract(a,b) {
+  return a - b
+};
+
+function multiply(a,b) {
+  return a * b
+};
+
+function divide(a, b){
+  return a/b
+};
+
+function inc(n) {
+  return a++
+};
+
+function dec(n) {
+  return a--
+};
+
+function makeInt(n) {
+  parseInt(n, 10)
+};
