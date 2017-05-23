f97076b5ebbfdaeb403709f4336f626cb448e733
Kyle Loftin <kaloftin@hotmail.com>
2017-05-23 03:14:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 var a = Math.floor(Math.random() * 1000);
 var b = Math.floor(Math.random() * 1000);
 
-function add() {
-  return a += b;
+function add(a, b) {
+  return a + b;
 }
 
-add();
+add(a, b);
+
+function subtract(a, b) {
+  return a - b;
+}
+
+subtract(a, b);
+
+function multiply(a, b) {
+  return a * b;
+}
+
+multiply(a, b);
+
+function divide(a, b) {
+  return a / b;
+}
+
+divide(a, b);
+
+function inc(a) {
+  return a + 1;
+  }
+
+inc(a);
+
+function dec(a) {
+  return a - 1;
+  }
+
+dec(a);
+
+function makeInt(a) {
+  return parseInt(a, 10);
+}
+makeInt(a);
+
+function preserveDecimal(a) {
+  return parseFloat(a, 10);
+}
+preserveDecimal(a);
