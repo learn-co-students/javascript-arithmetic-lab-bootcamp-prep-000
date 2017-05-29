9060c7830327997e9c09a245ddb7eeed5db94a9f
louis Hellinger <lhellinger@gmail.com>
2017-05-29 14:40:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,56 @@
+
+function add(a, b){
+  return a += b
+}
+
+add()
+
+
+function subtract(a, b){
+ return a -= b
+}
+
+subtract()
+
+
+function multiply(a, b){
+  return a *= b
+}
+
+multiply()
+
+function divide(a,b){
+  return a /= b
+}
+
+divide()
+
+function inc(n){
+  n++
+  return n
+
+}
+
+inc()
+
+function dec(n){
+  n--
+  return n
+  }
+
+dec()
+
+function makeInt(n){
+  parseInt(n, 10)
+return n
+}
+
+makeInt()
+
+
+function preserveDecimal(n){
+  parseFloat(n)
+  return n
+  }
+
+  preserveDecimal()
