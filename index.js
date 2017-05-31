b170c9dd324157ac6d9ed148559a431dc3c7618c
Amar Chatman <amarjerome@gmail.com>
2017-05-31 19:47:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return a + b 
+}
+function subtract(a,b){
+  return a - b 
+}
+function multiply(a,b){
+  return a * b 
+}
+function divide(a,b){
+  return a/b
+}
+ 
+function inc(n){
+  var n 
+  return n += 1  
+}
+
+function dec(n){
+  var n
+  return n -=1
+}
+
+function makeInt(n){
+  var n
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  var n 
+  return parseFloat(n)
+}
