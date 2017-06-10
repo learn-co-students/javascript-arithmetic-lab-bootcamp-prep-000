a0d51484a611cf4db29b38a389a1226c401163eb
Felipe Valencia <jfvalencia@gmail.com>
2017-06-10 00:04:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc(a){
+  var number = a
+  number++
+  return number
+}
+
+function dec(a){
+  var number = a
+  number--
+  return number
+}
+
+function makeInt(a){
+  return parseInt(a,10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a)
+}
