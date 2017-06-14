e2ebb5e76fab4bfcd8886642b2f59be80c741b4d
Bo Keopaseut <bopk@hawaii.edu>
2017-06-14 01:49:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+   return a + b;
+}
+
+function subtract(a,b) {
+   return a - b;
+}
+
+function multiply(a,b) {
+   return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(int) {
+     int++;
+     return int;
+}
+
+function dec(int) {
+  var number = int;
+
+  number--;
+  return number;
+}
+
+function makeInt(n) {
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
