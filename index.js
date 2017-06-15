e52080870df9d673ac00a3d143ce881f9e679395
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-06-15 00:02:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 function add (a,b)
 {
-  return ${a} + ${b}
+  return a + b
+}
+
+function subtract (a,b)
+{
+  return a - b
+}
+
+function multiply (a,b)
+{
+  return a * b
+}
+
+function divide (a,b)
+{
+  return a / b
+}
+
+function inc (n) {
+n++
+return n
 }
