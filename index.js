c6ed13b7c908bba4b092fda3ef8aca2fa9ca5e96
Bennett Marcelo <xxleben85@yahoo.com>
2017-06-17 17:10:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var number = 10
-function add5() {
-  number += 5
+function add (a, b) {
+  return a + b
 }
-function divideBy3() {
-  number/=3
+function subtract (a, b) {
+  return a - b
 }
-dvideby3()
-console.log(number)
-number = 10
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
+function multiply (a, b) {
+  return a * b
+}
+function divide (a, b) {
+  return a/b
+}
+function inc(n) {
+  return n += 1
+}
+function dec(n) {
+  return n -= 1
+}
+function makeInt(n) {
+  return parseInt(n,10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
