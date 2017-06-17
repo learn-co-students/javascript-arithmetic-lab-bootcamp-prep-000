dc041767f756df2a8571fbb14d8e4f1ae02f2fed
Bennett Marcelo <xxleben85@yahoo.com>
2017-06-17 16:16:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var number = 10
+function add5() {
+  number += 5
+}
+function divideBy3() {
+  number/=3
+}
+dvideby3()
+console.log(number)
+number = 10
+add5()
+console.log(number)
+divideBy3()
+console.log(number)
