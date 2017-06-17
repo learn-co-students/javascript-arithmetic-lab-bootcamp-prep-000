6d0273349ab902ea179fcabb5367648c1819a4f5
Andrew Adeniyi <drewadeniyi@gmail.com>
2017-06-17 00:45:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add(a,b) {
+  return (a + b)
+}
+
+function subtract(a,b) {
+  return (a - b)
+}
+
+function multiply(a,b) {
+  return (a * b)
+}
+
+function divide(a,b) {
+  return (a / b)
+}
+
+function inc(a) {
+  return (a += 1)
+}
+
+function dec(a) {
+  return (a -= 1)
+}
+
+function makeInt(n) {
+  if (n = parseInt(n,10)){
+    return parseInt(n,10)
+  }
+  else if (n = parseInt(n,10)) {
+    return parseInt(0)
+  }
+  else {
+    return ("NaN")
+  }
+}
+
+function preserveDecimal(n) {
+  if (n = parseFloat(n)) {
+    return parseFloat(n)
+  }
+  else {
+      return ("NaN")
+  }
+}
