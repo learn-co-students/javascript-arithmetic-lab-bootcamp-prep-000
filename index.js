8bc4bf2de25d2642d21cf798d7d49c051a6947b1
tom Scharwark <tscharwarkjr@gmail.com>
2017-06-17 03:03:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var add = function(a, b){
+  var result = a + b;
+  return result;
+}
+var subtract = function(a, b){
+  var result = a - b;
+  return result;
+}
+var multiply = function(a, b){
+  var result = a * b;
+  return result;
+}
+var divide = function(a, b){
+  var result = a / b;
+  return result;
+}
+var inc = function(n){
+  n++;
+  return n;
+}
+var dec = function(n){
+n--;
+return n;
+}
+var makeInt = function(n){
+  var r = parseInt(n, 10);
+  return r;
+}
+var preserveDecimal = function(n){
+  var r = parseFloat(n);
+  return r;
+}
