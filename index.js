cf31c756b429118a78efce4e2175d8afe63753e6
Catherine Giese <ceg224@cornell.edu>
2017-06-20 21:45:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
+function add(2, 3){
+  return 2 + 3
+}
+function subtract(2, 3){
+  return 2 - 3
+}
+function multiply(2, 3){
+  return 2 * 3
+}
+function divide(2, 3){
+  return 2 / 3
+}
+function inc(n){
+  return inc(n)
+}
+dec (n)
+function makeInt(n){
+  return parseInt('n')
+}
+makeInt(n)
+preserveDecimal(n)
