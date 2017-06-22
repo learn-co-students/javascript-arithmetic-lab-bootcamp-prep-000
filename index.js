bc6a2481a0d6ef421506523d3133ea7ba3329275
Ben Reuling <benreuling@hotmail.com>
2017-06-22 00:33:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  a  = a + b
+  return a
+}
+function subtract(a,b){
+  a = a-b
+  return a
+}
+function multiply(a,b){
+  a = a*b
+  return a
+}
+function divide(a,b){
+  a = a/b
+  return a
+}
+function inc(n){
+  n = n + 1
+  return n
+}
+function dec(n){
+  n = n - 1
+  return n
+}
+function makeInt(n){
+  n = parseInt(n,10)
+  return n
+}
+function preserveDecimal(n){
+  n = parseFloat(n)
+  return n
+}
