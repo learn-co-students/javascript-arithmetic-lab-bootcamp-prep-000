e5994dfbb51b7c9778dc425af5fba66c6273333d
Geoffrey Barnes <geoff@geoffreybarnes.com>
2017-06-22 16:16:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+// Anyone looking at this?  email geoff@geoffreybarnes.combined
+
+function add(a,b){
+  return a += b
+}
+
+function subtract(a,b){
+  return a -= b
+}
+
+function multiply(a,b){
+  return a *= b
+}
+
+function divide(a,b){
+  return a /= b
+}
+function inc(n){
+  return n += 1
+}
+
+function dec(n){
+  return n -= 1
+}
+
+function makeInt(a){
+  return parseInt(a,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
