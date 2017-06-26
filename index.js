65bd043ab04c3115ddcb7422cb24b8d0cb581e4f
Travis Williamson <travywill@gmail.com>
2017-06-26 18:34:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x,y) {
+  return x + y
+}
+
+function subtract(x,y) {
+  return x - y
+}
+
+function multiply(x,y) {
+  return x * y
+}
+
+function divide(x,y) {
+  return x / y
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  var integer = parseInt(n, 10);
+  return integer
+}
+
+function preserveDecimal(n) {
+  var float = parseFloat(n);
+  return float
+}
