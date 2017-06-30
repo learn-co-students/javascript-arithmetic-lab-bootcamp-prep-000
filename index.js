b1232cc575ff69d29b69699409f1e19504a9ef15
Kuntian Yu <yukuntian@gmail.com>
2017-06-30 03:17:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a=10
+var b=5
+function add(a,b){
+  return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+var n=10.553
+function inc(n){
+  return ++ n
+}
+
+function dec(n){
+  return -- n
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
