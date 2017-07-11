edbf0c0cfec37bf1577d237cb246c89dc0de5f2f
Brooke Ryan <brookeryan630@icloud.com>
2017-07-11 18:32:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+var n = 2.222
+
+var a = 1
+
+var b = 2
+
+
+
+function add(a, b) {
+  a += b;
+  console.log();
+}
+
+function subtract(a, b) {
+  a -= b;
+  console.log();
+}
+
+function multiply(a, b) {
+  a *= b;
+  console.log();
+}
+
+function divide(a, b) {
+  a /= b;
+  console.log();
+}
+
+function inc(n) {
+  n ++;
+  return n;
+}
+
+function dec(n) {
+  n --;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt(n, 10);
+  console.log(n);
+}
+
+function preserveDecimal(n) {
+  parseFloat('n');
+  console.log('n');
+}
