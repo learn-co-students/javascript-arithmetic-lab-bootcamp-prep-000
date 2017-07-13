0a637fe3d3e0428293cda1069b40eec46023dc2a
Karen Truss <truss.karen@gmail.com>
2017-07-13 05:09:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var a=5
+var b=6
+var n=25.4
+function add(a,b) {
+  return a+=b
+    }
+function subtract(a,b){
+  return a-=b
+}
+function multiply(a,b){
+  return a*=b
+}
+function divide(a,b){
+  return a/=b
+}
+function inc(n){
+  return n+=1
+}
+function dec(n){
+  return n-+1
+}
+function makeInt(n){
+  return parseInt(n,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
