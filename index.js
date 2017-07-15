0fb9774a9126ee05b19821ef741a79b114a7260e
Laura Webber <wwebby1@gmail.com>
2017-07-15 03:33:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(x,y){
+  return x+y
+}
+function subtract(x,y){
+  return x-y
+}
+function multiply(x,y){
+  return x*y
+}
+function divide(x,y){
+  return x/y
+}
+function inc(n){
+  n = n+1
+  return n
+}
+function dec(n){
+  n = n-1
+  return n
+}
+function makeInt(n){
+  return parseInt(n,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
