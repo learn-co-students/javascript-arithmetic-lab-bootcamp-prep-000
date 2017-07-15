bd26b389cc82b2f713127d62da6d6f60f8f592b0
Cory Powers <cory.pwrs@ymail.com>
2017-07-15 01:18:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add(a, b){
+  parseInt('a', 'b')
+  return a + b
+}
+
+function subtract(a, b){
+  parseInt('a', 'b')
+  return a - b
+}
+
+function divide(a, b){
+  parseInt('a', 'b')
+  return a / b
+}
+
+function multiply(a, b){
+  parseInt('a', 'b')
+  return a * b
+}
+
+function inc(a){
+  parseInt('a')
+  return a += 1
+}
+
+function dec(a){
+  parseInt('a')
+  return a -= 1
+}
+
+function makeInt(a){
+  parseInt('n')
+  return a
+}
+
+function preserveDecimal(n){
+  parseFloat('2.222', 'sldkjflksjf')
+  return 2.222 = false
+}
