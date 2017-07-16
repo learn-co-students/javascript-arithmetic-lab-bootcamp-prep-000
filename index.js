86394b29060c61b753b51b45e9fb2fec998093d1
Jeremy Lindstrom <dekard@gmail.com>
2017-07-16 22:38:37 +0000

    tests passed

---
+++ index.js
@@ -0,0 +1,32 @@
+// Test 1 add a, b and return results
+function add(a,b) {
+  return a + b;
+}
+//test 2: function subtract(a,b)
+function subtract(a,b){
+  return a - b;
+}
+//test 3: divide a and b
+function divide(a,b) {
+  return a / b;
+}
+//test 4: multiply a/b
+function multiply(a,b) {
+  return a * b;
+}
+//test 5 increment + 1
+function inc(n) {
+  return parseInt(n+= 1);
+}
+//test 6 decrement
+function dec(n) {
+  return parseInt(n-= 1);
+}
+//test 7 makeInt
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+//test 8
+function preserveDecimal(n) {
+   return parseFloat(n);
+}
