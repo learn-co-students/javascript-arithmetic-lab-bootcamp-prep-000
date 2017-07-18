0fc587eb048294e9554996bd2e3605b5d533848d
Antonio Filander <antonio.filander@gmail.com>
2017-07-18 12:04:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var number;
+function add (a,b){
+  number = a+b;
+  return number;
+}
+var number;
+function subtract (b,a){
+  number = b-a;
+  return number;
+}
+var number;
+function multiply (a,b) {
+  number = a*b;
+  return number;
+}
+var number;
+function divide (a,b) {
+  number = a/b;
+  return number;
+}
+var number;
+function inc(a) {
+  number = a+1;
+  return number;
+}
+var number;
+function dec(a) {
+  number = a-1;
+  return number;
+}
+var number;
+function makeInt(a) {
+  number = parseInt(a,10);
+  return number;
+}
+var number;
+function preserveDecimal(a) {
+  number = parseFloat(a,10);
+  return number;
+}
