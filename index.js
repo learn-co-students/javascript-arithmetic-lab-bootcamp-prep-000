6b373971c1af75503ce152d87916378c34f10d13
David Tannenbaum <david.tannenbaum@gmail.com>
2017-07-18 00:16:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
