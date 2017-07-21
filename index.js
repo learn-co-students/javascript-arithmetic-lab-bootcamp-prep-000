e816469f4893ae2b940d64b98e40f334d1194d1a
Brian Feibus <brianfeibus@gmail.com>
2017-07-21 23:52:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2) {
+  return (number1 + number2)
+}
+
+function subtract(number1, number2) {
+ return (number1 - number2)
+}
+
+function multiply(number1, number2) {
+ return (number1 * number2)
+ }
+
+function divide(number1, number2) {
+ return (number1 / number2)
+ }
+
+function inc(n) {
+return (n += 1)
+}
+
+function dec(n) {
+return (n -= 1)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
