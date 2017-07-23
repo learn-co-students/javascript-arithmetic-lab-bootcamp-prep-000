1c580b75ff6433af4e9026ae04ee648ca01b4187
Steven Snow <stevensno@gmail.com>
2017-07-23 21:41:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function add(a,b){
+  return a += b
+}
+
+function subtract(a,b){
+  return a -= b
+}
+
+function multiply(a,b){
+  return a *= b
+}
+
+function divide(a,b){
+  return a /=b
+}
+
+function makeInt(num){
+  return parseInt(num)
+}
+
+function preserveDecimal(num){
+  return parseFloat(num)
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(string){
+  return parseInt(string, 10)
+}
