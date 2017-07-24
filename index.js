ea520ff35fced1122c4cc70f6d30823dd74b01b9
Steven Chu <work@stevenchustudio.com>
2017-07-24 01:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function add(a,b) {
-  return a+=b;
+  return a + b
 }
 
 function subtract(a,b) {
-  return a-=b;
+  return a - b
 }
 
 function multiply(a,b) {
-  return a*=b;
+  return a * b
 }
 
 function divide(a,b) {
-  return a/=b;
+  return a / b
 }
 
 function inc(n) {
