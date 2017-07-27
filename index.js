c539888b3a5a23f32310f4c5b2025357a33f7490
Andrew Swearengen <andrew@arkadiagear.com>
2017-07-27 02:22:49 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function add (a,b) {
 function subtract (a,b) {
   return a - b
 }
+
+function multiply (a,b) {
+  return a * b
+}
+
+function divide (a,b) {
+  return a / b
+}
+
+function inc () {
+  n +=
+}
