5ac17ef164b5a681bd5ef64d93f8c6256769e60f
Andrew Swearengen <andrew@arkadiagear.com>
2017-07-27 02:29:01 +0000

    Done.

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ function divide (a,b) {
   return a / b
 }
 
-function inc () {
-  n +=
+function inc (n) {
+  return n += 1
+}
+
+function dec (n) {
+  return n -= 1
+}
+
+function makeInt (n) {
+    return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n,10)
 }
