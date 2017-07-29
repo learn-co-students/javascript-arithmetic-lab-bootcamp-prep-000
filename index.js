c47141bd7c191ed38461a6daf795910ac0230bd7
John Daise <jdaise@gmail.com>
2017-07-29 02:30:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  console.log(a+b)
+  return a+b
+}
+
+function subtract(a,b){
+  console.log(a-b)
+  return a-b
+}
+
+function multiply(a,b){
+  console.log(a*b)
+  return a*b
+}
+
+function divide(a,b){
+  console.log(a/b)
+  return a/b
+}
+
+function inc(a){
+  console.log(a++)
+  return a++
+}
+
+function dec(a){
+  console.log(a--)
+  return a-- 
+}
+
+function makeInt(n){
+  console.log(n)
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  console.log(n)
+  return parseFloat(n)
+}
