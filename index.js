2c7b118fe97cd7566cd0c98dc834d4eb9fd88840
Christine <christinetran825@gmail.com>
2017-08-04 03:10:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(n) {
+  return n+1
+}
+
+function dec(n) {
+  return n-1
+}
+
+function makeInt(n){
+  return parseInt('796',10)
+}
+/*function makeInt(n) {
+  return makeInt(a === a)
+}
+
+function preserveDecimal(n) {
+  return parseInt('2.222', 10)
+}*/
+
+function preserveDecimal(n) {
+  return parseFloat('sldkjflksjf')
+}
