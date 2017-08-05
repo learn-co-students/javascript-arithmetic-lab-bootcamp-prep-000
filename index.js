7292bd26f1b10a0de23ee3355173b35bbd8b760e
omikolaj <oscar222@sbcglobal.net>
2017-08-05 23:52:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  var result
+  return result = a + b
+}
+
+function subtract(a,b){
+  var result
+  return result = a - b
+}
+
+function multiply(a,b){
+  var result
+  return result = a * b
+}
+
+function divide(a,b){
+  var result
+  return result = a / b
+}
+
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
