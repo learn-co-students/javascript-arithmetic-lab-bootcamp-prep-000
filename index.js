f8d6ca355fd9f5faa6d1621895f0765d6c633e84
Gregory Helton <greg.helton.tx@gmail.com>
2017-08-07 17:48:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+let add = function(x,y) {
+  return x + y;
+}
+
+let subtract = function(x,y) {
+  return x - y;
+}
+
+let multiply = function(x,y) {
+  return x * y;
+}
+
+let divide = function (x,y) {
+  return x / y;
+}
+
+let dec = function(n) {
+  return --n;
+}
+
+let inc = function(n) {
+  return ++n;
+}
+
+let makeInt = function(n) {
+  return parseInt(n, 10);
+}
+
+let preserveDecimal = function(n) {
+  return parseFloat(n);
+}
