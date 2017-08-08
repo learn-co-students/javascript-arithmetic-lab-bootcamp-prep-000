ebd26a626298c91c180ddcc8021c225701baac1f
MCPitlik <matt.pitlik@gmail.com>
2017-08-08 20:53:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return(a + b)
+}
+function subtract(a, b) {
+  return(a - b)
+}
+function multiply(a, b) {
+  return (a * b)
+}
+function divide(a, b) {
+  return (a / b)
+}
+function inc(number) {
+  return (number+=1)
+}
+function dec(number) {
+  return (number-=1)
+}
+function makeInt(number) {
+  return (parseInt(number, 10))
+}
+function preserveDecimal(number) {
+  return (parseFloat(number))
+}
