28735fe7d48076cec2733add8fd6d7bc6bad4512
Peter Meister <prmeister89@gmail.com>
2017-08-08 04:37:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b) {
+  var add = a + b
+  return add
+}
+
+function subtract(a,b) {
+  var subtract = a - b
+  return subtract
+}
+
+function multiply(a,b) {
+  var multiply = a * b
+  return multiply
+}
+
+function divide(a,b) {
+  var divide = a / b
+  return divide
+}
+function inc(a){
+  var increment = a + 1
+  return increment++
+}
+
+function dec(a){
+  var increment = a - 1
+  return increment--
+}
+
+function makeInt(a){
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a){
+  return parseFloat (a, 10)
+}
