13dfd8ef378ac071ea0b6d4f748b7678a9603a25
Robert Lemp <robertlemp@verizon.net>
2017-08-09 02:38:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(a, b) {
+  return (a + b);
+}
+
+console.log(add)
+
+function subtract(a,b) {
+  return (a-b);
+  }
+
+  console.log(subtract)
+
+
+function multiply(a, b) {
+  return (a*b);
+}
+
+console.log(multiply)
+
+function divide(a,b) {
+  return (a/b);
+}
+
+console.log(divide)
+
+function inc(a) {
+  return a+1;
+}
+
+console.log(inc)
+
+function dec(a) {
+  return a-1;
+}
+
+console.log(dec)
+
+function makeInt(a) {
+  return parseInt(a);
+}
+
+console.log(makeInt)
+
+function makeInt(a) {
+  return parseFloat(a);
+}
+
+console.log(makeInt)
+
+function preserveDecimal(a) {
+  return parseFloat(a);
+}
+
+console.log(preserveDecimal)
