c360224a579f5c96420011eb0c4bae4ff8a2459b
Enrique Lopez <mr.kik3@hotmail.com>
2017-08-09 04:50:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a , b) {
+  return a + b
+}
+
+function subtract(a , b) {
+  return a - b
+}
+
+function multiply(a , b) {
+  return a * b
+}
+
+function divide(a , b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n,10)
+  return parseInt('asd',10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n,10)
+  return parseFloat('asd',10)
+}
