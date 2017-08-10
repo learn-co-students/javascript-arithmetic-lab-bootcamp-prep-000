d459f817de0942ea7991d22edf31dfbe45c35f98
1Programmer1 <a86749137@gmail.com>
2017-08-10 01:01:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var a, b
+
+function add(a, b) {
+  (a + b)
+  return(a + b)
+};
+function subtract(a, b) {
+  (a - b)
+  return(a - b)
+};
+function multiply(a, b) {
+(a * b)
+  return(a * b)
+};
+function divide(a, b) {
+(a / b)
+  return(a / b)
+};
+
+function inc(a) {
+(a + 1)
+  return(a + 1)
+};
+
+function dec(a) {
+(a - 1)
+  return(a - 1 )
+};
+
+function makeInt(a){
+  return(parseInt(a, 10))
+};
+
+function preserveDecimal(a){
+	return(parseFloat(a))
+};
