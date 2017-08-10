0f20e7560c2747796fa0a7ddb6ccfcab4dd4bff9
Liam Killion <liam.killion@gmail.com>
2017-08-10 16:43:32 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(a,b){
 }
 
 function inc(n){
-  return n+=
+  return n += 1
 }
 
 function dec(n){
-  return n-=
+  return n -= 1
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function perserveDecimal(n){
+  return parseFloat(n)
 }
