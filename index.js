fe6b67ad446ad8076f5ce8237701daf061b7cfa2
alexandrahill <alexandrahill@live.com>
2017-08-12 01:30:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function(){
+  add(a,b)
+}
+
+function(){
+  subtract(a,b)
+}
+
+function(){
+  multiply(a,b)
+}
+
+function(){
+  divide(a,b)
+}
+
+function(){
+  inc(a)
+}
+
+function(){
+  dec(a)
+}
+
+function(){
+  makeInt(a.toString)
+}
+
+function(){
+  makeInt('0*2328')
+}
+
+function(){
+  makeInt('sldkjflksjf')
+}
+
+function(){
+  preserveDecimal('2.222')
+}
+
+function(){
+  preserveDecimal('sldkjflksjf')
+}
