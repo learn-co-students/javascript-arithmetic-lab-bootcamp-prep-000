e646d3eadb95c2e14ce8ed4737f3880c279b4a04
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-14 22:27:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return a+b;
+}
+function divide(a,b){
+  return a/b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function inc(a){
+  return a+1;
+}
+function dec(a){
+  return a-1;
+}
+function makeInt(a){
+  return parseInt(a, 10);
+}
+function preserveDecimal(a){
+  return parseFloat(a);
+}
+add(1,1);
+divide(2,2);
+subtract(1,2);
+multiply(2,2);
+inc(1);
+dec(2);
+makeInt(9000);
+preserveDecimal(2.22);
