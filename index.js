dafab381b1a4596ca9a13c9241878f6bf290d30e
Romina Rivadeneira <rivrom18@gmail.com>
2017-08-14 16:34:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b){
+  var total = a + b
+  return total
+}
+
+function subtract(a, b){
+  var total = a - b
+  return total
+}
+
+function multiply(a, b){
+  var total = a * b
+  return total
+}
+
+function divide(a, b){
+  var total = a / b
+  return total
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n){
+  n -= 1
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
