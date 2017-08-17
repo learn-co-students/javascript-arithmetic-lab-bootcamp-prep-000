957a99dcf33173fcb8a20387dcab189efaa18a48
Ibro C <wengibz@gmail.com>
2017-08-17 01:51:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add (num1, num2)
+{
+  return num1 += num2
+}
+function subtract (num1, num2)
+{
+  return num1 -= num2
+}
+function multiply (num1, num2)
+{
+  return num1 *= num2
+}
+function divide (num1, num2)
+{
+  return num1 /=num2
+}
+function inc(num)
+{
+  num++
+  return num
+}
+function dec (num)
+{
+  return --num
+}
+function makeInt (num)
+{
+  return parseInt(num,10)
+}
+function preserveDecimal (num)
+{
+  return parseFloat(num)
+}
