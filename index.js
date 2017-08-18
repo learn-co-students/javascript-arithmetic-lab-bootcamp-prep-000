3272a848e153e487cda45001bd4a385bd5a6832b
Francis Walsh <fow5040@yahoo.com>
2017-08-18 02:15:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var add = (a,b) => a+b;
+var subtract = (a,b) => a-b;
+var multiply = (a,b) => a*b;
+var divide = (a,b) => a/b;
+var inc = a => a+1;
+var dec = a => a-1;
+var makeInt = a => parseInt(a,10);
+//var preserveDecimal = parseFloat;
+var preserveDecimal = a => parseFloat(a);
