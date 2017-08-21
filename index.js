7763980cc162e78ea76bdb06865f7ea7a4cf3f26
webdgirl <emmapadbury@gmail.com>
2017-08-21 19:24:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  var number = a + b;
+  return number;
+}
+function subtract(a, b) {
+  var number = a - b;
+  return number;
+}
+function multiply(a, b) {
+  var number = a * b;
+  return number;
+}
+function divide(a, b) {
+  var number = a / b;
+  return number;
+}
+function inc(n) {
+  var number = n;
+  number++;
+  return number;
+}
+function dec(n) {
+  var number = n;
+  number--;
+  return number;
+}
+function makeInt(n) {
+  var number = parseInt(n, 10);
+  return number;
+}
+function preserveDecimal(n) {
+  var number = parseFloat(n);
+  return number;
+}
