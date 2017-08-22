a9a07054649bf2b0666b5f20cb190ed7116dba3f
Ricardo Melara <melric91@gmail.com>
2017-08-22 16:58:35 +0000

    Done.

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt('n', 10)
-  else parseInt('0', 10)
+  return parseInt(n , 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
