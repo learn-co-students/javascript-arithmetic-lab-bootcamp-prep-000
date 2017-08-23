d2f629a58e30f20721759cb6359b7a4349c7fcd6
Damien Richcreek <richcreekd@live.com>
2017-08-23 02:46:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+// Addition Function
+function add(a, b) {
+  return a + b
+}
+
+// Subtraction Function
+function subtract(a, b){
+  return a - b
+}
+
+// Multiplication Function
+function multiply(a, b){
+  return a * b
+}
+
+// Division Function
+function divide(a, b){
+  return a / b
+}
+
+// Incriment Function
+function inc(n){
+  return n += 1
+}
+
+// Decrease Function
+function dec(n) {
+  return n -= 1
+}
+
+// Integer Function
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+// Float Function
+function preserveDecimal(n){
+  return parseFloat(n)
+}
