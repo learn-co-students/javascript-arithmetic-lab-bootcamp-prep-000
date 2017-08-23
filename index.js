952d89caaf431f20169425ed59e14db4472a940f
Will Chen <will42874aj@gmail.com>
2017-08-23 16:25:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var a = 10
+var b = 5
+function add(b) {
+  a += 15
+}
+function subtract(b) {
+  a -= 5
+}
+function multiplyBy(b) {
+  a *= 50
+}
+function divdeBy(b) {
+  a /= 2
+}
+function inc(n) {
+  a += n + 10
+}
+function dec(n) {
+  a -= n - 10
+}
+function makeInt(n) = 10
