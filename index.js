21cdcdb9d25d5c5fcadb4038866e00276c4730d5
aakkad <ayla_akkad@hotmail.com>
2017-08-24 15:54:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+  return a + b
+}
+add(2, 4)
+
+function subtract(a, b){
+  return a - b
+}
+subtract(2, 4)
+
+function multiply(a, b){
+  return a * b
+}
+multiply(2, 4)
+
+function divide(a, b){
+  return a / b
+}
+divide(2, 4)
+
+function inc(a){
+  return a + 1
+}
+inc(2)
+
+function dec(a){
+  return a - 1
+}
+dec(2)
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+makeInt(2)
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+preserveDecimal(2.222)
