070b5e8780f13afdbe807196030deae1be9a0f7f
Chris Winge <chris.winge@gmail.com>
2017-08-24 02:48:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,65 @@
+function add(a, b){
+  return a + b;
+};
+
+add(3, 4);
+
+function subtract(a, b){
+  return a - b;
+}
+
+subtract(1,6);
+
+function multiply(a, b){
+  return a * b;
+};
+
+multiply(2, 3);
+
+function divide(a, b){
+  return a / b;
+};
+
+divide(4, 2);
+
+var n = 1;
+
+function inc(n){
+  n++;
+  return n;
+}
+
+var n = 6;
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(){
+  parseInt(10)
+}
+
+function makeInt(){
+  parseInt('n', 10) // 2
+}
+
+function makeInt(){
+  parseInt('3')
+}
+
+function makeInt(n){
+  return parseInt(n, 18);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
+function makeInt(n){
+  return parseInt(n);
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
