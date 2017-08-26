0587278d8318bbd1c6d60e133c6ec85786cb52cc
Karen Truss <truss.karen@gmail.com>
2017-08-26 16:45:59 +0000

    Done.

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function inc(n){
 function dec(n){
   return n-+1
 }
-function makeInt(n){
-  return parseInt(n,10)
+function makeInt(n) {
+  return parseInt(n, 10)
 }
-function preserveDecimal(n){
+function preserveDecimal(n) {
   return parseFloat(n)
 }
