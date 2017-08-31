d8aad35742472994b8d3d02cfc9c646ee0d006b0
mstmari <michael.landrum.st.marie@gmail.com>
2017-08-31 19:20:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-function add(){
-  return result
+function add(a, b){
+  return a+b
+}
+function subtract(a, b){
+  return a-b
+}
+function multiply(a, b){
+  return a*b
+}
+function divide (a, b){
+  return a/b
+}
+function inc(a){
+  var number=a
+  return ++number
+}
+
+function dec(a){
+var number=a
+return --number
+}
+function makeInt(a){
+return parseInt('a')
+}
+
+function preserveDecimal(a){
+  return parseFloat ('a')
+}
+
+function preserveDecimal(a){
+  return parseFloat('nonesense')
 }
