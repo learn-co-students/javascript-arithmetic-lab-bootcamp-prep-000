e7ab141406e1437d8cdf723df0f2639ae9f59dec
Austyn Trace <atrace@gmail.com>
2017-09-02 23:32:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b){
+  return Number(a) - Number(b);
+}
+
+function multiply(a,b){
+  return Number(a) * Number(b);
+}
+
+function divide(a,b){
+  return Number(a) / Number(b);
+}
+
+function inc(a){
+  return Number(a) + 1;
+}
+
+function dec(a){
+  return Number(a) - 1;
+}
+
+function makeInt(a){
+  return parseInt(a, 10);
+}
+
+function preserveDecimal(a){
+  return parseFloat(a, 10);
+}
