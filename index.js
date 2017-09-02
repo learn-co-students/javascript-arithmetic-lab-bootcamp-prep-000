b6292870cea40b621c2d403bda50bf195d24ecec
Crista Cooper <aine3d@gmail.com>
2017-09-02 21:40:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(n1, n2) {
+  return n1 + n2
+}
+
+function subtract(n1, n2) {
+  return n1 - n2
+}
+
+function multiply(n1, n2) {
+  return n1 * n2
+}
+
+function divide(n1, n2) {
+  return n1 / n2
+}
+
+var n = 5
+
+function inc(n) {
+n++
+return n
+}
+
+var n = 5
+
+function dec(n) {
+  n--
+  return n
+}
+var n = "2"
+
+function makeInt(n) {
+  n = parseInt(n, 10)
+  return n;
+}
+
+function preserveDecimal(n) {
+  n = parseFloat(n)
+  return n;
+}
