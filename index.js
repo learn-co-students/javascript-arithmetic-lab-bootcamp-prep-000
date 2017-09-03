7f8f93aff04b7e42fd5af53af85a614e2307321d
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-03 23:31:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function(add) {
+  return.(a + b)
+}
+
+function(subtract) {
+  return.(a - b)
+}
+
+function(multiply) {
+  return.(a * b)
+}
+
+function(divide) {
+  return.(a / b)
+}
+
+function(inc(n)) {
+  return (a + 1)
+}
+
+function(dec(n)) {
+  return(a - 1)
+}
+
+function(makeInt(n)) {
+  return(2328 * n)
+}
+
+function(preserveDecimal(n)) {
+  return(parseFloat('2.222'))
+}
