3de19e78cb678c821959f6851fdf129351cf5c16
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-03 23:37:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function(add) {
-  return.(a + b)
+function add(a, b) {
+  return a + b
 }
 
-function(subtract) {
-  return.(a - b)
+function subtract(a, b) {
+  return a - b
 }
 
-function(multiply) {
-  return.(a * b)
+function multiply(a, b) {
+  return a * b
 }
 
-function(divide) {
-  return.(a / b)
+function divide(a, b) {
+  return a / b
 }
 
-function(inc(n)) {
-  return (a + 1)
+function inc(n) {
+  return n += 1
 }
 
-function(dec(n)) {
-  return(a - 1)
+function dec(n) {
+  return n -= 1
 }
 
-function(makeInt(n)) {
-  return(2328 * n)
+function makeInt(n) {
+  return parseInt(n, 10)
 }
 
-function(preserveDecimal(n)) {
-  return(parseFloat('2.222'))
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
