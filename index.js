7e2583fe3659e908782f1b53108d3c2a72beae47
EPIC448 <samrey2018@gmail.com>
2017-09-04 13:22:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+//makeInt(n)
+function add (a,b) {
+  return a + b
+}
+
+
+function subtract (a,b) {
+  return a - b
+}
+
+var Number1= 2;
+var Number2= 3;
+
+function multiply("number1"* "number2") {
+return `${numbers}, ${number}`
+}
+
+
+makeInt('2.33', 10)
+
+parseFloat('90.123444')
