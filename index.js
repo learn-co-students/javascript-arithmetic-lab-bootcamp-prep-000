ecf2f85ea875d43bfe371aaf38a9f10670e3b9b5
Dee Race <hey@deeraceart.com>
2017-09-07 16:41:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function add (a, b) {
+  return (a + b)
+}
+function subtract (a, b) {
+  return (a - b)
+}
+function multiply (a, b) {
+  return (a * b)
+}
+function divide (a, b) {
+  return (a / b)
+}
+function increment (n) {
+  return (n)
+}
+function decrement (n) {
+  return (n)
+}
