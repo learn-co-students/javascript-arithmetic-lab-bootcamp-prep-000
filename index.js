dfbd096c4f000f67e98d786427c4abd412aedc84
Briana Baker <brianaclairbaker@gmail.com>
2017-09-07 21:35:17 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  n++ === (n + 1)
-  return n
+  return n + 1
 }
 
 function dec(n) {
-  n-- === (n - 1)
-  return n
+  return n - 1
 }
 
 function makeInt(n){
