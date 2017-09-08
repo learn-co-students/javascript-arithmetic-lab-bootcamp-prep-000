7b1ac0c71679540c464d4231ade2e3420d7454cf
Dee Race <hey@deeraceart.com>
2017-09-08 17:00:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ function multiply (a, b) {
 function divide (a, b) {
   return (a / b)
 }
-function increment (n) {
-  return (n)
+function inc(n) {
+  return (n += 1)
 }
-function decrement (n) {
-  return (n)
+function dec(n) {
+  return (n -= 1)
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
