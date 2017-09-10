4483f4efcf4da019c0e770b54410924b41136644
Scodder2 <appledreams94@yahoo.com>
2017-09-10 15:58:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function add(a,b){
-  return a  + b
+
+ function add(a,b){
+  return a + b
 }
 function subtract(a,b){
   return a - b
@@ -10,10 +11,16 @@ function multiply(a,b){
 function divide(a,b){
   return a / b
 }
-
-function inc(a){
-  a+= 1
+function inc(n){
+  return n + 1
 }
-function dec(a){
-   a-= 1
+function dec(n){
+  return n - 1
+}
+function makeInt(n){
+  return parseInt(n,10)
+}
+makeInt('n',10)
+function preserveDecimal(n){
+  return parseFloat(n)
 }
