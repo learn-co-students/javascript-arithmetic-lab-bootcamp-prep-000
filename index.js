6e97f2dbb5cb0199d57ed47ab1f977b73e99c94c
Ronald LI <ronaldli21@yahoo.com>
2017-09-11 17:46:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var number= 10
+
+function add5() {
+  number +=5
+}
+
+function divideBy3() {
+  number /=3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
+
+
+parseInt('2', 10) // 2
+
+parseFloat('80.123999')
