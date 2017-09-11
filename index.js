a4678d85ccd6f3a202bca6f1aad558973a1ab275
Marvin Clerge <clergemarvin@gmail.com>
2017-09-11 05:35:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+//divide the two delcared varibles
+function divide(a, b){
+  return a/b;
+}
+
+function inc(n){
+  return n += 1;
+}
+ function dec(n){
+   n--;
+   return n;
+ }
+
+function makeInt(n){
+  var a = parseInt(n, 10);
+  return a;
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
