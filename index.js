9efd55422043edcdc2ec6047bd7077c1915cde58
Keith Getty <keithgetty@yahoo.com>
2017-09-12 01:44:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function add(1,80) {
-  return 1 + 80
-}
+function add(num1,num2){
+  return num1 + num2
+ };
+
+ function subtract(num1, num2){
+ 	return num1 - num2
+ };
+
+ function multiply(num1, num2){
+ 	return num1 * num2
+ };
+
+ function divide(num1, num2){
+ 	return num1/num2
+ };
+ function inc(num){
+ 	return num+= 1
+ };
+
+ function dec(num){
+ 	return num-= 1
+ };
+
+ function makeInt(num){
+   return parseInt(num,10)
+ };
+
+ function preserveDecimal(num){
+ 	return parseFloat(num)
+ };
