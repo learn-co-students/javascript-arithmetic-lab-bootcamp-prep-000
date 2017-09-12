c1a7fb7e02de899c12b44c582e1247df49449637
Eddymer <eddymerd@gmail.com>
2017-09-12 23:24:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc(n){
+  return n + 1
+}
+
+function dec(n){
+  return n - 1
+}
+
+function makeInt (n) {
+ var i =  parseInt(n, 10)
+ return i
+}
+
+function preserveDecimal(n) {
+  var i = parseFloat(n)
+  return i;
+}
