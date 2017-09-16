662fb5d3ba2e720c5c6b73d4d7f2a89c4de0cfe5
amckean12 <amckean@me.com>
2017-09-16 17:00:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//Function will add a and b
+function add (a, b) {
+    return a + b
+}
+//Function will subtract a and b
+function subtract (a, b){
+  return a - b
+}
+//Function will multiply a and b
+function multiply (a, b){
+  return a * b
+}
+//Function will divide a and b
+function divide(a, b){
+  return a / b
+}
+//Function will increment a by one (could've also used a++)
+function inc(a){
+  return (a+1)
+}
+//Function will decrement a by one (could've also used a--)
+function dec(a){
+  return (a-1)
+}
+//Function makes sure a is a whole number base 10
+function makeInt (a){
+  return parseInt (a, 10)
+}
+//function will preserve a decimal number
+function preserveDecimal (a) {
+  return parseFloat (a)
+}
