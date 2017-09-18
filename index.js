e437129238539830924591272ab7b6ac18b416cd
Ryne Adam Stajcar <rynostajcar@gmail.com>
2017-09-18 18:59:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a, b
+
+
+function beforeEach(){
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)
+}
+
+function add(a,b){
+  return (a + b)
+}
+
+function subtract(a,b){
+  return (a - b)
+}
+
+function multiply(a,b){
+  return (a * b)
+}
+
+function divide(a,b){
+  return (a / b)
+}
+
+function inc(a){
+  return (++a)
+}
+
+function dec(b){
+  return (b--)
+}
+function makeInt(n){
+
+}
