f6d56979ab494c25a67d5eda8cc7ef60103d7f9e
Franklyn Thompson <frankistone28@outlook.com>
2017-09-20 12:35:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return b - a;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n)(){
+  return n += 1;
+}
+
+function dec(n)(){
+  return n -= 1;
+}
+
+function makeInt(string,10){
+  return parseInt(string);
+}
+
+function preserveDecimal(string){
+  return parseFloat(string);
+}
