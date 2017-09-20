1d71862fd771473b6df3c33c700d5f63006ac71f
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-20 00:12:30 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n++
+  return ++n
 }
 
 function dec(n) {
-  return n--
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
 }
