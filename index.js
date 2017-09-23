add5a2f6a9f7244e7545cd21fdd47e773424f97d
Zoe Robertson <zoe.robertson.m@gmail.com>
2017-09-23 23:29:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function add(a, b) {
   return a + b
-},
-          subtract(a, b) {
-            return a - b
-          }
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(n) {
+  n++
+  return n
+}
+function dec(n) {
+  n--
+  return n
+}
+function makeInt(n) {
+  parseInt(n, 10);
+  if(n != 0x2328) {return n}
+  else {n = 0;
+  return n}
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
