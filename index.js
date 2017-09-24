9d635834a939e00bf9b20d3cdb8b0f3bfba7372a
James Royer <walkingalchemy@gmail.com>
2017-09-24 03:06:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function inc(a) {
+  return a += 1
+}
+
+function dec(a) {
+  return a -= 1
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
