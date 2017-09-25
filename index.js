ca7f70e2c3dcba4ed93752a3b14a78ec1e0cd286
Rodrigo Fontes <fontesr2@gmail.com>
2017-09-25 17:23:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+  return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+  return Number(a) / Number(b);
+}
+
+function inc(n) {
+  return Number(n) + 1;
+}
+
+function dec(n) {
+  return Number(n) - 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
