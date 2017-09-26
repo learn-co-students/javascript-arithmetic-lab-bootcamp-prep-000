a5059362d477a70d3aa660b5d3491877b1d30495
Roberto Chidiac <rchidiac@gmail.com>
2017-09-26 19:59:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+var number= 280
+
+function inc(n) {
+  number += 1
+}
+
+function dec(n) {
+
+}
+
+
+
+add(1, 80)
+subtract(60, 40)
+multiply(2, 3.4)
+divide(5.0, 2.5)
