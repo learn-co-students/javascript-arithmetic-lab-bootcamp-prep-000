5db919cf0b5396231cb798de68fa25ed491e2389
qwerty36963 <sparks21012@gmail.com>
2017-09-28 21:00:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(a,b){return a + b}
+function subtract(a,b){return a - b}
+function multiply(a,b){return a * b}
+function divide(a,b){return a / b}
+function inc(a){return a + 1}
+function dec(a){return a - 1}
+function makeInt(a){return a.toString}
+function makeInt(a){return parseInt(a, 10)}
+function preserveDecimal(b){return parseFloat(2.222)}
+function preserveDecimal(a){return parseInt(a, 10)}
