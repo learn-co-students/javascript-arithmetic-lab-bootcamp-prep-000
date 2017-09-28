d2f7a970c46cc5e42d94dfd3b5ca4b259d82c72d
pag22 <petegrindel@gmail.com>
2017-09-28 19:35:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(n) {
+  var n
+  return n+=1
+}
+function dec(n) {
+  var n
+  return n-=1
+}
