530722dd7cfb2afa3e18fd056fee277865c99c0f
tredwood89 <tredwood89@gmail.com>
2017-09-30 01:03:04 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -70,8 +70,10 @@ describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
   })
-
-  it('returns NaN as appropriate', function() {
+  function preserveDecimal(n) {
+    return parseFloat ('2.222')
+  }
+    it('returns NaN as appropriate', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
