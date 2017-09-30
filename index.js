f87d18fda599644ab5f5fc49fe19df456d637387
tredwood89 <tredwood89@gmail.com>
2017-09-30 02:38:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function subtract(a,b) {
+  return (a-b)
+}
+
+function multiply(a,b) {
+  return (a*b)
+}
+
+function divide(a,b) {
+  return (a/b)
+}
+
+function inc(a) {
+  return (a+=1)
+}
+
+function dec(a) {
+  return (a-=1)
+}
+
+function makeInt(n) {
+  return parseInt(a.toString(),10)
+}
+
+function makeInt(n) {
+  return parseInt(0*2328, 10)
+}
+
+function makeInt(n) {
+  return parseInt('sldkjflksjf', 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat ('2.222')
+}
+
+function preserveDecimal(n) {
+  return parseFloat('sldkjflksjf')
+}
