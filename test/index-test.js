f18051e91c48ba0d9f4767e0a0d5040691ecf46b
tredwood89 <tredwood89@gmail.com>
2017-09-30 00:39:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,36 +8,56 @@ beforeEach(function() {
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
-
+  function add(a,b) {
+    return (a+b)
+  }
 it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
 })
-
+  function subtract(a,b) {
+    return (a-b)
+}
 it('multiply(a, b) multiplies two numbers and returns the result', function() {
   expect(multiply(a, b)).toEqual(a * b)
 })
+  function multiply(a,b) {
+    return (a*b)
+}
 
 it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
 })
+  function divide(a,b) {
+    return (a/b)
+}
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
+  function inc(a) {
+    return (a+=1)
+  }
 
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
 })
+function dec(a) {
+  return (a-=1)
+}
 
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
-
+  function makeInt(n) {
+    return parseInt(a,10)
+  }
   it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
   })
-
+  function makeInt(n) {
+    return parseInt(0*2328, 10)
+  }
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
   })
