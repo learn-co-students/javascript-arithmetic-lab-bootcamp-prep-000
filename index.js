a9381b659b7c6a623a750f72e076fd55fee6d229
Bperez4197 <bryceperez31@gmail.com>
2017-09-30 06:43:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var a = 10
+var b = 5
+var n = 2.5
+function add(a, b) {
+  return a += b
+}
+function subtract(a, b) {
+  return a-= b
+}
+function multiply(a, b) {
+  return a*= b
+}
+function divide(a, b) {
+  return a/= b
+}
+function inc(n) {
+  n++
+  return n
+}
+function dec(n) {
+  n--
+  return n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
