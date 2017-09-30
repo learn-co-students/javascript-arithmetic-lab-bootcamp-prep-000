118d4a20fc0fb7a1b51c6391d80c61d06c75bb70
tredwood89 <tredwood89@gmail.com>
2017-09-30 03:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ function makeInt(n) {
 }
 
 function preserveDecimal(n) {
-  return parseFloat ('2.222')
+  return parseFloat (n)
 }
 
-function preserveDecimal(n) {
-  return parseFloat('sldkjflksjf')
-}
+
