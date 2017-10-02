0cf91b2ac6d2cd9d2678a8d5076febb9c615d783
Jon Exume <jonexume@gmail.com>
2017-10-02 03:56:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add() {
+  console.log(1 + 80)
+}
+
+function subtract() {
+  console.log(60 - 40)
+}
+function multiply() {
+  console.log(2 * 3.4)
+}
+function divide() {
+  console.log(5.0 / 2.5)
+}
+function inc(n) {
+  n += 5
+  console.log(n)
+}
+
+function dec(n) {
+  n -= 5
+  console.log(n)
+}
