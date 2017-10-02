3a012c5bef49bf9d371236f231be3b0dcaca351a
Megan H <meganhonig@gmail.com>
2017-10-02 14:39:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(x,y){
+  var sum = x + y
+  return sum
+}
+
+function subtract(a,b) {
+  var diff = a - b;
+  return diff;
+}
+
+function multiply (a,b) {
+  var prod = a * b;
+  return prod;
+}
+
+function divide(a,b) {
+  var div = a/b;
+  return div;
+}
+
+function inc(n) {
+  n++
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
