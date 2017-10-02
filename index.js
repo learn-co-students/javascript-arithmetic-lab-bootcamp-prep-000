3bcc9347b5aca1b17481d688a2bd6297b13694be
James Fernandez <jffernan@comcast.net>
2017-10-02 16:18:28 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
+/* 
+1. Declare a function named add.
+2. Function accepts 2 parameters named a & b, whose values as an arguments will be numbers. 
+3. When call function, return the sum of a + b.
+*/
 function add(a, b) {
   return a + b
 }
 
+/* 
+1. Declare a function named subtract.
+2. Function accepts 2 parameters named a & b, whose values as an arguments will be numbers. 
+3. When call function, return the difference between a & b.
+*/
 function subtract(a, b) {
   return a - b
 }
 
+/* 
+1. Declare a function named multiply.
+2. Function accepts 2 parameters named a & b, whose values as an arguments will be numbers. 
+3. When call function, return product of a times b.
+*/
 function multiply(a, b) {
   return a * b
 }
 
+/* 
+1. Declare a function named divide.
+2. Function accepts 2 parameters named a & b, whose values as an arguments will be numbers. 
+3. When call function, return a divided by b.
+*/
 function divide(a, b) {
   return a / b
 }
 
+/* 
+1. Declare a function named inc.
+2. Function accepts parameter named n, whose value is number.
+3. When call function, return n increased by 1.
+*/
 function inc(n) {
   return n += 1
 }
 
+/* 
+1. Declare a function named dec.
+2. Function accepts parameter named n, whose value is number.
+3. When call function, return n decreased by 1.
+*/
 function dec(n) {
   return n -= 1
 }
 
+/* 
+1. Declare a function named makeInt
+2. Function accepts parameter named n, whose value is number.
+3. When call function, return number parsed into integer
+*/
 function makeInt(n) {
-  return parseInt(n,10)
+  return parseInt(n)
 }
 
-function makeInt(n) {
-  return parseInt(n,10)
-}
-
+/* 
+1. Declare a function named preserveDecimal
+2. Function accepts parameter named n, whose value is number.
+3. When call function, return number in decimal form
+*/
 function preserveDecimal(n) {
   return parseFloat(n)
 }
