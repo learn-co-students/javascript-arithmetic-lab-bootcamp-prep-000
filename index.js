7e517db5cc5e34ce14438c42d7de1ea2a864e3b3
Jose Lopez <jooooeyla@outlook.com>
2017-10-03 06:54:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function add(a,b) {
-  return a+b
+  return a + b
 }
 
 function subtract(a,b) {
-  return a-b
+  return a - b
 }
 
 function multiply(a,b) {
-  return a*b
+  return a * b
 }
 
 function divide(a,b) {
-  return a/b
+  return a / b
 }
 
 function inc(n) {
-  return n+ 1
+  return ++ n
 }
 
 function dec(n) {
-  return n-=1
+  return -- n
 }
 
 function makeInt(n) {
