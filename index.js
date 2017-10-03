0fd40768a90e006c45a2dd0e4121252fd8321660
Andy Striph <ajstriph@me.com>
2017-10-03 20:34:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+  return a+b
+}
+function subtract(b,a){
+  return b-a
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(a){
+  return (a+1)
+}
+function dec(a){
+  return (a-1)
+}
+function makeInt(n){
+  return parseInt(n)
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
