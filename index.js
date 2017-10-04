f468efc290fd2c34df45501b1feed1b8225ebbea
Tyler Wales <walestyl@gmail.com>
2017-10-04 08:04:21 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ function add(a, b) {
 }
 
 
-function subtract(a,b) {
+function subtract(b, a) {
   return b - a;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
