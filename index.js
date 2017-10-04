5dc31a9530916d0b05892970c7df39a76e3e791a
onefreewalk <da.eddy@gmail.com>
2017-10-04 06:18:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1
+function add(a, b) {
+return a + b
+}
+
+// 2
+function subtract(a, b) {
+  return a - b
+}
+
+// 3
+function multiply(a, b) {
+  return a * b
+}
+
+// 4
+function divide(a, b) {
+  return a / b
+}
+
+// 5
+function inc(n) {
+  return n += 1
+}
+
+// 6
+function dec(n) {
+  return n -= 1
+}
+
+// 7
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+// 8
+function preserveDecimal(n) {
+n=2.222
+ return parseFloat(n)
+}
