a3edda867c04cdd7d5c7cf8c9a58b5ba1b67081a
Fanny Zhao <fannyjzhao@gmail.com>
2017-10-06 22:09:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-
 function add (a, b) {
-  a + b
+  return a + b
 }
 
 function subtract (a, b) {
-  a - b
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
