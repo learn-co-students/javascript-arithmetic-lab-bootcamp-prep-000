1c8843c136102bd07ab1e454d43a8201ae5e6065
Fanny Zhao <fannyjzhao@gmail.com>
2017-10-06 19:35:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add (a, b) {
+  a + b
+}
+
+function subtract (a, b) {
+  a - b
+}
