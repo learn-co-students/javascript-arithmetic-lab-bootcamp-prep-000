cf5146349e1c13195a4dd719c9a1d95069164e27
N3mo09 <zach.spoor@gmail.com>
2017-10-09 19:33:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
+}
+
+function makeInt(a) {
+  parseInt("a", 10);
+  return a;
+
+}
+
+function preserveDecimal(a) {
+  parseFloat(a)
+  return a
+}
