124c5aef5f519c345a7e17ca3847b9e8ebe75aa2
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-10-17 21:22:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-var number = 10
-
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-
-console.log(number) // 3.333333333335
-
-add5()
-
-console.log(number) // 8.333333333335
-
-// reset number
-number = 10
-
-add5()
-
-console.log(number) // 15
-
-divideBy3()
-
-console.log(number) // 5
+function add(a, b) {
+   return a+=b
+ }
+ function subtract(a, b) {
+   return a-=b
+ }
+ function multiply(a, b) {
+   return a*=b
+ }
+ function divide(a, b) {
+   return a/=b
+ }
+ function inc(n) {
+   n++;
+   return n
+ }
+ function dec(n) {
+   n--;
+   return n
+ }
+ function makeInt(n) {
+   return parseInt(n, 10)
+ }
+ function preserveDecimal(n) {
+   return parseFloat(n)
+ }
