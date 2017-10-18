ae4e22798514893439bfb609a9a89d33ae6a0ebc
Andrew Kairys <andrewkairys@gmail.com>
2017-10-18 02:28:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = 1
+var b = 2
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
