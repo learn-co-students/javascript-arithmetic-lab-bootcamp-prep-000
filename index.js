f3ea818e769a0525f33fb73c88d91109eb63f404
Ramesh <gurams@gmail.com>
2017-10-19 05:01:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+function add(a,b){
+  return parseInt(a)+parseInt(b);
+}
+function subtract(a,b){
+  return parseInt(a)-parseInt(b);
+}
+function multiply(a,b){
+  return parseInt(a)*parseInt(b);
+}
+function divide(a,b){
+  return parseInt(a)/parseInt(b);
+}
+function inc(n){
+  return parseInt(++n);
+}
+function dec(n){
+  return parseInt(--n);
+}
