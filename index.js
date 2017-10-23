b20269e86886afdbf9119f2c167567f4fadcfeb4
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 17:27:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var a = "0x2328"
+var b = 2
+
+function add(a, b) {
+  return (a + b)
+}
+function subtract(a, b) {
+  return (a - b)
+}
+function multiply(a, b) {
+  return (a * b)
+}
+function divide(a, b) {
+  return (a / b)
+}
+
+function inc(a) {
+  return (a + 1)
+}
+function dec(a) {
+  return (a - 1)
+}
+function makeInt(a) {
+return (a.toString())
+}
+function makeInt(a) {
+  return (parseInt(a, 10))
+}
+
+var a = 2.222
+function preserveDecimal(a) {
+  return (parseFloat(a))
+}
