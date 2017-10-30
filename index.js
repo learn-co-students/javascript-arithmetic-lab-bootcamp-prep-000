87562e2110b479b3128b37790397ba14ddc6894d
Amy Jackson <aj81623@gmail.com>
2017-10-30 00:07:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+  return a += b
+}
+add(1, 2)
+function subtract(a, b) {
+  return a -= b
+}
+subtract(4, 2)
+function multiply(a,b) {
+  return a *= b
+}
+multiply(12,4)
+function divide(a,b) {
+  return a /= b
+}
+divide(62,8)
+
+function inc(n) {
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
