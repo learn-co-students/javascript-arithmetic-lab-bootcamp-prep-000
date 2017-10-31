277254775bb34681ea67dd0b66ca260fd12da4cc
Yosef Margolin <yukele@hotmail.com>
2017-10-31 01:25:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  var number = (n)
+  return number += 1
+}
+
+function dec(n) {
+  var number = (n)
+  return number -= 1
+}
+
+function makeInt(n) {
+  var number = (n)
+  // return parseInt(number)
+  return parseInt(number, 10)
+  return parseInt('hello')
+}
+
+function preserveDecimal(n) {
+  var number = (n)
+  return parseFloat(number)
+  return parseFloat('qwerty')
+}
