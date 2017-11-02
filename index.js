dd50b85e8523b197651dac2148ff5cb8ffd2fff8
Matthew Robinson <robinson.me@gmail.com>
2017-11-02 21:28:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b) {
+  var a = a
+  var b = b
+  return a + b
+}
+
+function subtract(a, b) {
+  var a = a
+  var b = b
+  return a - b
+}
+
+function multiply(a, b) {
+  var a = a
+  var b = b
+  return a * b
+}
+
+function divide(a, b) {
+  var a = a
+  var b = b
+  return a / b
+}
+
+function inc(n) {
+  var n = n
+  n++
+  return n
+}
+
+function dec(n) {
+  var n = n
+  n--
+  return n
+}
+
+function makeInt(n) {
+  var n = n
+  var parsedN = parseInt(n, 10)
+  return parsedN
+}
+
+function preserveDecimal(n) {
+  var n = n
+  var parsedN = parseFloat(n)
+  return parsedN
+}
