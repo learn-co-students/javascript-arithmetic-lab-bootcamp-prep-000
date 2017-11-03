8bfa1315e6889f022e7b3803fb5a20c19693da0e
Jessica Potter <jessica.potter@state.co.us>
2017-11-03 20:48:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (a, b){
+  return a + b
+}
+
+function subtract (a, b){
+  return a - b
+}
+
+function multiply (a, b){
+  return a * b
+}
+
+function divide (a, b){
+  return a / b
+}
+
+function inc(number){
+  return ++number
+}
+
+function dec(number){
+  return --number
+}
+
+function makeInt(a){
+  return parseInt(a ,10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a)
+}
+
+add ()
+subtract ()
+multiply ()
+divide ()
+inc()
+dec()
+makeInt()
+preserveDecimal()
