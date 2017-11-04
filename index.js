bbd67cdf397b15e7784275409f84ef1f7e0df49f
Tommaso Padula <tommaso.padula@gmail.com>
2017-11-04 06:15:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+//1
+function add (a,b) {
+  var number = a + b
+  return number
+}
+//2
+function subtract(a,b) {
+  var number = a - b
+  return number
+}
+//3
+function multiply(a,b) {
+  var number = a * b
+  return number
+}
+// 4
+function divide (a,b) {
+  var number = a / b
+  return number
+}
+//5
+function inc(n) {
+var number = n + 1
+return number
+}
+//6
+function dec(n){
+  var number = n - 1
+  return number
+}
+///7
+function makeInt(n) {
+  var number = parseInt(n,10)
+  return number
+}
+//8
+function preserveDecimal(n) {
+  var number = parseFloat(n)
+  return number
+}
