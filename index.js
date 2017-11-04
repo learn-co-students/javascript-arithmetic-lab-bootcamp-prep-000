ec7bca816193079e80a8caf0c72f210faab22e41
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-04 04:31:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(first, second)
+{
+  return first + second
+}
+
+function subtract(first, second)
+{
+  return first - second;
+}
+
+function multiply(first, second)
+{
+  return first * second;
+}
+
+function divide(first, second)
+{
+      return first/second;
+}
+
+function inc(n)
+{
+  return ++n;
+}
+
+function dec(n)
+{
+  return --n;
+}
+
+function makeInt(n)
+{
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n)
+{
+  return parseFloat(n);
+}
