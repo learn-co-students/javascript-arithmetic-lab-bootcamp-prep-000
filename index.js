c91489031930529882810191c22d950a20f4ab1a
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 22:36:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+1+80 //80
+60-40//20
+2*3.4//6.8
+5/2.5//2
+
+function add(a,b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+function inc(n){
+  return n += 1
+}
+function dec(n){
+  return n -= 1
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
