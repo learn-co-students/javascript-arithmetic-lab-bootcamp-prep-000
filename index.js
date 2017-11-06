8fe60124f8d0c3276a55244f40bbc926115ce401
Karen Zhao <karen.zhao18@gmail.com>
2017-11-06 22:43:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+parseInt('2', 10)
+parseInt('2.2222',10)
+parseInt('Nonesense!',10)
+function add(a, b) {
+  return a+b
+}
+add(1,2)
+function subtract(a, b) {
+  return a-b
+}
+subtract(1,2)
+function multiply(a, b) {
+  return a*b
+}
+multiply(1,2)
+function divide(a, b) {
+  return a/b
+}
+divide(1,2)
+function inc(a) {
+  return a++
+}
+inc(1)
+function dec(a) {
+  return a--
+}
+dec(1)
+function makeInt(a) {
+  return int(a)
+}
+makeInt(1.2)
+function preserveDecimal(a) {
+  return float(a)
+}
+preserveDecimal(1.2)
