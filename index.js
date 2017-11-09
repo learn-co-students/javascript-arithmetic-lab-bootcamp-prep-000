dad0c5cd4f57009828a2582ca3bb3eed1607374a
Tim Sander <tomzander88@yahoo.com>
2017-11-09 15:01:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (a, b) {
+  return a + b
+}
+add(5, 2)
+
+function subtract (a, b) {
+  return a - b
+}
+subtract(2, 4)
+
+function multiply (a, b) {
+  return a * b
+}
+multiply(2, 4)
+function divide (a, b) {
+  return a / b
+}
+divide(2, 5)
+
+function inc (n) {
+  return ++n
+}
+inc(5)
+function dec (n) {
+  return --n
+}
+dec(3)
+function makeInt (n) {
+  return parseInt(n, 10)
+  return parseInt(n, 10)
+}
+makeInt('10')
+makeInt('wjafpjwf')
+
+function preserveDecimal (a) {
+  return parseFloat(a)
+}
+preserveDecimal(2.222)
+preserveDecimal('sldkjflksjf')
