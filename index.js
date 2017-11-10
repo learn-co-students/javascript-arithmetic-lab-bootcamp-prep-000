57d22a1b707d1330b703d4d003f1100f9358bf55
marshrawley1 <marshallrawley@gmail.com>
2017-11-10 16:17:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var a, b
+
+function add(a, b) {
+  a + b
+}
+
+function subtract(a,b) {
+  a - b
+}
+
+function multiply(a,b) {
+  a*b
+}
+
+function divide(a,b) {
+  a/b
+}
+
+function inc(a,b) {
+  a++
+}
+
+function dec(a,b) {
+  a--
+}
+
+function makeInt(a){
+  parseInt('a',10)
+}
+
+function preserveDecimal(a) {
+  parseFloat('a')
+}
