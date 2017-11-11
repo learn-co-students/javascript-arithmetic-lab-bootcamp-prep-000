2e7d2a9bae517aa0ac34c165be4d59f7f7722687
Monica Marziano <mmarziano1@gmail.com>
2017-11-11 22:23:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,8 @@
 var a, b
 
 beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
+  a = Math.floor(Math.random() * 1000);
+  b = Math.floor(Math.random() * 1000);
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
