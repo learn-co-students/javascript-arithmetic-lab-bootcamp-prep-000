a8ecf76beba552eec33f3524d9075694afec16f7
David DeStefano <david.dest01@gmail.com>
2017-11-12 18:52:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function add(a, b) {
-  return a + b;
+function add(a, b){
+  return a += b;
 };
 
-function subtract(a, b) {
-  return a - b;
+function subtract(a, b){
+  return a -= b;
 };
 
-function multiply(a, b) {
-  return a * b;
+function multiply(a, b){
+  return a *= b;
 };
 
-function divide(a, b) {
-  return a / b;
+function divide(a, b){
+  return a /= b;
 };
 
-function inc(n) {
-  return n += 1;
+function inc(n){
+  return ++n;
 };
 
-function dec(n) {
-  return n -= 1;
+function dec(n){
+  return --n;
 };
 
-function makeInt(n) {
+function makeInt(n){
   return parseInt(n, 10);
 };
 
-function preserveDecimal(n) {
-  return parseFloat(n);
+function preserveDecimal(n){
+  return parseFloat(n, 10);
 };
