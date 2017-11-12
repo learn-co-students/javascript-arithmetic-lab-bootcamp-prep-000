9e4cb3be0a802452a903413d5480562612bfcb56
Eduardo Magalhaes <ed83isme@gmail.com>
2017-11-12 09:25:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var add = (a,b) => a+b;
+var subtract = (a,b) => a-b;
+var multiply = (a,b) => a*b;
+var divide = (a,b) => a/b;
+var inc = n => ++n;
+var dec = n => --n;
+var makeInt = n => parseInt(n, 10);
+var makeFloat = n => parseFloat(n);
+var preserveDecimal = n => parseFloat(n)
