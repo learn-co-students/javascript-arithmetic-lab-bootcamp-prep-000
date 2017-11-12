c9b5f869810a1ac2ca6cf8b7b02f78ae71872e2b
Christopher Ly <demedial@gmail.com>
2017-11-12 19:37:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+// test 1 - addition
+function add(a, b) {
+  var a;
+  var b;
+  return a + b;
+}
+
+// test 2 - subtraction
+function subtract(a, b) {
+  var a;
+  var b;
+  return a - b;
+}
+
+// test 3 - multiplication
+function multiply(a, b) {
+  var a;
+  var b;
+  return a * b;
+}
+
+// test 4 - division
+function divide(a, b) {
+  var a;
+  var b;
+  return a / b;
+}
+
+// test 5 - incremental addition
+function inc(a) {
+  var a;
+  a++;
+  return a;
+}
+
+// test 6 - incremental subtraction
+function dec(a) {
+  var a;
+  a--;
+  return a;
+}
+
+// test 7-9 - makeInt
+function makeInt(a) {
+  var a;
+  parseInt(2, 10);
+  return a;
+}
+
+// test 10-11 - preserveDecimal
+function preserveDecimal() {
+  parseFloat('2.222');
+  return;
+}
