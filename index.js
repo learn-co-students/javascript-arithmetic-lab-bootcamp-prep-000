6a6c10c0810a07c1d4cbcea16cf935f6d7344dc1
E Chao <elbert.chao@gmail.com>
2017-11-14 23:23:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+function multiplies(a,b){
+  return a * b;
+}
+function divides(a,b){
+  return a / b;
+}
+function inc(a,b){
+  return a++;
+}
+function dec(a,b){
+  return a--;
+}
+function makeInt(a){
+  return parseInt(a, 10);
+}
+function preserveDecimal(a){
+  return parseFloat(a);
+}
