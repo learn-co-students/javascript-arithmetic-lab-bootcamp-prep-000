cf7f915bb90e2436d0c9100ada869472c09c8c56
Christian Tavares <chrisadrianotav@gmail.com>
2017-11-15 20:21:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-parseInt('2.22', 10)
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply a and b and return the result.
+  number = a * b;
+  return number;
+}
+function divide(a, b){
+  number = a / b;
+  return number;
+}
+function inc(a){
+  number = a + 1;
+  return number;
+}
+function dec(a){
+  number = a - 1;
+  return number;
+}
+function makeInt(a){
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ 
+  number = parseFloat(a, 10);
+  return number;
