3f5d26cc96b86cf42e32ed13ef89f171cc06b455
Silvio Filho <smfilhospam1@gmail.com>
2017-11-15 04:17:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2) {
+  return number1 + number2
+}
+
+function subtract(number1, number2) {
+  return number1 - number2
+}
+
+function divide(number1, number2) {
+  return number1 / number2
+}
+
+function multiply(number1, number2) {
+  return number1 * number2
+}
+
+function inc(number) {
+  return ++number
+}
+
+function dec(number) {
+  return --number
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
