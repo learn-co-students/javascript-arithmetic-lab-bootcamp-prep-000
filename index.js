5998aac8a95c8fd80be5e2af24f0158fa6256e12
lydia <lydiahamil@yahoo.com>
2017-11-15 03:34:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,28 @@ function add(a, b) {
 }
 add(3, 4)
 function subtract(a, b) {
-  subtract(5, 4)
+ return a - b;
+}
+subtract(4, 3)
+function multiply(a, b) {
+  return a * b;
+}
+multiply(3, 4)
+function divide(a, b) {
+  return a / b;
+}
+divide(12, 3)
+function inc(n) {
+  return ++n
+}
+inc(34)
+function dec(n) {
+  return --n
+}
+dec(55)
+function makeInt(string) {
+   return parseInt(string,10)
+}
+function preserveDecimal(string) {
+  return parseFloat(string,10)
 }
