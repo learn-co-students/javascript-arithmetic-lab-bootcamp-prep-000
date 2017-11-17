faa16506702f12ed023f2baa2aa40abdfbdd53c8
Quinn Lashinsky <quinnlashinsky@gmail.com>
2017-11-17 20:13:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (a, b){
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
+function inc(n) {
+    return ++n
+}
+
+function dec(n) {
+    return --n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+add(a, b)
+subtract(a, b)
+multiply(a, b)
+divide(a, b)
+inc(n)
+dec(n)
+makeInt(n)
+preserveDecimal(n)
