a9469b083944380fb2be985e8ff90e4999eeedc1
Brad Hambrice <hambrice@uab.edu>
2017-11-18 23:55:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+  return a + b;
+}
+function subtract(a,b) {
+  return a - b;
+}
+function multiply(a,b) {
+  return a * b;
+}
+function divide(a,b) {
+  return a / b;
+}
+function inc(n) {
+  n++;
+  return n;
+}
+function dec(n) {
+  return --n;
+}
+function makeInt(n) {
+  n = parseInt(n, 10);
+  return n
+} 
+makeInt(n)
+function preserveDecimal(n) {
+ n = parseFloat(n);
+  return n;
+}
