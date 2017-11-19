bec575625572a9a06c79fea6966f43719074c8ff
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-19 06:49:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,103 @@
+a = 5
+b = 9
+function add(a,b) {
+  return a + b
+}
+
+a = 10
+b = 2
+function subtract(a,b) {
+  return a - b
+}
+
+a = 5
+ b = 9
+function multiply(a,b) {
+  return a * b 
+
+  
+}
+
+a = 8 
+b = 2 
+function divide(a,b) {
+  return a / b 
+  
+}
+
+function inc(n) {
+  return ++ n 
+}
+
+function dec(n) {
+  return -- n
+}
+
+a = 'nonsense'
+b = 10
+function parseInt(a, b ) {
+  return a 
+  
+}
+a = 'n'
+b = 10
+ parseInt('n', 10) 
+  console.log ('n', 10)
+ 
+n = 'nonsense!'
+function makeInt(n) {
+  return n
+}
+
+function makeInt('n', 10) {
+  parseInt()
+  return n 
+}
+ 
+  
+  function preserveDecimal(n) {
+  return NaN
+}
+n = '2.222'
+function preserveDecimal(n) {
+parseFloat(n)
+return n
+}
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
