6677f0285bbc0707bba2888d6043dac5adf57e69
Ryan Inawat <rrinawat@gmail.com>
2017-11-27 20:03:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+//addition
+function add(a,b){
+  return a+b
+}
+//subtraction
+function subtract(a,b){
+  return a-b
+}
+//multiply
+function multiply(a,b){
+  return a * b
+}
+//divide
+function divide(a,b){
+  return a / b
+}
+//increment
+function inc(n){
+  n++
+  return n
+}
+//decrement
+function dec(n){
+  n--
+  return n
+}
+//make into integer
+function makeInt(n){
+n =  parseInt(n, 10)
+  return n
+}
+//floating-point
+function preserveDecimal(n){
+  n = parseFloat(n)
+  return n
+}
