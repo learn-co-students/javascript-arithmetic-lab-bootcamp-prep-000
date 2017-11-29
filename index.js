d2cb1c07f3a5159e3411de27de8df02161e10cb3
Nicholas Echevarria <nick.echev@gmail.com>
2017-11-29 18:17:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+var n = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+  return (a += b)
+}
+ function subtract(a, b) {
+   return (a -= b)
+ }
+ function multiply(a, b) {
+   return (a *= b)
+ }
+ function divide(a, b) {
+   return (a /= b)
+ }
+function inc(n) {
+  return (n += 1)
+}
+function dec(n) {
+  return (n -= 1)
+}
+function makeInt(n) {
+  return (parseInt(n, 10))
+}
+function preserveDecimal(n) {
+  return (parseFloat(n))
+}
