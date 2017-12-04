26ade3c4cc49754be75a08117980bf674f336eac
chance baars <chancebaars@gmail.com>
2017-12-04 16:36:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var a, b
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+n = 1
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return ++n
+}
+
+inc(a)
+
+function dec(n) {
+  return --n
+}
+
+dec(a)
+
+function makeInt(n) {
+  return parseInt('n', 10)
+
+}
+
+makeInt(a)
+
+makeInt(sldkjflksjf)
