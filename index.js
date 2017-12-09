790b67a48df3931a0977f951e259c63973f6cfc8
Ryan Eckerle <e_ryan1@yahoo.com>
2017-12-09 23:00:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function add(a, b){
+function add(a, b) {
   return a + b
 }
 
-function subtract(a, b){
+function subtract(a, b) {
   return a - b
 }
 
-function multiply(a, b){
+function multiply(a, b) {
   return a * b
 }
 
-function divide(a, b){
+function divide(a, b) {
   return a / b
 }
 
-function inc(n){
-  return n++
+function inc(n) {
+  return n += 1
 }
 
-function dec(n){
-  return n--
+function dec(n) {
+  return n -= 1
 }
 
-function makeInt(n){
-  return parseInt(n)
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
