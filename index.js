e885cacb0178d5cf7b870ef62ed25574a897a6b5
Matt Redmon <matt.redmon@live.com>
2017-12-11 01:18:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function add(a,b) {
+  return a + b
+}
+add(1,2)
+
+function subtract(a,b) {
+  return a - b
+}
+subtract(10,6)
+
+function multiply(a,b) {
+  return a * b
+}
+multiply(5,7)
+
+function divide(a,b) {
+  return a / b
+}
+divide(45,5)
+
+function inc(n) {
+  return ++n
+}
+inc(485)
+
+function dec(n) {
+  return --n
+}
+dec(107)
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)  
+}
+
+
+
+
+
+
