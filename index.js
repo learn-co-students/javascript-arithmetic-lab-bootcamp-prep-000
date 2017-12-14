e55dd1e00308fac096978582b293383f544e7c45
Charles Mainegra <cmainegra@gmail.com>
2017-12-14 22:49:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+  return a + b }
+function subtract(a,b) {
+  return a - b } 
+function multiply(a, b) {
+  return a * b }
+function divide(a,b) {
+  return a / b }
+function inc(n) {
+  var number = n++ 
+  return n }
+function dec(a) {
+  var number = a-- 
+  return a }
+function makeInt(a) {
+  return parseInt(a, 10) 
+  return `tosString(a)`
+}
+function preserveDecimal(a) {
+  return parseFloat(a) }
