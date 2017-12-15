6d2db72391ce11447199d1278589820f5b058ef3
Ben Deutsch <ben.deutschiii@gmail.com>
2017-12-15 01:13:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+  return a + b;
+}
+add (5, 2);
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract (52 - 21);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply (865 * 432);
+
+function divide(a, b) {
+  return a / b;
+}
+divide (432 / 51);
+
+function inc(n) {
+  return ++n;
+}
+inc (23);
+
+function dec(n) {
+  return --n;
+}
+dec (32);
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
