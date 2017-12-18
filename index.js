2a444492a590360916650b998c13e3a3fe050ca5
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-18 05:11:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(x,y) {
+  return (x+y)
+}
+function subtract(x,y) {
+  return (x-y)
+}
+function multiply(x,y) {
+  return(x*y)
+}
+function divide(x,y){
+  return(x/y)
+}
+
+function inc(number) {
+  return(number+=1)
+}
+function dec(number) {
+  return(number-=1)
+}
+function makeInt(n) {
+  return(parseInt(n,10))
+}
+function preserveDecimal(n) {
+  return(parseFloat(n))
+}
