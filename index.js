891ce103bb456bc3f872cd247d1f3a619501599d
Andrew Walker <hovercar00@gmail.com>
2017-12-18 03:26:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return(a + b);
+}
+
+function subtract(a, b) {
+  return(a - b);
+}
+
+function multiply(a, b) {
+  return(a * b);
+}
+
+function divide(a, b) {
+  return(a / b);
+}
+
+function inc(n) {
+  return(n + 1);
+}
+
+function dec(n) {
+  return(n - 1);
+}
+
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(Number) {
+  return parseFloat(Number);
+}
