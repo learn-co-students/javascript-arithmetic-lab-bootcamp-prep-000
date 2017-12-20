26d712a24481f480a37a0a68636ecb506d4c222c
yannick101693 <yannick101693@outlook.com>
2017-12-20 03:51:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var number = 5
+function add(a, b) {
+  return a + b
+}
 
-number++
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
