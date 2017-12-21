b1d83afcf58681dfcf661162ca831ffaf133bc27
Benjamin Bowes <bbowes999@gmail.com>
2017-12-21 15:09:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+return (a+b) 
+}
+function subtract(a,b) {
+  return (a-b)
+}
+function multiply(a,b) {
+  return (a*b)
+}
+function divide(a,b) {
+  return (a/b)
+}
+function inc(n) {
+  return (n + 1)
+}
+function dec(n) {
+  return (n-1)
+}
+function makeInt(n) {
+  return (n)
+}
+function makeInt(string) {
+  var n = parseInt(string,10)
+  return (n)
+}
+function preserveDecimal(string) {
+ var n = parseFloat(string)
+ return (n)
+}
