42ae6292440fb75f7157d0733e803f3f5253466f
daniel m tallen <dtallen80@gmail.com>
2017-12-22 20:14:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x,y) {
+ return x+y
+}
+
+function subtract(x,y) {
+  return x-y
+}
+
+function multiply(x,y) {
+  return x*y  
+}
+
+function divide(x,y) {
+  return x/y 
+} 
+
+function inc(x,y) {
+  return x += y
+ 
+}
+
+
+function dec(x,y) {
+  return x -= y
+}
+
+function makeInt(n) {
+ return parseInt(n)
+}
+
+function preserveDecimal(n) {
+ return parseFloat(n)
+}
