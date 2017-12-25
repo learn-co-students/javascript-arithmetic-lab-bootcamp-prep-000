c874833d65936b6cdfd475091c2ab76eda968e54
moty pitusy <moty.p2c@gmail.com>
2017-12-25 19:50:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return a+b;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc (num){
+  return num +1;
+}
+
+function dec(num){
+  return num -1;
+}
+function makeInt(num){
+  return parseInt(num,10);
+}
+function preserveDecimal(num){
+  return parseFloat(num);
+}
