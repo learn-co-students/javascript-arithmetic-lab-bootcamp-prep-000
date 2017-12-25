e2e473759709ad80bd81e46ad027157ef552d4ff
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-25 17:19:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const inc = (a) => {
+  return a += 1;
+};
+
+const dec = (a) => {
+  return a -= 1;
+};
+
+const makeInt = (n) => {
+  return parseInt(n, 10);
+};
+
+const preserveDecimal = (n) => {
+  return parseFloat(n);
+};
