7f1382f2bb599b76e1a6a8386eeadfa20cfb4357
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-26 07:08:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+var result
+function add(a, b) {
+  result = a + b
+  return result
+}
+
+function subtract(a, b) {
+  result = a - b 
+  return result
+}
+function multiply(a, b) {
+  result = a * b 
+  return result 
+}
+function divide(a, b) {
+  result = a / b 
+  return result
+}
+
+
+function inc(n) {
+  result = ++n
+  return result
+}
+function dec(n) {
+  result = --n
+  return result
+}
+
+function makeInt(n) {
+  result = parseInt(n, 10)
+  return result
+}
+
+function preserveDecimal(n) {
+  result = parseFloat(n)
+  return result
+}
+
+
+
+
+
+
+
+
+
