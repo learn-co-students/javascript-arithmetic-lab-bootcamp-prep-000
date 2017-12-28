ce32a3ded8966e7a52c4caf4ec6cbe1b9198f90a
Joseph Epifano <joepifano@gmail.com>
2017-12-28 04:20:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,65 @@
+function add(a, b) {
+  return (a + b)
+}
+function subtract(a, b) {
+  return (a - b)
+}
+function multiply(a, b) {
+  return (a * b)
+}
+function divide(a, b) {
+  return (a / b)
+}
+function inc(n) {
+  return (n + 1)
+}
+function dec(n) {
+  return (n - 1)
+}
+function makeInt(n) {
+  return (parseInt)
+  console.log ("tostring = n")
+}
+function makeInt(10) {
+  console.log ("0 * 2328 = 0")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
