b4b300a47d934842628ae6637388949bc27114fb
Megi Shehi <mshehi97@gmail.com>
2017-12-29 16:16:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(n, m){
+  return n+m;
+}
+
+function subtract(n, m){
+  return n-m;
+}
+
+function multiply(n,m){
+  return n*m;
+}
+
+function divide(n,m){
+  return n/m;
+}
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
