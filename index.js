8a08d110bf8b52889d8eb37eb9316084bf170d04
Brian David Lehto <bdlden1978@outlook.com>
2018-01-01 02:58:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(n) {
+  return ++n
+}
+function dec(n) {
+  return --n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
