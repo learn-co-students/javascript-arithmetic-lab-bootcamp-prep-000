1ea698581a5e93f8611b3b51c9ca53ef4d090cff
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-03 21:14:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var a = 10, b = 20;
+function add(a,b){
+  return (a+b)
+}
+function subtract(a,b){
+  return (a-b)
+}
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return (a/b);
+}
+function inc(a){
+  return (++a);
+}
+function dec(a){
+  return (--a);
+}
+parseInt("a");
+return (a);
