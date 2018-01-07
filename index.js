81e5650430d7ed861047d8d9093aa6f383971171
Kateri Casey <casey1km@gmail.com>
2018-01-07 23:57:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+  return a += b 
+}
+function subtract(a,b) {
+  return a -= b 
+}
+function multiply(a,b) {
+  return a *= b 
+}
+function divide(a,b) {
+  return a /= b 
+}
+function inc(a) {
+  a++
+  return a 
+}
+function dec(a) {
+  a -= 1
+  return a 
+}
+function makeInt(string) {
+  return parseInt(string,10)
+}
+function preserveDecimal(float) {
+  return parseFloat(float)
+}
