8a7363c67fec4cc1bf050e150eacade7adc32dda
Demetrious Young <zendo425@gmail.com>
2018-01-08 08:05:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+ function add(a, b) {
+   
+  return a + b;
+}
+
+function subtract(a, b) {
+   
+  return a - b;
+}
+
+ function multiply(a, b){
+    
+    return a * b;
+ }
+ 
+ function divide(a, b){
+    
+    return a / b;
+ }
+ 
+var num = 25;
+function inc(n){
+n++;
+
+return n;
+}
+
+var num = 25;
+function dec(n){
+n--;
+
+return n;
+}
+
+
+function makeInt(n) {
+  
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  
+  return parseFloat(n);
+}
