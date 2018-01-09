bc7f0247f7c910cfb693baa5dbb8c25f5191d246
Krista Dotzenrod <kdotzenrod517@gmail.com>
2018-01-09 02:51:57 +0000

    Done.

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function dec(n){
 }
 
 function makeInt(n){
-  n = parseInt(n, 10)
-  return (n)
+  return (parseInt(n, 10))
 }
 
 function preserveDecimal(n){
-  n = parseFloat(n)
-  return (n)
+  return (parseFloat(n))
 }
