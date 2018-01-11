0965d5c80ba6160f3905fde89450f17370e6ccb2
SDS 800 <jeremyhyman@gmail.com>
2018-01-11 21:41:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+ return (a+b);
+}
+
+function subtract(a, b) {
+ return (a-b);
+}
+
+function multiply(a, b) {
+ return (a*b);
+}
+function divide(a, b) {
+ return (a/b);
+}
+
+function inc(a) {
+  return (a+=1);
+}
+
+function dec(a) {
+  return (a-=1);
+}
+
+function makeInt(a) {
+  return parseInt(a, 10);
+}
+
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
+
