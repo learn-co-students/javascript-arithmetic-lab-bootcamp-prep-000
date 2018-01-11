8b4fe66d016d3bfcd88e74563f79952630ef767a
Riyaad Azad <azad.riyaad@gmail.com>
2018-01-11 13:15:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,56 @@
+function add(a, b) {
+  return a + b
+  add(1, 80)
+}
+
+function subtract(a, b) {
+  return a - b 
+  subtract(60, 40)
+}
+
+function multiply(a, b) {
+  return a * b 
+  multiply(2, 3.4)
+}
+
+function divide(a, b) {
+  return a / b 
+  divide(5.0, 2.5)
+}
+
+function inc(n) {
+  n++
+  return n
+  inc(805)
+}
+
+function dec(n) {
+  n--
+  return n
+  dec(753)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  makeInt(798)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  makeInt(87)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  makeInt(0)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+  preserveDecimal(2.222)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+  preserveDecimal(2.222)
+}
