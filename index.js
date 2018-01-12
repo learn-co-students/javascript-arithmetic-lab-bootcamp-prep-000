f98fcc90087e3b98ffb638c603acab758d1903dc
kendocode <git@kendocoding.com>
2018-01-12 13:35:15 -0600

    Try first several tests

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function add(a,b) {
+function add(a, b) {
   return a + b;
 }
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+  return a + 1;
+}
+
+function dec(a) {
+  return a - 1;
+}
+
+function makeInt(a) {
+  return a.parseInt();
+}
+
+
+
