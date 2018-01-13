2d3368515ef17003b5643983f91e5f3a1c9256b1
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-13 17:50:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function inc(a) {
+  return ++a
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function dec(a) {
+  return --a
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
