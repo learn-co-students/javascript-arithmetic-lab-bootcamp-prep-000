5d37233fac5d07eea64d219474448d3c286d5918
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-13 03:29:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+   return parseInt(a) + parseInt(b)
+ }
+
+function subtract(a,b){
+  return parseInt(a) - parseInt(b)
+}
+
+function multiply(a,b){
+  return parseInt(a) * parseInt(b)
+}
+
+function divide(a,b){
+  return parseInt(a) / parseInt(b)
+}
+
+function dec(n){
+  n = (parseInt(n)) - 1
+  return n
+}
+
+function inc(n){
+  n = (parseInt(n)) + 1
+  return n
+}
+
+function makeInt(n){
+  n = parseInt(n, 10)
+  return n
+}
+
+function preserveDecimal(n) {
+  n = parseFloat(n)
+  return n
+}
