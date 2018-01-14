a2b057d05e983fb941e4a3299c80496e921ab903
craig stevens <c_stevens44@hotmail.com>
2018-01-14 17:11:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+  var a= "1000";
+  var b= 146.2328;
+
+function add(a,b) {
+ return (a+b)
+}
+
+function subtract(a,b) {
+  return (a-b)
+}
+
+function multiply(a,b) {
+  return (a*b)
+}
+
+function divide(a,b) {
+  return (a/b)
+}
+
+function inc(a) {
+  return (a+1)
+}
+
+function dec(a) {
+  return (a-1)
+}
+
+function  makeInt(a) {
+  return parseInt(a)
+}
+
+function makeInt(b) {
+  return parseInt(b, 10)
+}
+
+function preserveDecimal(b) {
+  return parseFloat(b) 
+}
