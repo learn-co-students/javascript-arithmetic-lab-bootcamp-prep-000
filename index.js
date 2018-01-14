2db1c0b76e9520d5c22b06ec5ea3ead77e5fca0d
Daix Ur <d.urnikyte@gmail.com>
2018-01-14 16:13:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
-var number =10
-
-function add5(){
-  number+=5
+//number 1
+var a=6;
+var b=9;
+function add(a,b){
+  return a+b
 }
-function divideBy3(){
-  number/=3
+//number 2
+var a=6;
+var b=9;
+function substract(b,a){
+return substract()
 }
-divideBy3()
+//number 3
+var a=6;
+var b=9;
+function multiply(a,b){
+  return a*b
+}
+//number 4
+var a=6;
+var b=9;
+function divide(a,b){
+  return a/b
+}
+//number 5
+var a=6;
+var b=9;
+function parseInt(a,b){
+  return (a,b)
+}
+//number 6
+var a=6;
+var b=9;
+function inc(a,b){
+  return b-=a
+}
+//number 7
+var a=6;
+var b=9;
+function makeInt(a,b){
+  return a,b
+}
+//number 8
+function makeInt(a,b){
+  // assumes base 10\\
+}
 
-console.log(number)
-
-add5()
-console.log(number)
+//number 9
+var a=6;
+var b=9;
+function makeInt(a,b){
+  return a,b
+  //returns NaN
+}
+//number 10
+var a=6;
+var b=9;
+function preserveDecimal(a,b){
+  return a,b
+}
+//number 11
+var a=6;
+var b=9;
+function preserveDecimal(a,b){
+  return a,b
+}
