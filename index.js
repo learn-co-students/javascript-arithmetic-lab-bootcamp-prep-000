b956cc291de50aeffd6b9fc292552d1aaa406960
Mark Grant <mark.rd.grant@gmail.com>
2018-01-14 22:33:22 +0000

    Done.

--- index.js
+++ index.js
@@ -23,15 +23,7 @@ function dec(n){
 }
 
 function makeInt(n){
-  return parseInt(n)
-}
-
-function makeInt(n){
   return parseInt(n, 10)
-}
-
-function makeInt(n){
-  return parseInt(n)
 }
 
 function preserveDecimal(n){
