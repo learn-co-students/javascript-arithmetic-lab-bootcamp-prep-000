7d684dd55bb3eee2d6bee4db8882557f9341dd82
semih Fatih <s_fatihh@hotmail.com>
2018-01-14 22:12:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+add(a,b){
+  return a+b
+}
+subtract(a,b){
+  return a-b
+}
+multiply(a,b){
+  return a*b
+}
+divide(a,b){
+  return a/b
+}
+inc(n){
+  return ++n
+}
+dec(n){
+  return --n
+}
+makeInt(n){
+  return parseInt(n,10)
+}
+preserveDecimal(n){
+  return parseFloat(n)
+}
