485d04d87b24f4e91760ca5ef8a673bf6479582e
Remya Jacob <remyaabi@gmail.com>
2018-01-16 22:22:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+var a=10
+var b=5
+var number3='hai'
+var n=67.8999
+
+function add(a,b){
+ 
+  return a+b
+}
+function subtract(a,b){
+  
+  return a-b
+}
+
+function multiply(a,b){
+ 
+  return a*b
+}
+
+function inc(a){
+ 
+  return ++a
+}
+function dec(a){
+ 
+  return --a
+}
+function makeInt(a){
+  return parseInt(a)
+}
+function makeInt(a){
+  return parseInt(a,10)
+}
+
+ function makeInt(number3){
+  return parseInt(number3)
+}
+function divide(a,b){
+  return a/b
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
+
