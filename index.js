fbc35ac4c7c622b1038456adc2e26a949926442f
Alexandra Batrak <alexandrabatrak@gmail.com>
2018-01-16 22:58:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  var n = n+1
+  return n
+}
+function dec(n){
+  var n = n-1
+  return n
+}
+function makeInt(n){
+  var n = parseInt(n, 10)
+  return n
+}
+function preserveDecimal(n){
+  var n = parseFloat(n)
+  return n
+}
