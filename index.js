2dd986e16ab3bed198700ee3047408ac374ca93c
Dana Kessler <kessler.danam@gmail.com>
2018-01-16 06:56:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+beforeEach(function() {
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+})
+
+var number = a, b
+
+function add(number) {
+  console.log(add);
+  return a + b;
+}
+
+function subtract(a, b) {
+  console.log(subtract);
+  return a - b;
+}
+
+function multiply(a, b) {
+  console.log(multiply);
+  return a*b;
+}
+
+function divide(a, b) {
+  console.log(divide);
+  return a/b;
+}
+
+function inc(n) {
+  console.log(inc);
+  parseInt(n + 1);
+  return n + 1;
+}
+
+function dec(n) {
+  console.log(dec);
+  parseFloat(n - 1);
+  return n - 1;
+}
+
+function makeInt(n) {
+  console.log(makeInt);
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  console.log(preserveDecimal);
+  return parseFloat(n. NaN);
+}
