df5de18b379859e73da1b13d7265f5b282e20591
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-17 14:52:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
+//var a, b
 
-function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-}
+//beforeEach(function() {
+//  a = Math.floor(Math.random() * 1000)
+//  b = Math.floor(Math.random() * 1000)
+//})
+
+//adds two numbers and returns the result
 
 function add(a, b){
-  a+b
+
+  return (a + b)
+
 }
 
+//subtracts b from a and returns the result
 function subtract(a, b){
-  a-b
+  return (a - b)
 }
 
-function multiply(a,b){
-  a*b 
+//multiplies two numbers and returns the result
+function multiply(a, b){
+  return (a * b)
 }
 
-function divide (a, b){
-  a/b 
+//divides a by b and returns the result
+function divide(a, b){
+  return(a / b)
 }
 
-function inc(a, b){
-  a+1
+//increments n and returns the result
+function inc(a){
+  return (a + 1)
 }
+
+//decrements n and returns the result
+function dec(a){
+  return (a - 1)
+}
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
