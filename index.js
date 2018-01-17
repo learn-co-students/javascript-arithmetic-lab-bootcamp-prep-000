29e8457d00815567b1c4b45e3cce0af6eb19423b
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-17 21:40:56 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 function add(a, b){
 
-  return (a + b)
+  return (a + b);
 
 }
 
@@ -20,44 +20,35 @@ function subtract(a, b){
 
 //multiplies two numbers and returns the result
 function multiply(a, b){
-  return (a * b)
+  return (a * b);
 }
 
 //divides a by b and returns the result
 function divide(a, b){
-  return(a / b)
+  return(a / b);
 }
 
 //increments n and returns the result
 function inc(a){
-  return (a + 1)
+  return (a + 1);
 }
 
 //decrements n and returns the result
 function dec(a){
-  return (a - 1)
+  return (a - 1);
 }
 
 //parses n as an integer and returns the parsed integer
-function makeInt(a.toString()){
-  return (a)
-  }
+function makeInt(a, base){
+  var parsed = parseInt(a, base);
+  if (isNaN(parsed)) { return 0 }
+  return parsed;
+}
 
 //assumes base 10
-function makeInt('0x2328'){
-  return (0)
+function makeInt(x, base) {
+  var parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0x2328 }
+  return parsed;
   }
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
