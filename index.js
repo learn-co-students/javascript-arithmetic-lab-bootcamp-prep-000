f48ef8cd40ed0e45331b51a13bbc222eaa8735a1
Brock Rohloff <00agentb@gmail.com>
2018-01-18 08:30:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(a,b){a += b; return a;}
+function subtract(a,b){a -= b; return a;}
+function multiply(a,b){a *= b; return a;}
+function divide(a,b){a /= b; return a;}
+function inc(n){n++; return n;}
+function dec(n){n--;return n;}
+function makeInt(n){n = parseInt(n, 10); return n;}
+function preserveDecimal(n){n = parseFloat(n); return n;}
