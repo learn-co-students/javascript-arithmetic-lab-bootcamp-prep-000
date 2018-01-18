96cb9d2578c33ddf6789ce1035c7dfb753eb2ec5
Matthew Trenk <matt.trenk@gmail.com>
2018-01-18 18:27:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function makeInt(p){
+  var l = parseInt(p, 10)
+  return l
+}
+
+function preserveDecimal(n){
+  var o = parseFloat(n)
+  return o
+}
+
+function add(a, b){
+  var sum = a + b
+  return sum
+}
+
+function subtract(a, b){
+  var x = a - b
+  return x
+}
+
+function multiply (a, b){
+  var product = a*b
+  return product
+}
+
+function divide(a, b){
+  var v = a/b
+  return v
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(m) {
+  m -= 1
+  return m
+}
