906e5d36d83d9f6c28b7f3a49c8d1decfd68b263
Alevtina Lobova <ally1125@gmail.com>
2018-01-19 01:23:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+function add(a,b){
+  return a+b;
+}
+add(3,5);
+
+function subtract(a,b){
+  return a-b;
+}
+subtract(10,2);
+
+function multiply(a,b){
+  return a*b;
+}
+multiply(3,6);
+
+function divide(a,b){
+  return a/b;
+}
+divide(4,5);
+
+var n = 6 
+
+function inc(){
+  n++;
+  return n;
+}
+inc(n);
+
+function dec(){
+  n--;
+  return n;
+}
+dec(n);
+
+
+
+
