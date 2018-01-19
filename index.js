271c1b64187fd500d45a883347737499ce375922
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 16:26:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-var add = (x+=y);
 
-var subtract = (x-=y)
+function add(a, b) {
+  return a += b
+}
 
-var multiply = (x *= y)
+function subtract(a, b){
+  return a -= b
+}
 
-var divide = (x /= y)
+function multiply(a, b){
+  return a *= b
+}
+
+function divide(a, b){
+  return a /= b
+}
+
+function inc(n){
+  return ++n
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
