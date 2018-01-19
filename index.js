f9734996331fdd2190861429a987da73dcc38ccf
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-19 06:20:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+return a - b
+}
+
+function multiply(a, b) {
+return a * b
+}
+
+function divide(a, b) {
+return a / b
+}
+
+function inc(a) {
+return a + 1
+}
+
+function dec(a) {
+return a -1
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  parseFloat(n) = 2.22222
+  return n
+}
+
