551472af432bac0deabdd5fa8325300499f34272
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-20 23:53:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var a,b,n;
+
+//Part 1
+function add(a,b){
+  return a + b ;
+}add (9,2);
+
+//Part 2
+function subtract(a,b){
+  return a-b;
+}subtract(2,7);
+
+//Part 3
+function multiply(a,b){
+  return a * b;
+}multiply(4,2);
+
+//Part 4
+function divide(a,b){
+  return a/b;
+}divide(a,b);
+
+//Part 5
+function inc(n){
+  return n+=1;
+}inc(4156);
+
+//Part 6
+function dec(n){
+  return n-=1;
+}dec(28)
+
+//Parts 7,8 and 9
+function makeInt(n){
+  var p = parseInt(n, 10);
+  return p;
+}makeInt('22');
+
+//Parts 10 and 11
+function preserveDecimal(n){
+  var f = parseFloat(n, 10);
+  return f;
+}preserveDecimal('10.9801');
