cf8781af03c22b3428ec8ffbb4bab93a07907a6d
Susan <suzutoyuki@gmail.com>
2018-01-21 10:49:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+function divide(a, b){
+  return a /= b;
+
+}
+
+divide();
+
+function add(a,b){
+  return a+=b;
+}
+add()
+function subtract(a, b){
+  return a -= b;
+
+}
+
+subtract();
+
+function multiply(a, b){
+  return a *= b;
+
+}
+
+multiply();
+
+function inc(n){
+  return n+1;
+
+}
+
+inc();
+
+function dec(n){
+  return n-1;
+
+}
+
+dec();
+
+function makeInt(n){
+  return parseInt(n);
+}
+makeInt()
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+makeInt()
+
+function preserveDecimal(n){
+  return parseFloat(n,10);
+}
+preserveDecimal()
