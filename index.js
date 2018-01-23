8e96308bf6d2b8e474cf9953f4e70fb573027bcb
petr brown <petrbrown@gmail.com>
2018-01-23 06:10:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+function add(n,nn) {
+  return parseFloat(n)+parseFloat(nn)
+}
+function subtract(n,nn) {
+  return parseFloat(n)-parseFloat(nn)
+}
+function multiply(n,nn) {
+  return parseFloat(n)*parseFloat(nn)
+}
+function divide(n,nn) {
+  return parseFloat(n)/parseFloat(nn)
+}
+function inc(n) {
+  return parseFloat(n)+1
+}
+function dec(n) {
+  return parseFloat(n)-1
+}
+function makeInt(n) {
+  return parseInt(n,10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,7 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
+
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
