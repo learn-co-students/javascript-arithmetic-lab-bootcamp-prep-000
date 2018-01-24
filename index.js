39e68ef9605d2c58379e4f7011157ff8f6574e25
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:03:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-1 + 80;
-60 - 40;
-2 * 3.4;
-5.0 / 2.5;
+function add(a, b) {
+  1 + 80;
+}
+
+function subtract(a, b) {
+  60 - 40;
+}
+
+function multiply(a, b) {
+  2 * 3.4;
+}
+
+function divide(a, b) {
+  5.0 / 2.5;
+}
