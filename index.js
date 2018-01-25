4920a49d8090b87eba67cb522b9af63e45653d30
Jassen McClain <new.coder@aol.com>
2018-01-25 21:20:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var a = 10;
+var b = 5;
+function add(a,b) {
+  return a + b;
+}
+add(a,b)
+function subtract(a,b) {
+  return a - b;
+}subtract(a,b)
+function multiply(a,b) {
+  return a * b;
+}multiply(a,b)
+function divide(a,b) {
+  return a / b;
+}multiply(a,b)
+function inc(a) {
+  return a + 1;
+}inc(a)
+function dec(a) {
+  return a - 1;
+}dec(a)
+function makeInt(a) {
+  return parseInt(a, 10);
+}makeInt(a)
+function preserveDecimal(a) {
+  return parseFloat(a);
+}preserveDecimal(a)
