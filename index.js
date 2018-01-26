e4cebbdba10c10de5f6af231dcda3415a6fcdcc9
Eric Niu <eric@goswaggle.com>
2018-01-26 00:16:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply (a,b) {
+  return a * b
+}
+
+function divide (a,b) {
+  return a / b
+}
+
+function inc(a,b) {
+  return a+= 1
+}
+
+function dec(a,b) {
+  return a-= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n, 10)
+}
