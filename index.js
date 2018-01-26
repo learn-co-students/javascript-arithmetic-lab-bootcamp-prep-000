812220d5e1cddcd4a332736c78c4c9638d809cfb
jairaj shetty <jayuchintu@gmail.com>
2018-01-26 06:25:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  var sum=a+b;
+  return sum;
+}
+function subtract(a,b){
+  var difference=a-b;
+  return difference;
+}
+function multiply(a,b){
+  var mul=a*b;
+  return mul;
+}
+function divide(a,b){
+  var div=a/b;
+  return div;
+}
+function inc(n){
+  var increment=++n;
+  return increment;
+}
+function dec(n){
+  var decrement=--n;
+  return decrement;
+}
+function makeInt(n){
+  var integer=parseInt(n);
+  return integer;
+}
+function makeInt(n){
+  var integer=parseInt(n,10);
+  return integer;
+}
+function preserveDecimal(n){
+  var integer=parseFloat(n,10);
+  return integer;
+}
