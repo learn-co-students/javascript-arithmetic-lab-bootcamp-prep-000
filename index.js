5c03eb13e3781ab285e0f7d3490a2ed0be120dc9
Oghene Michael <zonalmeet@gmail.com>
2018-01-27 20:45:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  n++
+  return n;
+}
+
+function dec(n) {
+  n--
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
