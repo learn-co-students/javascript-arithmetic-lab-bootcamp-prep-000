f4ed7e6a03dd37d41edc0d374add5638109e43f2
Charlie Warner <charlie@omnium-events.com>
2018-01-29 04:08:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(1,b) {
+return a + b;
+}
+
+function subtract(a,b) {
+
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a * b;
+
+}
+
+function divide(a,b) {
+  return a/b;
+
+//}
