9acddf4826218273bb72361c83122a911b988cf3
Devin DiNapoli <devindinapoli@gmail.com>
2018-01-30 23:05:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  var sum = a+b 
+  return sum 
+}
+
+function subtract(a,b) {
+  var sum = a-b
+  return sum
+}
+
+function multiply(a,b) {
+  var sum = a*b 
+  return sum
+}
+
+function divide(a,b) {
+  var sum = a/b 
+  return sum
+}
+
+function inc(a) {
+  var increment = ++a
+  return increment
+}
+
+function dec(a) {
+  var decrement = --a
+  return decrement 
+}
+
+function makeInt(a) {
+  var parsed = parseInt(a, 10)
+  return parsed
+}
+
+function preserveDecimal(a) {
+  var decimal = parseFloat(a, 10)
+  return decimal
+}
