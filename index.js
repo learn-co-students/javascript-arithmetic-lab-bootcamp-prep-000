7db8cafbca44b8df2c4ebb4de19302f1c1bf8b1d
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 01:21:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var a = 5
+var b = 3 
+
+function add(a, b) {
+  return a + b 
+}
+
+function subtract(a,b) {
+  return a - b 
+}
+
+function multiply(a, b) {
+  return a * b 
+}
+
+function divide(a, b) {
+  return a / b 
+}
+
+var n = 10
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+n = 2.222
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
