d96f5ddb950bd41b5ea4efe7d357a8044765fb7c
Deep Patel <deepgpatel93@gmail.com>
2018-01-30 18:48:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b)
+{
+  var sum = a + b
+return sum 
+}
+
+function subtract(a, b)
+{
+  var difference = a - b
+return difference 
+}
+
+function multiply(a, b)
+{
+  var product = a * b
+return product 
+}
+
+function divide(a, b)
+{
+  var quotient = a / b
+return quotient 
+}
+
+function inc(a)
+{
+  var increment = ++a
+return increment 
+}
+
+function dec(a)
+{
+  var decrement = --a
+return decrement 
+}
+
+function makeInt(a)
+{
+  var parsed = parseInt(a, 10)
+return parsed 
+}
+
+function preserveDecimal(a)
+{
+  var float = parseFloat(a, 10)
+return float 
+}
