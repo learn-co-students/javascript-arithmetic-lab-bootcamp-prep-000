841f954f838b31b5c9088e669e61988cb57827f9
Pitta <mailez4all@gmail.com>
2018-01-31 10:20:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+  return a + b
+}
+
+add(2, 3)
+
+function subtract(a, b) {
+  return a - b
+}
+
+subtract(5, 4)
+
+function multiply(a, b) {
+  return a * b
+}
+
+multiply(4, 5)
+
+function divide(a, b) {
+  return a / b
+}
+
+divide(10, 2)
+
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+preserveDecimal('7.99')
