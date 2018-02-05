ae874dc2409d1f54e4c99a94bdc0ac046bc20de2
Damon Brown <dbrow437@gmail.com>
2018-02-05 21:32:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b){
+  return (a + b)
+}
+
+function subtract(a, b){
+  return (a-b)
+}
+
+function multiply(a, b){
+  return (a*b)
+}
+
+function divide(a, b){
+  return (a/b)
+}
+
+function inc(a, n){
+  return (a + 1)
+}
+
+function dec(a, n){
+  return (a-1)
+}
+
+function makeInt(a, n){
+  return parseInt()
+}
+
+function makeInt(n){
+  return parseInt(n, 0)
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat()
+}
+
+function preserveDecimal(n){
+  return parseFloat(n, 2.222)
+}
