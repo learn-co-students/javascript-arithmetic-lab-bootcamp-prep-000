33cb5cb489e76f923c6ffa11edb3c6501a08f14a
Kachel Wittig <kjwittig@gmail.com>
2018-02-05 08:05:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+//inc(n) increments n and returns the result
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n, x) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
