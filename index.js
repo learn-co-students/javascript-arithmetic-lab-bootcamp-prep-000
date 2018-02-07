3cf71a27a5a0ccfa182c1d2dfea2cab13a25f6be
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-07 04:23:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add (numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+function multiply (numOne, numTwo) {
+  return numOne * numTwo;
+}
+
+function subtract (numOne, numTwo) {
+  return numOne - numTwo;
+}
+
+function divide (numOne, numTwo) {
+  return numOne / numTwo;
+}
+
+function inc (n) {
+  n++;
+  return n;
+}
+
+function dec (n) {
+  n--;
+  return n;
+}
+
+function makeInt (n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n);
+}
