03297434e14f4a55dcb42343d4fb0a3c46e70b06
Everett <everett.bowline@gmail.com>
2018-02-09 13:47:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-var a = 134
+function add(a, b) {
+  return a + b
+}
 
-var b = 1
+function subtract(a, b) {
+  return a - b
+}
 
-function add() {
-  return (a + b)
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n, base) {
+  return parseInt(n, 10)
+}
+
+function makeInt(n) {
+  return parseInt('Does your mother still work at dockside bars?')
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n)
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n)
 }
