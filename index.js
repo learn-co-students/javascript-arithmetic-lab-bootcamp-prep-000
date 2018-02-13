087df06bf7183d8da3961fd01b2f7d634516e926
Thomas Casady <tscasady@gmail.com>
2018-02-13 19:51:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add (a,b){
+  var sum = a + b
+  return sum
+}
+
+function subtract (a,b){
+  var difference = a - b
+  return difference
+}
+
+function multiply (a,b){
+  var product = a * b
+  return product
+}
+
+function divide (a,b){
+  var quotient = a / b
+  return quotient
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(a){
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
