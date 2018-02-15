4622f1d069c02ff5495b9492344394e328e5d36e
Tif Flowers <tif@gentlebull.com>
2018-02-15 15:56:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a+b
+}
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+var number = 5
+
+function inc(n) {
+    n++;
+  return n;
+}
+
+function dec(n) {
+    n--;
+  return n;
+}
+
+
+function makeInt(n) {
+  var parsed = parseInt('2', 10)
+  return  parsed ;
+}
+
+function preserveDecimal(n) {
+  var parsed = parseFloat(n)
+  return parsed;
+}
+
+
