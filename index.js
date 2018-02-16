4a574e56f56507614d8830b69c0b7e2a963aad6b
Michelle Olivieri <michelleo72@gmail.com>
2018-02-16 02:29:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var number
+function add(a,b) {
+  number = a + b
+  return number
+}
+
+function subtract(a,b) {
+  number = a - b
+  return number
+}
+
+function multiply(a,b) {
+  number = a * b
+  return number
+}
+
+function divide(a,b) {
+  number = a / b 
+  return number
+}
+
+function inc(n) {
+  number = n
+  number++
+  return number
+}
+
+function dec(n) {
+  number = n
+  number--
+  return number
+}
+
+function makeInt(n) {
+  number = parseInt(n, 10)
+  return number
+}
+
+function preserveDecimal(n) {
+  number = parseFloat(n)
+  return number
+}
