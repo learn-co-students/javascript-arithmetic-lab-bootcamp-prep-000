b75b7400f7061b83df218daf7a817c291ac403d7
wley3337@gmail.com <wley3337@gmail.com>
2018-02-18 22:28:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add (a,b) {
+    var c = a + b ;
+    return `${c}` ;
+  }
+
+function subtract(a,b) {
+    var c = a - b ;
+    return `${c}` ;
+  }
+  
+function multiply(a,b) {
+    var c = a * b ;
+    return `${c}`;
+  }
+
+function divide(a,b) {
+    var c = a / b ;
+    return `${c}`;
+  }
+
+function inc(n) {
+    n++;
+    return `${n}`;
+    }
+
+function dec(n) {
+    n-- ;
+    return `${n}`;
+    }
+
+function makeInt(n){
+    return parseInt(n,10);
+    }
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+  
