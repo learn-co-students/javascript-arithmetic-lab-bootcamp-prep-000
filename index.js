718b43f9c7e04556a6cac2ed5bd2c8566fb0fd67
christopher zambroski <zambrosk84ever@gmail.com>
2018-02-19 02:38:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add (a, b) {
+  return (a + b)}
+    (5,3)
+    
+function subtract (b, a) {
+  return (b - a)}
+   (19, 12)
+   
+function multiply (a, b) {
+  return (a * b)}
+  (2,2)
+  
+function divide (a, b) {
+  return (a / b)}
+  (6,2)
+  
+function inc (a) {
+  return (a + 1)}
+  (3)
+  
+function dec (a) {
+  return (a - 1)}
+  (3)
+
+function makeInt(a){
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a, '2.222')
+}
+
+
