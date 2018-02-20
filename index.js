4d17896442157f5c546d459fbddfeb3cedf8316a
Daniel Walz <walzdan@gmail.com>
2018-02-20 20:10:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b)      {
+  return ('{$a} + {$b}')
+}
+add(1,583)
+
+function subtract(a,b) {
+  return ('{$a} - {$b}')
+}
+subtract(12,4)
+
+function multiply(a,b) {
+  return ('{$a} * {$b}')
+}
+multiply(63723,2)
+
+function divide(a,b)   {
+  return ('{$a} / {$b}')
+}
+divide(4,2)
+
+function inc(n) {
+  return ('{$n}++')
+}
+inc(715)
+
+function dec(n)        {
+  return ('{$n}--')
+}
+dec(414)
+
+
