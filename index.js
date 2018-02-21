7ab87ad75d5d6103adb665563eeeed7e8be087a5
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-21 21:33:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+//Karalyn Ferrari
+//2/21/2018
+
+/* add function, add's two floats a and b and returns the answer
+*/
+function add(a,b) {
+  return a + b
+}
+
+/* subtract, subtracts two floats a from b and returns the answer
+*/
+function subtract(a,b) {
+  return a-b 
+}
+
+/* multiply, multiplys two floats and returns the answer
+*/
+function multiply(a,b) {
+  return a*b
+}
+
+/*divide, divides to floats and returns the answer
+*/
+function divide(a,b) {
+  return a/b
+}
+
+/* inc, increments a float n by 1 and returns the result
+*/
+function inc(n) {
+  n++
+  return n
+}
+
+/*dec, decrements a float n and returns the result
+*/
+function dec(n) {
+  n--
+  return n
+}
+
+/* makeInt, accepts a string and converts into an integer
+*/
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+/*preserveDecimal(n) accepts a string and turns it into a float*/
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
