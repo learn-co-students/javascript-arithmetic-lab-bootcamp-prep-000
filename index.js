c4dc40a07152ee29e668c04bd9f89b4999bd65bb
kate ROSENBERG <kate@digitalmachinations.com>
2018-02-22 18:16:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+ function add(a, b) {
+   return a + b;
+   }
+   
+   function substract(a, b) {
+     return a - b;
+   }
+   
+   function multiply(a, b) {
+     return a * b;
+   }
+   
+   function inc(n) {
+     return n++;
+   }
+    function dec(n) {
+      return dec--;
+    }
+
+ function makeInt(n) {
+   return parseInt(n);
+ }
+  function makeInt(n) {
+    return parseFloat(n);
+  }
+
+  
