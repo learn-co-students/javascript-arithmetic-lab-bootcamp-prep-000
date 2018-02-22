a44ba7d17f3f2d04877853e97e640a34415f9bc7
Eli Draluk <eli.draluk@gmail.com>
2018-02-22 17:16:54 +0000

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function divide(a,b){
   return a/b
 }
 function inc(a){
-  return a++
+  return ++a
 }
 function dec(a){
-  return a--
+  return --a
 }
 function makeInt(a){
-  return parseInt(a);
+  return parseInt(a,10);
 }
 function preserveDecimal(a){
   return parseFloat(a);
