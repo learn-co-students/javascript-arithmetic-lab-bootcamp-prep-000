2808c4970e7187a27669d8c432ef9be742798de7
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-23 23:30:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var add = function(a, b) { 
+  return a + b;
+}
+
+var subtract = function(a, b) {
+  return a - b;
+}
+
+var a=2
+var b=1
+var n="2"
+
+var multiply = function (a, b) {
+  return a * b;
+}
+
+var divide = function (a, b) {
+  return a / b;
+}
+
+var inc = function (n) {
+  return a + 1;
+}
+
+var dec = function (n) {
+  return a - 1;
+}
+
+var makeInt = function (n) {
+  return parseInt(n, 10) 
+}
+  
+var preserveDecimal = function (n) {
+  return parseFloat(n)
+}
