fc4330b5b3aa1d000dac028e9fe18c22979accef
Jenny Kam <jkam17@gmail.com>
2018-02-25 21:31:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  return parseInt(a)+parseInt(b)
+}
+
+function subtract(a,b){
+  return parseInt(a)-parseInt(b)
+}
+
+function multiply(a,b){
+  return parseInt(a)*parseInt(b)
+}
+
+function divide(a,b){
+  return parseInt(a)/parseInt(b)
+}
+
+function inc(a){
+  var x=parseInt(a)
+  x++
+  return x
+}
+
+function dec(a){
+  var x=parseInt(a)
+  x--
+  return x
+}
+
+function makeInt(a){
+ return parseInt(a,10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a)
+}
+
