764619a8acc0def423f503bbe607f167e5d9b792
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-27 22:00:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var num1 = 1
+var num2 = 20
+function add(num1, num2){
+  return num1 + num2
+}
+
+function subtract(num1, num2){
+  return num1 - num2
+}
+
+function multiply(num1, num2){
+  return num1 * num2
+}
+function divide(num1, num2){
+  return num1 / num2
+}
+
+
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+ return parseFloat(n) 
+}
