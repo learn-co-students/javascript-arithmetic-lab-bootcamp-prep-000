22ed6d49dca33b3486758fcdd6048b8654c33f55
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 01:09:10 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -73,11 +73,13 @@ describe('makeInt(n)', function() {
 })
 
 describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
+  it('parseFloat('2.222') === ('2.222'), function() {
+    expect(preserveDecimal('2.222')).toBe('2.222')
+    parseFloat('2.222')
   })
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+
