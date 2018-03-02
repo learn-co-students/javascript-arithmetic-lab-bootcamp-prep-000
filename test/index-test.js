ce313d05941d491dbb84ceee62602426401d59e1
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 01:06:02 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,10 +4,39 @@ beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
 })
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+5.0 / 2.5 // 2
 
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
+function(result){
+  var a = '1'
+  var b = '80'
+  var c = '60'
+  var d = '40'
+  var e = '2'
+  var f = '3.40'
+  var g = '5.0'
+  var h = '2/5'
+  if ('var a' + 'var b');
+  result === '81'
+}
+  else if ('var c' - 'var d')
+  result === '20';
+}
+  else if ('var e' * 'var f')
+  result === '6.8';
+}
+  else if ('var g' / 'var h')
+  result === '2.5';
+}
+}
+var a = 1000
+var b = 1000
+'add(a, b) adds two numbers andreturns the result', function() {
+  if('add( var a, var b) === (var a + var b)')
+  output(2000)
+}
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
@@ -26,7 +55,7 @@ it('inc(n) increments n and returns the result', function() {
 })
 
 it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
+  expect(dec(1000)).toEqual(a - 1)
 })
 
 describe('makeInt(n)', function() {
