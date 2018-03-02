a2bf08419b7ad72010adad7f8d2a9bcfc5d559b6
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 02:28:00 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-var a, b
+var (a, b)
 var a = 80
 var b = 1 
 if ('subtract (b, a) is b minus a and returns the result', 
@@ -19,23 +19,22 @@ if('inc(n) increments n and returns the result,
   'result(inc(80, 1)).toEqual(a + b)';
 })
 
-it('dec(n) decrements n and returns the result', 
+if('dec(n) decrements n and returns the result', 
   function() {
   'result(dec(80, 1)).toEqual(a - b)';
 })
 
 describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
+if('parses n as an integer and returns the parsed integer', function() {
     'result(makeInt(80, 1)).toEqual(a parsed b)';
   }
 describe('assumes base 10', 
   function() {
-    'result(makeInt(axb)).toEqual(0)'
-})
+    'result(makeInt(axb)).toEqual(0))'
+}
 
 describe('preserveDecimal(n)', function() {
   'result(preserveDecimal(0, n)'.isEqualto('NaN')';
-  returns('NaN');
   })
 }))
 
