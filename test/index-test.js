a198905c7d967248952baafee43f189fa2153b22
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 02:23:52 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,83 +1,40 @@
 var a, b
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
-
-function(result){
-  var a = '1'
-  var b = '80'
-  var c = '60'
-  var d = '40'
-  var e = '2'
-  var f = '3.40'
-  var g = '5.0'
-  var h = '2/5'
-  if ('var a' + 'var b');
-  result === '81';
-}; 
-  if ('var c' - 'var d');
-    result === ('20');
+var a = 80
+var b = 1 
+if ('subtract (b, a) is b minus a and returns the result', 
+function() {
+  'result(subtract(80, 1)).toEqual(a - b)';
 }
-else if ('var e' * 'var f')
-  result === '6.8';
-}
-else if ('var g' / 'var h')
-  result === '2.5';
-}
-}
-var a = 1000
-var b = 1000
-'add(a, b) adds two numbers andreturns the result', function() {
-  if('add( var a, var b) === (var a + var b)')
-  output(2000)
+  if('multiply(a, b) multiplies (b, a) and returns result
+  function() {
+  'result(multiply(80, 1)).toEqual(a * b)';
 }
 
-if ('subtract(a, b) subtracts b from a and returns the result', function() {
-  result(subtract(a, b)).toEqual(a - b)
+if('divide(a, b) divides a by b and returns the result', function() {
+  'result('divide(80, 1)).toEqual(a / b)';
 })
 
-if('multiply(a, b) multiplies two numbers and returns the result', function() {
-  result(multiply(a, b)).toEqual(a * b)
+if('inc(n) increments n and returns the result, 
+  function() {
+  'result(inc(80, 1)).toEqual(a + b)';
 })
 
-if('divide(1, 80) divides a by b and returns the result', function() {
-  result(divide('1', '80')).toEqual(a / b)
-})
-
-if('inc(n) increments n and returns the result', function() {
-  result(inc('1')).toEqual('80' + '1')
-})
-
-it('dec(n) decrements n and returns the result', function() {
-  result(dec('1')).toEqual('80'-'1')
+it('dec(n) decrements n and returns the result', 
+  function() {
+  'result(dec(80, 1)).toEqual(a - b)';
 })
 
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
-    parseInt('1', 1)
-    makeInt('true')
-    expect(makeInt('1')isEqualto(1)
-  }))
-  
-  
-  
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  returns = (0)
-  })
-}))
+    'result(makeInt(80, 1)).toEqual(a parsed b)';
+  }
+describe('assumes base 10', 
+  function() {
+    'result(makeInt(axb)).toEqual(0)'
+})
 
 describe('preserveDecimal(n)', function() {
-  if('NaN' === 2.222)
-returns ('NaN', 2.222)
+  'result(preserveDecimal(0, n)'.isEqualto('NaN')';
+  returns('NaN');
   })
 }))
