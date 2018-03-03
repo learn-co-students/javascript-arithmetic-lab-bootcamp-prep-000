2cf71e05b8b441ed237449e6b5fc28ad0c37a0d4
Charles Comer <charles.comer68@gmail.com>
2018-03-03 06:10:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(num1, num2){
+  
+  return num1+num2;
+}
+
+function subtract(num1, num2){
+  
+  return num1-num2;
+}
+
+function multiply(num1, num2){
+  
+  return num1*num2;
+}
+
+function divide(num1, num2){
+  
+  return num1/num2;
+}
+
+function inc(n){
+  
+  return ++n;
+}
+
+function dec(n){
+  
+  return --n;
+}
+
+function makeInt(n){
+  
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+
+  return parseFloat(n);
+}
