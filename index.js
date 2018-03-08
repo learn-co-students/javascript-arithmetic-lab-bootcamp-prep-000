2d06e2ef1abc604de1aaa79c30d1cc68065736ae
Jamie Catania <catania.jamie@gmail.com>
2018-03-08 19:40:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply (a, b) {
+  return a * b;
+}
+function divide (a, b) {
+  return a / b;
+}
+function inc (n) {
+  n++;
+  return n;
+}
+function dec (n) {
+  n--;
+  return n;
+}
+function makeInt (num) {
+  return parseInt(num, 10);
+}
+function preserveDecimal (num) {
+  return parseFloat(num);
+}
