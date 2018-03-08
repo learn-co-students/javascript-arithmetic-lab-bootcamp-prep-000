72dee78d56d83b980c8a9b7cff0bf3fa71e859d4
karagarvey <kgarvey87@gmail.com>
2018-03-08 21:23:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+  return (a+b)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function multiply(a,b) {
+  return (a*b)
+}
+
+function divide(a,b) {
+  return (a/b)
+}
+
+function inc(n) {
+  return a++
+}
+
+function dec(n) {
+  return a--
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
