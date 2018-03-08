bdbfaaf0968f91917b93e88b9e2f8b2ce782f99c
David Teitelbaum <david.teitelbaum@wework.com>
2018-03-08 14:39:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+   parseInt('n');
+ return Number(n);
+ return NaN;
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return Number(n);
+return NaN;
+}
