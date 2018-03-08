f19529bcd2b4d821036be6075a0ba30a05e734e1
karagarvey <kgarvey87@gmail.com>
2018-03-08 21:34:12 +0000

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,17 @@ function divide(a,b) {
 }
 
 function inc(n) {
-  return a++
+  return ++n
 }
 
 function dec(n) {
-  return a--
+  return --n
 }
 
 function makeInt(n) {
-  parseInt(n)
-  return n
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  parseFloat(n)
-  return n
+  return parseFloat(n)
 }
