a7c8d02642fc9d4b08720fd5a07ddbed61e8aa94
Oren Magid <autoimpedicis@gmail.com>
2018-03-10 11:27:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  parseInt("n", 10)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat("n")
+  return n
+}
