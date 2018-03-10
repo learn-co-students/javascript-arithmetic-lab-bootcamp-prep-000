affa10d1374de4d97833928bcd7ff960421ab252
Oren Magid <autoimpedicis@gmail.com>
2018-03-10 14:10:53 +0000

    Done.

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt("n", 10)
-  return n
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  parseFloat("n")
-  return n
+  return parseFloat(n)
 }
