e8fd9e62e9b99da13cb4380b1a5cbb0910b89323
Valerie Hudson <vhudsonphoto@gmail.com>
2018-03-12 19:56:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+var Number
+function add(a,b) {
+  Number = a+b;
+  return Number;
+}
+
+var Number
+function subtract(a,b) {
+  Number = a-b;
+  return Number;
+}
+
+var Number 
+function multiply(a,b) {
+  Number = a*b;
+  return Number;
+}
+
+var Number
+function divide(a,b) {
+  Number = a/b;
+  return Number;
+}
+
+var Number
+function inc(n) {
+  Number = n+1;
+  return Number;
+}
+
+ var Number
+ function dec(n) {
+   Number = n-1;
+   return Number;
+ }
+ var Number
+ function makeInt(n) {
+   Number = parseInt(n, 10);
+   
+   return Number;
+ }
+ 
+ function preserveDecimal(n) {
+   var Number;
+   Number = parseFloat(n)
+   return Number;
+ };
+
