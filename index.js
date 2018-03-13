9ed4c698ab4c9a17d1b564209ed64c58ebfc8eee
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-13 16:41:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function inc(n){
+  return n+1;
+}
+function dec(n){
+  return n-1;
+}
+function makeInt(n){
+  return parseInt(n);
+}
+function makeInt(n,b){
+  b=10;
+  return parseInt(n,b);
+}
+function preserveDecimal(n){
+
+  return parseFloat(n);
+}
