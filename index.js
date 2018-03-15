af7202ef4e535459064e3b94c471962de6cecd3e
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-15 02:52:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+
+}
+
+function multiply(a,b) {
+  return a*b
+
+}
+
+function divide(a,b) {
+  return a/b
+
+}
+
+function inc(n) {
+  n++
+  return n
+
+}
+
+function dec(n) {
+  n--
+  return n
+
+}
+
+function makeInt(n) {
+  return n = parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return n = parseFloat(n)
+
+}
