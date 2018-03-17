4bea183d4596dfdc36d73f17a63bdbe14c23efbb
gracey12051@gmail.com <gracey12051@gmail.com>
2018-03-17 20:47:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc(a){
+  a++
+  return a
+ }
+
+function dec(a){
+  a--
+  return a
+}
+function makeInt(n){
+  n = parseInt(n,10)
+  return n
+}
+
+function preserveDecimal(n){
+    n = parseFloat(n)
+  return n
+}
+
