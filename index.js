4f85f2ffb1763717945715e9a2bba393e992706d
Nancy Jo Chang <enciie@gmail.com>
2018-03-26 22:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function divide(a,b){
   return a/b
 }
 function inc(a){
-  return a + 1
+  return a += 1
 }
 function dec(a){
-  return a - 1
+  return a -= 1
 }
 function makeInt(a){
   return parseInt(a, 10)
