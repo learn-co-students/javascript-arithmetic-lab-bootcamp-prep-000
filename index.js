033bed5e449dc6e223ff4790438dfc129786f857
Jackie First <jackiejeep@gmail.com>
2018-03-27 19:56:06 +0000

    Done.

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ function dec(n) {
 function makeInt(n) {
   return parseInt(n, 10);
 }
-function makeInt(n) {
-  return parseInt(n, 10);
-}
-function preserveDecimal(n) {
-  return parseFloat(n);
-}
 function preserveDecimal(n) {
   return parseFloat(n);
 }
