82e2361fa65afe73e801b35ab2532c673d9758d7
Rommy Elyan <relyan@email.arizona.edu>
2018-03-29 16:21:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function inc(n) {
+  n += 1
+  return n
+}
+
+function dec(n) {
+  n -= 1
+  return n
+}
+
+function makeInt(n) {
+  n = parseInt(n, 10);
+  return n
+}
+
+function preserveDecimal(n) {
+  n = parseFloat(n)
+  return n
+}
+
