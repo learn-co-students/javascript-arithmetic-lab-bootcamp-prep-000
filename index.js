3e505390bfeb7f0cea35ffa9fc1001b817ac4bde
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-03-29 15:46:19 +0000

    Done.

--- index.js
+++ index.js
@@ -15,5 +15,21 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n++
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
