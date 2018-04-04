d79bb58876e564ee11db169a8819a8e502dd32c0
Nicholas Powers <njpowers7915@gmail.com>
2018-04-04 13:52:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  var number = n += 1;
+  return number;
+}
+
+function dec(n) {
+  var x = n -= 1;
+  return x;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function makeInt(n) {
+  return parseInt('sdf', 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat('sfsdf');
+}
