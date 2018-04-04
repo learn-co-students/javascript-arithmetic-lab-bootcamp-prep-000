59e48d0d1a5ed8eae568aac7c16458675d69621e
bladeburris@gmail.com <bladeburris@gmail.com>
2018-04-04 19:12:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a*b 
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(a) {
+  a += 1
+  return a
+}
+
+function dec(a) {
+  a -= 1 
+  return a
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
