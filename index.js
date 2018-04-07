adf971787f4346f0452f41ebc75b2f47055068af
Jon Marquez <jon.marquez9258@gmail.com>
2018-04-07 15:27:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add (x,y) {
+  return x + y;
+}
+add(1,80);
+function subtract (x,y) {
+  return x - y;
+}
+subtract (60,40);
+function multiply (x,y) {
+  return x * y;
+}
+multiply (2,3.4)
+function divide (x,y) {
+return x / y;
+}
+divide (5.0,2.5)
+function inc(n) {
+  return n +=1;
+}
+function dec(n) {
+  return n -=1;
+}
+function makeInt(n) {
+  return parseInt(n,10);
+}
+function preserveDecimal(n) {
+return parseFloat(n);
+}
