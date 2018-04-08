91a4171e95b87baa3c944496aa75afb164f20d25
Natalie Halloran <natalie.halloran@yahoo.com>
2018-04-08 21:14:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b) {
+  var sum = a + b;
+  return sum;
+}
+function subtract(a,b) {
+  var diff = a - b;
+  return diff;
+}
+function multiply(a,b) {
+  var mult = a*b;
+  return mult;
+}
+function divide(a,b) {
+  var div = a/b;
+  return div;
+}
+function inc(n) {
+  n++;
+  return n;
+}
+function dec(n) {
+  n--;
+  return n;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
