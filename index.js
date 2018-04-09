902702606e13558a597bcfef736c8872494819dc
Kvasemi <alancho@gmail.com>
2018-04-09 13:41:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return parseInt(a, 10) + parseInt(b, 10);
+}
+
+function subtract(a, b) {
+  return parseInt(a, 10) - parseInt(b, 10);
+}
+
+function multiply(a, b) {
+  return parseInt(a, 10) * parseInt(b, 10);
+}
+
+function divide(a, b) {
+  return parseInt(a, 10) / parseInt(b, 10);
+}
+
+function inc(n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(a) {
+  return parseInt(a, 10);
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a);
+}
