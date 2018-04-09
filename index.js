143b307d43737f3f1ff4c64289b9f9724aadf6f1
srideviqa7@gmail.com <srideviqa7@gmail.com>
2018-04-09 04:16:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var a
+var b
+ 
+function add(a,b) {  return a+b}
+function subtract(b,a) {return b-a }
+function multiply(b,a) {return a*b }
+function divide(a,b) {return a/b }
+function inc(a) {return a +1}
+function dec(a){return a-1}
+function makeInt(a){return a}
+function makeInt(a) {  return parseFloat(a) }
+function parseInt(n,b){return makeInt (0)}
+function preserveDecimal(n){return 'n'}
+function preserveDecimal(n){return parseFloat(n)}
