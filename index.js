74d51508c01db5ca71a4834a3a892cd9289401ca
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-10 22:14:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+let add = (a,b) => a+b;
+let subtract = (a,b) => a-b;
+let multiply = (a,b) => a*b;
+let divide = (a,b) => a/b;
+let inc = (n) => ++n;
+let dec = (n) => --n;
+let makeInt = (n) => parseInt(n, 10);
+let preserveDecimal = (n) => parseFloat(n);
