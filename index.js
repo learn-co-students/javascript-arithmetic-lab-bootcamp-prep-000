52e58a95768f51f6aab5a675dff739113440ae84
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-11 02:00:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b
+}
+add(1+2)
+
+function subtract(a, b) {
+  return a - b
+}
+subtract(2-1)
+
+function multiply(a, b) {
+  return a * b
+}
+multiply(2-1)
+
+function divide(a, b) {
+  return a / b
+}
+divide(2/1)
+
+function inc(n) {
+  return  n += r
+}
+inc(6)
