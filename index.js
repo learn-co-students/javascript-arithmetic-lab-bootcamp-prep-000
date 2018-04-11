cd440415706bde640a5b8a9110cbc762a6f643ce
Simon Ip <waterloo178@yahoo.com>
2018-04-11 00:26:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var a, b;
+
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a*b;
+} 
+
+function divide (a, b) {
+  return a/b;
+}
+var a;
+
+function inc (a) {
+  return a += 1;
+}
+
+function dec (a) {
+  return a -= 1;
+}
+
+var n = 3.14;
+
+function makeInt (n) {
+  return parseInt(n, 10);
+}
+
+var n = 9.999;
+
+function preserveDecimal (n) {
+  return parseFloat (n);
+}
