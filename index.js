bb54737eddedb74ec7e451e6e75dc85f80745974
Wasif Khan <waasif.khaan@gmail.com>
2018-04-12 01:03:15 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function divide(a, b) {
 function inc(n) {
   return n+1
 };
+function dec(n) {
+  return n-1
+};
+
+function makeInt(n) {
+  return parseInt(n)
+};
+
+function preserveDecimal(n) {
+return parseFloat(n)
+};
