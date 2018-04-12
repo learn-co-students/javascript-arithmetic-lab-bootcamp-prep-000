d2d430301071e229caff20c41384bddea0ebb3fb
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-04-12 21:50:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return parseInt(a)+parseInt(b);
+}
+function subtract(a,b){
+  return parseInt(a)-parseInt(b);
+}
+function multiply(a,b){
+  return parseInt(a)*parseInt(b);
+}
+function divide(a,b){
+  return parseInt(a)/parseInt(b);
+}
+
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
