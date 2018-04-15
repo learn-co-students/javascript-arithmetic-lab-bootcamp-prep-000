700edd182bda319a356bf02a2a6e4822546461bf
Michael Ballard <ballard.michael9@gmail.com>
2018-04-15 13:57:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+// variables for the excersises
+
+var a, b;
+
+// the  add function 
+function add(a,b){
+  return a + b
+};
+
+// the subtract function
+function subtract(a,b){
+  return a - b 
+};
+
+// the multiply function
+function multiply(a,b){
+  return a * b
+};
+
+// the divide function
+function divide(a,b){
+  return a / b
+};
+
+// the increment function
+function inc(n){
+  var result = n
+  result++
+  return result
+};
+
+// the deincrement function
+function dec(n){
+  var result = n
+  result--
+  return result
+};
+
+// the makeint() function
+function makeInt(n){
+  var number = n
+  return parseInt(number,10)
+};
+
+// the preserveDecimal function
+function preserveDecimal(n){
+  var number = n
+  return parseFloat(number,)
+  
+}
