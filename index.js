870731cef5304b4dcd140384a9a98c2c64de1ba6
Justin <jkimm516@gmail.com>
2018-04-17 08:41:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(a){
+  var n = a
+  parseInt(n, 10) 
+  return n
+} 
+
+function preserveDecimal(a){
+  var n = a
+  parseFloat(n)
+  return n
+}
