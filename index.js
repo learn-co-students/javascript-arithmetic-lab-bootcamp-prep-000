64ec90682fe18b0e246de88ee97a999c22c37ad0
Michael Eby <michaeleby1@gmail.com>
2018-04-18 04:49:34 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n += 1
+ return n += 1
 }
 
-function inc(n) {
+function dec(n) {
   return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
 }
