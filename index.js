b3c0aa3b97b2bbb81e64e1280c7c5a9b3d0b8eca
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-04-18 17:04:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+
+function inc(n){
+  return (n + 1)
+}
+
+function dec(n){
+  return (n - 1)
+}
+
+function makeInt(num){
+  
+  var newNum = parseInt(num, 10)
+  return newNum
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
