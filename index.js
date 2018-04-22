30ab217f7045a6048beaea518c4773066e9692a4
Oday <oday.altimimi@gmail.com>
2018-04-22 19:49:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b 
+}
+function multiply(a,b) {
+  return parseInt(a) * parseInt(b)
+}
+
+function divide(a,b){
+  return a / b 
+}
+
+function inc(n){
+return ++n
+}
+
+function dec(n){
+return --n
+}
+
+function makeInt(n) {
+  return parseInt(n) 
+  }
+  
+  function preserveDecimal(n) {
+    parseFloat(n)
+  }
+  
+  function preserveDecimal(a){
+  return parseFloat(a, 10)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+  
+  
