9efb41d02f6902d2ffffc4dff1956ea65536caee
Charity Jackson <charitylennjackson@yahoo.com>
2018-04-24 19:31:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+1 = 80 
+60 - 40 
+2 * 3.4
+5.0 / 2.5 
+
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+//reset number
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
+
