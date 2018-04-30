c0a322fe824b1f5fa9dfea6d282b6ed0d2ba2651
drewisatlas@gmail.com <drewisatlas@gmail.com>
2018-04-30 20:37:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(a,b) {
+  return a + b;
+  
+}
+
+add(1,80)
+
+function subtract(a,b) {
+  return a - b;
+
+}
+
+subtract(60,40)
+
+function multiply(a,b) {
+  return a * b;
+  
+}
+
+multiply(2, 3.4)
+
+function divide(a,b) {
+  return a / b;
+  
+}
+
+divide(5.0, 2.5)
+
+
+function inc(a) {
+  return a + 1;
+  
+}
+
+inc(5)
+
+function dec(a) {
+  return a - 1;
+  
+}
+
+dec(5)
+
+function makeInt(a) {
+  return parseInt(a, 10);
+  
+}
+
+makeInt(3.256)
+
+function preserveDecimal(a) {
+  return parseFloat(a);
+}
+
+preserveDecimal(10.1235235)
