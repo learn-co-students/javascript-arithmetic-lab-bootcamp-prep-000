1675e758f6d25ce9f44609f82fd799f0049e3897
Joseph Milliken <joseph97milliken@gmail.com>
2018-05-02 00:18:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const inc = n => n + 1;
+const dec = n => n - 1;
+
+const makeInt = n => parseInt(n, 10);
+const preserveDecimal = n => parseFloat(n);
