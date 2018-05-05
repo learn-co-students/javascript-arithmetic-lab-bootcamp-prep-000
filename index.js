fcfe1405b02fd3b62c9631a2e03b9d4f0ba01009
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-05 00:27:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function inc(a){
+  return a+1
+}
+
+function dec(a){
+  return a-1
+}
+
+function makeInt(n) {
+  var a = parseInt(n, 10)
+  return a
+}
+
+function preserveDecimal(n){
+  var a=parseFloat(n)
+  return a
+}
