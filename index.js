f2f6859e21ea8962a6e745adbd751df65a4432a0
Paul Bomba` <paul@paulbomba.com>
2018-05-05 19:39:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+let a = 2;
+let b = 3;
+let n;
+
+function add(a,b) {
+  return a + b;
+}
+
+function subtract (a,b){
+  return a - b;
+}
+
+function multiply (a, b){
+  return a * b;
+}
+
+function divide (a, b){
+  return a / b;
+}
+
+function inc(n){
+  n++;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+ }
+
+function preserveDecimal(n) {
+  parseFloat(n);
+  return +n;
+}
