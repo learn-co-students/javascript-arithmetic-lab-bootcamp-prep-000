c0fed94ce50a241fdcf703c4b4615370f2d53163
Alvin Galit <aroblesgalit@gmail.com>
2018-05-11 04:29:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function makeInt(n, 10) {
+  return parseInt(n, 10);
+}
+
+function makeInt(n) {
+  return parseInt('nonsense!');
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat("hi");
+}
