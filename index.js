6f1938aed526ba20ae5628a8d59dd4cc9b1192aa
Edwin Melendez <boricuababyhuey@gmail.com>
2018-05-13 02:02:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+var number
+
+//function add
+function add(a,b){
+  number = a + b
+return number
+}
+add();
+
+//function subtract
+function subtract(a,b){
+  number = a - b
+return number
+}
+subtract();
+
+//function multiply
+function multiply(a,b){
+  number = a * b
+return number
+}
+multiply();
+
+//function divide
+function divide(a,b){
+  number = a / b
+return number
+}
+divide();
+
+//function increment
+function inc(n){
+  return n+=1
+}
+inc();
+
+//function decrement
+function dec(n){
+  return n-=1
+}
+dec();
+
+//function parse
+function makeInt(n){
+  return parseInt(n, 10)
+}
+makeInt(n);
+
+//function float 
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+preserveDecimal();
+
