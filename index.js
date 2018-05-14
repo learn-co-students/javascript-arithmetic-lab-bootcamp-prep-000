be391f7e128d806808197f58fa63bcdf23a8d17c
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-14 23:22:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+  var result = a + b
+  return result
+}
+function subtract(a, b) {
+  var result = a - b
+  return result
+}
+function multiply(a, b) {
+  var result = a * b
+  return result
+}
+function divide(a, b) {
+  var result = a / b
+  return result
+}
+function inc(n) {
+  var result = ++n
+  return result
+}
+function dec(n) {
+  return --n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
