1900b7cfbb3ab68dddf565e4b9c916e878906334
Nancy D <nancydo7@gmail.com>
2018-05-14 02:58:49 +0000

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,27 @@ function divide(a, b){
   return a / b
 }
 function inc(n){
-  return n + 1
+  return n += 1
 }
 function dec(n){
-  return n - 1
+  return n -= 1
 }
 function makeInt(n){
   return parseInt(n, 10) 
 }
+
 function preserveDecimal(n){
   return parseFloat(n)
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
 }
