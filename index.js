986e938b2714344bb86d8d4519c3b98f7d2a438d
Shafi Khalisdar <shafi.khalisdar@gmail.com>
2018-05-16 20:48:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+
+  var n = a + b;
+    return n;
+};
+
+function subtract(a, b) {
+  var n = a - b;
+  return n;
+};
+
+function multiply(a, b) {
+  var n = a * b;
+  return n;
+};
+
+function divide (a, b) {
+  var n = a / b;
+  return n;
+};
+
+function inc(n) {
+  var n = n + 1;
+  return n;
+};
+
+function dec(n) {
+  var n = n - 1;
+  return n;
+};
+
+function makeInt(n) {
+  return parseInt(n, 10);
+};
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+};
