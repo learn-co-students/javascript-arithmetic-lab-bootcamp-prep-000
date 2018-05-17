3d47116eecd2720359930b36f2845b5824def7e0
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-17 04:34:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+  return(a+b)
+}
+add(5,3)
+
+function subtract(c,d) {
+  return(c-d)
+}
+subtract(5,1)
+
+function multiply(e,f) {
+  return(e*f)
+}
+multiply(5,10)
+
+function divide(x,y) {
+  return(x/y)
+}
+divide(10,5)
+
+
+function inc(n) {
+  
+  return ++n;
+}
+inc(5)
+
+function dec(n) {
+  return --n;
+}
+dec(5)
+
+function makeInt(n) {
+  return parseInt(n, 10)
+} 
+
+
+function preserveDecimal(n) {
+return parseFloat(n)  
+}
