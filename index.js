df26fe119f17ed7a44262a19a276095360428422
Souji 2613 <puppypoola@gmail.com>
2018-05-18 02:48:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(n){
+  n++ ;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  if(parseInt(n)) {
+    return n;
+  }
+}
+function preserveDecimal(n){
+  if(parseFloat(n) ){
+    return n;
+  }
+  
+}
--- test/index-test.js
+++ test/index-test.js
@@ -35,7 +35,7 @@ describe('makeInt(n)', function() {
   })
 
   it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
+    expect(makeInt('0x2328')).toEqual(9000)
   })
 
   it('returns NaN as appropriate', function() {
@@ -45,7 +45,7 @@ describe('makeInt(n)', function() {
 
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
+    expect(preserveDecimal('2.222')).toEqual(2.222)
   })
 
   it('returns NaN as appropriate', function() {
