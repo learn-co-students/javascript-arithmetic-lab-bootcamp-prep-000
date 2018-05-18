22efa53622a676c3d67f0b4dec9acbc47144a59f
Nathan Hill <nateohill@gmail.com>
2018-05-18 19:23:08 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -47,8 +47,8 @@ describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
   })
-
+  
   it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+    expect(isNaN(preserveDecimal('asdfasdf'))).toEqual(true);
   })
 })
