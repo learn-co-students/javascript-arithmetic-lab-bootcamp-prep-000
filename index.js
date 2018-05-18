9583c28689023b7298f31e92c621b0bb264ccc5f
Esayas Gebre <dave183278@gmail.com>
2018-05-18 11:35:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a,b)
+{
+  var sum= a+b;
+  return sum;
+}
+
+function subtract(a,b)
+{
+  var subtract= a-b;
+  return subtract;
+}
+
+function divide(a,b)
+{
+  var sum= a/b;
+  return sum;
+}
+
+function multiply(a,b)
+{
+  var sum= a*b;
+  return sum;
+}
+
+
+function inc(a)
+{
+  a=a+1;
+  return a;
+}
+
+function dec(a)
+
+{
+  a=a-1;
+  return a;
+}
+
+function makeInt(n)
+{
+  n=parseInt(n,10);
+  return n;
+}
+
+function preserveDecimal(n)
+{
+  n=parseFloat(n);
+  return n;
+}
+
+
