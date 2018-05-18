b93bcb8b54867fb14a55b0c9c3951072d66ca30a
Angelica Fields <angelicafields08@gmail.com>
2018-05-18 16:38:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  a+b
+  return a+b
+}
+
+function subtract(a,b){
+  a-b
+  return a-b
+}
+
+function multiply(a,b){
+  a*b 
+  return a*b
+}
+
+function divide(a,b){
+  a/b 
+  return a/b
+}
+
+function inc(n){
+  n++
+  return n++
+}
+
+function dec(n){
+  n--
+  return n--
+}
+
+function makeInt(n){
+  parseInt(n,10)
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  parseFloat(n)
+  return parseFloat(n)
+}
