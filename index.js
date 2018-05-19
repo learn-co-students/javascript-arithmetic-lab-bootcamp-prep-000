d2be23e9d4f241b34a6c7e9e0dd1b54d8bd4f0a9
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-19 23:47:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add (a, b) {
+  return (a + b)
+}
+function subtract (a, b) {
+  return (a - b)
+}
+function multiply (a, b) {
+  return (a * b)
+}
+function divide (a, b) {
+  return (a / b)
+}
+function inc (a) {
+  return (a + 1)
+}
+function dec (a) {
+  return (a - 1)
+}
+function makeInt(n) {
+ return parseInt (n, 10)
+}
+// '0*2328'=== 0
+// if (parseInt === isNan) {
+//   return ('true')
+// }
+function preserveDecimal(n) {
+  return parseFloat (n)
+}
+  // 'n' === n
+  // isNan === true
+
+
+ 
+
