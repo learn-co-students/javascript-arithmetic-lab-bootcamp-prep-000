602ebdd5f775c4880a9c4eecd6ec7ab03abf4911
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-19 19:48:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+  return(a + b)
+}
+
+function subtract (a,b) {
+  return (a - b)
+}
+
+function multiply (a,b) {
+  return (a * b)
+}
+
+function divide (a,b) {
+  return (a / b)
+}
+
+var n = 2.222
+
+function inc(n) {
+  return(n + 1)
+}
+
+function dec(n) {
+  return(n - 1)
+}
+
+function makeInt(a, b) {
+  return(parseInt(a, 10))
+} 
+
+function preserveDecimal(n) {
+  return(parseFloat (n))
+}
