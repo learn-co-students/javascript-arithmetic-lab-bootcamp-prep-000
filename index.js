6b69752e5a98eb6440d19ce04d8ae51f694deb95
Mike Walker <jerry.kowal@gmail.com>
2018-05-19 02:55:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function makeInt(n) {
+  var number = parseInt(n, 10)
+  return number
+}
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  var number = n
+  number ++
+  return number
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+function dec(n) {
+  var number = n
+  number --
+  return number
+}
