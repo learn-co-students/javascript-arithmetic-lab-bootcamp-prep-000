f5f5a8f5d8e8a5929a6127a9b6e83472a4e6d09d
Rafael Lopez <lpz.raf@gmail.com>
2018-05-19 03:45:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+  return num1 + num2;  
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;  
+}
+
+
+function divide(num1, num2) {
+  return num1 / num2;  
+}
+
+function inc(num1) {
+  return num1++;  
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;  
+}
