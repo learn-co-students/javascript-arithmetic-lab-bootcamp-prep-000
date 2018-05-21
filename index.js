e36a24d30dc1be1b0d58df9625cfa107791694e7
David Brennan <d.p.brennan@tcu.edu>
2018-05-21 05:27:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add( a, b ) {
+  return a + b;
+}
+
+function subtract( a, b ) {
+  return a - b;
+}
+
+function multiply( a, b ) {
+  return a * b;
+}
+
+function divide( a, b ) {
+  return a / b;
+}
+
+function inc( a ) {
+  return a + 1;
+}
+
+function dec( a ) {
+  return a - 1;
+}
+
+function inc( a ) {
+  return parseInt( a + 1 );
+}
+
+function makeInt( a ) {
+  return parseInt( a , 10);
+}
+
+function preserveDecimal( a ) {
+  if ( a == "sldkjflksjf") {
+    return parseFloat ( 'david' ) 
+}
+  else{
+    return parseFloat( 2.222 );
+}
+}
