5559f71a5e03beaa9e4b15be4dfaef2ed4965707
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-22 23:45:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var a = 3; 
+var b = 9;
+
+function add(a, b) {
+    return a + b; 
+}
+function subtract(b, a) {
+    return b - a; 
+}
+function multiply(a, b) {
+    return a * b; 
+}
+function divide(b, a) {
+    return b / a; 
+}
+
+var number = 10;
+
+function inc(number) {
+  return number += 1; 
+}
+
+function dec(number) {
+  return number -= 1; 
+}
+
+function makeInt(n) {
+  var parsed = parseInt('2', 10);
+}
+
+function makeInt(n) {
+  var parsed = parseInt('2.2222', 10);
+  return n; 
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
