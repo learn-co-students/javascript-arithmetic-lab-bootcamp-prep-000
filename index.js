2f21d6ac777856f9380e78d0698b67228eaec137
Jesus Garcia <jgarcia.network@gmail.com>
2018-05-24 23:07:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  var x = a+b
+  return x
+}
+
+function subtract(a,b){
+  var x = a-b
+  return x
+}
+
+function multiply(a,b){
+  var x = a*b
+  return x
+}
+
+function divide(a,b){
+  var x = a/b
+  return x
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  var x = parseInt(n, 10);
+  return x
+}
+
+function preserveDecimal(n){
+  let v = parseFloat(n);
+  return v;
+}
