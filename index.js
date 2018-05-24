c8d721acb5aeda33c82604154805902907097aaf
Rosa Tu <rosalina.tu@gmail.com>
2018-05-24 15:13:59 +0000

    Done.

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ function dec(n)
   
 function makeInt(n) 
 {
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 }  
 
 function preserveDecimal(n)
 {
-  return parseFloat('n')
+  return parseFloat(n)
 }
