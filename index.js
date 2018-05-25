8f1eaf13cd61de501cd48d8a7dfc84ed5230a818
Yohannes Abera <yohannesns@gmail.com>
2018-05-25 15:12:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+  function add(a,b){
+    return a+b
+  }
+  function subtract(a,b){
+    return a-b
+  }
+  function multiply(a,b){
+    return a*b
+  }
+  function divide(a,b){
+    return a/b
+  }
+  function inc(n){
+    return n = n+1
+  }
+  function dec(n){
+    return n = n-1
+  }
+  function makeInt(n){
+    if(n!=isNaN){
+    return parseInt(n,10)
+   }
+   else{
+     return NaN
+   }
+ }
+  function preserveDecimal(n){
+    return parseFloat(n)
+  }
