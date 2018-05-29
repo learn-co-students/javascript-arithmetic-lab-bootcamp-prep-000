4944a6bbc503ff38170feed0cb3926e47c850779
Tom De Vries <tdevries60@gmail.com>
2018-05-29 19:53:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b) {
+  var number
+  number = a + b
+  return(number)
+}
+
+function subtract(a, b) {
+  var number
+  number = a - b
+  return(number)
+}
+
+function multiply(a, b) {
+  var number
+  number = a * b
+  return(number)
+}
+
+function divide(a, b) {
+  var number
+  number = a / b
+  return(number)
+}
+
+function inc(n) {
+  var number
+  number = ++n
+  return(number)
+}
+
+function dec(n) {
+  var number
+  number = --n
+  return(number)
+}
+
+function makeInt(n) {
+  var number
+  number = parseInt(n,10)
+  return(number)
+}
+
+function preserveDecimal(n) {
+  var number
+  number = parseFloat(n)
+  return(number)
+}
