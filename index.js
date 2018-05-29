dac52bd4668d3e8ad73a9076970bd8967aa8ae96
John Higgins <johnhiggins@outlook.ie>
2018-05-29 16:44:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,66 @@
+function add (a,b){
+  var result = a + b; 
+  return result;
+}
+
+function subtract (a,b){
+  var result = a - b; 
+  return result;
+}
+
+function multiply (a,b){
+  var result = a * b; 
+  return result;
+}
+
+function divide (a,b){
+  var result = a / b; 
+  return result;
+}
+
+function inc (n){
+  n++; 
+  return n;
+}
+
+function dec (n){
+  n--; 
+  return n;
+}
+
+
+
+
+
+function makeInt (n){
+  var result = parseInt ('0x2383')
+  return result;
+}
+
+
+
+function preserveDecimal (n){
+  parseFloat ('n');
+  return n;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
