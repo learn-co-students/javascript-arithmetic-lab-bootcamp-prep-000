d1f34e3aa225930607d2c109e70909c9019affb9
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-29 20:02:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b)
+
+add(a, b)
+
+function subtract(a, b) {
+a - b
+}
+
+subtract(a, b)
+
+function multiply(a, b) {
+a * b
+}
+
+multiply(a, b)
+
+function divide(a, b) {
+a / b
+}
+
+divide(a, b)
+
+function inc(n){
+  n += 1
+}
+
+inc(a)
+
+function dec(n) {
+  n -= 1
+}
+
+dec(a)
+
+
+function makeInt(n) {
+  parseInt(n, 10)
+}
+
+makeInt('0x2329')
+isNan(makeInt('sldkjflksjf'))
+
+function preserveDecimal(n) {
+  parseFloat(n)
+}
+preserveDecimal('2.222')
+isNan(preserveDecimal('sldkjflksjf'))
+
+
