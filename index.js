2e0c4e1efb9522bf01cef316751189e95462ad06
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-05-30 20:33:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(){
+var value= 1 + 80;
+ return(value);
+}
+
+function subtract(){
+ var value= 60 - 40;
+  return(value);
+}
+
+function multiply(){
+var value= 2 * 3.4;
+return(value);
+}
+
+function divide(){
+var value= 5.0 / 2.5;
+return(value);
+}
