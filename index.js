89edd25305a4b4350476172b0ce974508db47d8b
Michael Carlos <michaelpcarlos@gmail.com>
2018-05-30 17:15:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,60 @@
+var a = 1;
+var b = 80;
+
+function add(a, b) {
+  return a + b;
+}
+
+var a = 60;
+var b = 40;
+
+function subtract(a, b) {
+  return a - b;
+}
+
+var a = 2;
+var b = 3.4;
+
+function multiply(a, b) {
+  return a * b;
+}
+
+var a = 5.0;
+var b = 2.5;
+
+function divide(a, b) {
+  return a / b;
+}
+
+var n = 5;
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+var n = 5;
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+var n = '2';
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+var n = 2.2;
+
+function preserveDecimal(n) {
+  return parseInt(n, 4);
+}
+
+var n = 2.2;
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
