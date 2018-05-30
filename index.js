f96731d46928f5453d3ca78a7ac48696103131d8
Matt Nicholas <fostermatt82@gmail.com>
2018-05-30 15:41:52 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function multiply(num1, num2){
 function divide(num1, num2){
   return num1 / num2;
 }
+
+function inc(num){
+  return ++num;
+}
+
+function dec(num){
+  return --num;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
