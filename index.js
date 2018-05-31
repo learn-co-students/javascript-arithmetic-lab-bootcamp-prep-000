9d638857186da38295bf18ac03dcb15ecf23e1ec
Jonathan Fernandez <jfern20@gmail.com>
2018-05-31 02:32:02 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(a,b){
 }
 
  function inc(a){
-   return  a + 1
+   return  a += 1
  }
 
  function dec(a){
-   return a - 1
+   return a -= 1
+ }
+ 
+ function makeInt(n){
+   return parseInt(n, 10)
+ }
+ 
+ function preserveDecimal(n){
+   return parseFloat (n)
  }
