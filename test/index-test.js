c424c5f060d5eba2122d997ce701358420495a6f
Anthony Udezi <tonywaka@uniben.edu>
2018-06-03 01:40:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,6 +3,7 @@ var a, b
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
+  console.log(a + b)
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
