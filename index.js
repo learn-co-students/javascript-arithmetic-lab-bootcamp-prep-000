e8e10b276504f74059c7dff6f9ac02e7331a3f4b
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-03 22:00:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+function inc(n) {
+  n += 1
+  return n
+}
+
+function dec(n) {
+  n-=1
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
