df44a97b53b40fd7f80c30221b51ab41e918dda2
Aiden Loyd <aidensaloyd@gmail.com>
2018-06-04 12:04:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(lhs, rhs) {
+  return lhs + rhs;
+}
+
+function subtract(lhs, rhs) {
+  return lhs - rhs;
+}
+
+function multiply(lhs, rhs) {
+  return lhs * rhs;
+}
+
+function divide(lhs, rhs) {
+  return lhs / rhs;
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
