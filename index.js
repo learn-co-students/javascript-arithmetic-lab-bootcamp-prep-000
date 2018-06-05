3f620844d024e00b6170da9e0f686ffa48ded1f9
michael <michaeltilman77@gmail.com>
2018-06-05 17:51:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(firstNum, secondNum){
+  return firstNum+secondNum
+}
+function subtract(firstNum,secondNum){
+  return firstNum - secondNum
+}
+function multiply(firstNum,secondNum){
+  return secondNum*firstNum
+}
+function divide(nominator,denominator){
+  return nominator/denominator
+}
+function inc(number){
+  return ++number
+}
+function dec(number){
+  return --number
+}
+function makeInt(a){
+  return parseInt(a, 10);
+}
+function preserveDecimal(b){
+  return parseFloat(b);
+}
+
+
