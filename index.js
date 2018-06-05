96c85a3c013f321138151c978e1eb8f2bdc88988
BHAGYARAJ HARANALE <bhagyarajharanale34@gmail.com>
2018-06-05 04:19:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var a,b
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(a){
+  return ++a
+}
+function dec(a){
+  return --a
+}
+function makeInt(n){
+  return parseInt(n)
+} 
+function makeInt(n){
+  return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
