55985b0e81e1dc7560275a63d71816d936cea326
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-05 21:43:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(x, y) {
+  var product = x + y;
+  return product;
+}  
+
+function subtract(x,y){
+  var product = x - y;
+  return product;
+}
+
+function multiply(x,y){
+  var product = x * y;
+  return product;
+}
+
+function divide(x,y){
+  var product = x / y;
+  return product;
+}
+
+function inc(n){
+  var product = n + 1;
+  return product;
+}
+
+function dec(n){
+  var product = n - 1;
+  return product;
+}
+
+
+function makeInt(n){
+  var product = parseInt(n)
+  return product;
+}
+
+function makeInt(n){
+  var product = parseInt(n , 10)
+  return product;
+}
+
+
+function preserveDecimal(n){
+  var product = parseFloat(n)
+  return product;
+}
+
