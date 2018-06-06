14380ea8599c7c15269fe8ebdc9d8300f0537eaf
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-06 17:02:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var number = 10
+var a = 2, b = 10;
 
-function add5() {
-  number += 5
+function add(a , b) {
+  return a + b
 }
+function subtract(a , b) {
+  return a - b 
+}
+function multiply(a , b) {
+  return a * b 
+}
+function divide(a , b) {
+  return a / b 
+}
+function inc(a) {
+   return a += 1
+}
+function dec(a) {
+  return a -= 1
+}
+var n = "4"
 
-function divideBy3() {
-  number /= 3 
+function makeInt(n) {
+  return parseInt(n , 10)
+}
+var f = "2.364"
+
+function preserveDecimal(f) {
+  return parseFloat(f)
 }
