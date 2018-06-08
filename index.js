5bae428855f8b2880ff7ca8d098e1633c97829f9
Michelle Kim <pasolre95kim@gmail.com>
2018-06-08 20:01:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  return a + b
+}
+function subtract(b,a) {
+  return b - a
+}
+
+function multiply(a,b){
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+function inc(n) {
+  return (n+1)
+}
+
+function dec(n) {
+  return (n-1)
+}
+
+function makeInt(n) {
+  parseInt(n,10)
+  return(n)
+}
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  parseFloat(n)
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return parseFloat(n)
+}
