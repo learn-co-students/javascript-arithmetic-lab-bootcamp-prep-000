b279eb80802d60c454a4cbc2883b82f160ac3bc8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-08 17:32:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function makeInt(n) {
+var a = parseInt(n, 10)  
+return a
+}
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(n) {
+  return ++n
+}
+function dec(n) {
+  return --n
+}
+function preserveDecimal(n) {
+  var a = parseFloat(n)
+  return a
+}
