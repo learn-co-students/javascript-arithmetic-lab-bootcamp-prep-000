17f782831a31871aa7dffb61fcca50b7fdef4811
Brandon <brschneider@gmail.com>
2018-06-11 05:10:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n += 1
+  return n
+}
+
+function dec(n) {
+  n -= 1
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+  return n
+}
