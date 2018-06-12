1a052e7ec5dbb22f10b35e676e97f7a8cef35cc2
PJ Valentini <pj.valentini@gmail.com>
2018-06-12 15:39:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+add(1, 81);
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract(60, 40);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(2, 3.4);
+
+function divide(a, b) {
+  return a / b;
+}
+multiply(5.0, 2.5);
+
+function inc(n) {
+  return ++n;
+}
+inc(749);
+
+function dec(n) {
+  return --n;
+}
+inc(800);
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+makeInt("3");
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal("2.45");
+
