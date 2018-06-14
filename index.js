26f69e65f9214355ea0d3b6c5dba281cf6e0390d
cerylean@gmail.com <cerylean@gmail.com>
2018-06-14 21:32:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(a, b) {return a + b}
+function subtract(a, b) {return a - b}
+function multiply(a, b) {return a * b}
+function divide(a, b) {return a / b}
+var number = 2 
+function inc(n) {return number++}
+function dec(n) {return number--}
+function makeInt(n) {return parseInt(n,10)}
+function preserveDecimal (n) {return parseFloat(2.222)}
+function preserveDecimal (n) {return parseFloat('nonsense')}
