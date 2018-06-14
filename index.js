8d3bea84aae760c99c6bc9ab9649d235034aa61d
Anthony Elijah <lionnet.ant@gmail.com>
2018-06-14 21:12:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+// addition function
+function add(a,b) {
+  return a + b 
+}
+
+// subtract function 
+function subtract(a,b) {
+  return a - b
+}
+
+// multiply function 
+function multiply(a,b) {
+  return a * b
+}
+
+// divide function
+function divide(a,b) {
+  return a / b
+}
+
+// increment function
+function inc(n) {
+  return n += 1
+}
+
+// decrement function 
+function dec(n) {
+  return n -= 1
+}
+
+// parse to int 
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+// parse to floating number 
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
