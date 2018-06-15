c7e0093961082ce1592f7938a791412792a28f2b
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-15 23:47:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,76 @@
+function add (a,b)
+
+{
+  
+  return a + b
+  
+}
+
+function subtract(a,b)
+
+
+{
+  
+  return a - b
+  
+}
+
+
+function multiply (a,b)
+
+
+{
+  
+  return a * b
+  
+}
+
+
+
+function inc (n)
+
+{
+  
+  return n += 1
+  
+}
+
+
+function dec (n)
+
+{
+  
+  return n -= 1
+  
+}
+
+
+function makeInt (n)
+
+
+{
+  
+  return parseInt(n, 10)
+  
+}
+
+
+function divide (a,b)
+
+{
+  
+  return a /= b
+  
+}
+
+
+function preserveDecimal (n)
+
+
+{
+  
+  return parseFloat(n)
+  
+}
+
+
