6639decc7cb97038f5c3566e66b8724b610b6a38
NPryngler <natasha.pryngler@gmail.com>
2018-06-15 23:50:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(a, b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function multiply(a,b){
+  return a * b
+}
+function divide(a, b){
+  return a/ b
+}
+function inc(n) {
+ var number = n
+ return n + 1
+}
+function dec(n) {
+  var number = n
+  return n - 1
+}
+function makeInt(n){
+  parseInt('n', 10)
+  return n
+}
+function makeInt(n){
+  parseInt('0', 10)
+  return n
+}
+function preserveDecimal(n) {
+  parseFloat('n')
+  return n 
+}
+function preserveDecimal(n){
+parseFloat('nonsense')
+return n 
+}
