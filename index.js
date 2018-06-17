4105aae6a581cc798f5810257d3f213c0d17c67b
sumaya <sumaya.shahoyi@gmail.com>
2018-06-17 23:28:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number)
+ 
+add5()
+ 
+console.log(number)
+ 
+
+number = 10
+ 
+add5()
+ 
+console.log(number)
+ 
+divideBy3()
+ 
+console.log(number)
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number)
+ 
+add5()
+ 
+console.log(number)
+ 
+
+number = 10
+ 
+add5()
+ 
+console.log(number)
+ 
+divideBy3()
+ 
+console.log(number)
