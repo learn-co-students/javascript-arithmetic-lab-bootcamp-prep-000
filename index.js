d611c9a087b1104cca85941bc63865b4bcc5d3fb
EdPutans <edputans@gmail.com>
2018-06-17 22:40:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(n1,n2) {
+  return n1+n2
+}
+
+function subtract (n1,n2) {
+  return n1-n2
+}
+
+function divide(n1,n2){
+  return n1/n2
+}
+
+function multiply(n1,n2) {
+  return n1*n2
+}
+
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n-1
+}
+
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n,10)
+}
