c3d100b0d10997941dd441d7ccccb55259d80b45
Alexa Schaeffer <alps231@gmail.com>
2018-06-19 04:20:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var x = 1
+var y = 80
+function add(x, y){
+  return x + y
+}
+
+var a = 60
+var b = 40
+function subtract(a,b) {
+  return a - b
+}
+
+var c = 2 
+var d = 3.4
+function multiply(c,d) {
+  return c * d
+}
+
+var e = 5.0 
+var f = 2.5
+function divide(e,f) {
+  return e / f
+}
+
+var n= 765
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
