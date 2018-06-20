b066c69d5523b200e686395fe47577d9f79b3e94
Andrei Sevostianov <sevost.andrey@gmail.com>
2018-06-20 00:24:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = 1 
+var b = 2
+
+function add (a, b) {
+   return a + b
+} 
+
+function subtract (a, b) {
+   return a - b
+} 
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
+function inc (n) {
+  return a += n
+}
+
+function dec (n) {
+  return n -= a
+}
+
+function makeInt (n) {
+  return parseInt (n, 10)
+}
+
+function preserveDecimal (a) {
+  return parseFloat (a)
+}
