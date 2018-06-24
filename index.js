c81a347e73b0719b3e20b584d911307bb7ceb9b0
raina <rainaoo2017@gmail.com>
2018-06-24 12:22:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var a= parseInt('3')
+var b= parseInt('3')
+var n=2
+function add(a,b){
+ return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+return a*b
+}
+function divide(a,b){
+ return a/b
+}
+function inc(n) {
+  n += 5
+}
+function des(n) {
+  n -= 5
+}
