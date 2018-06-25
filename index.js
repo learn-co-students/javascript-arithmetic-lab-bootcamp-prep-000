de49f21b3c23ea8e72526db67a8117852f8eeafc
David Schoenholtz <dmschoenholtz@gmail.com>
2018-06-25 16:01:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,59 @@
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+function add(a, b) {
+  return a + b
+}
+
+add()
+
+function subtract(a, b) {
+  return a - b
+}
+
+subtract()
+
+function multiply(a, b) {
+  return a * b
+}
+
+multiply()
+
+function divide(a, b) {
+  return a / b
+}
+
+divide()
+
+function inc(n) {
+  return n += 1
+}
+
+inc()
+
+function dec(n) {
+  return n -= 1
+}
+
+dec()
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+makeInt()
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+preserveDecimal()
