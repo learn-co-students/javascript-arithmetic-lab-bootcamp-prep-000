4f2b9debfdd7d7354cf487b3db7dc4d77417eacb
monthergholam@gmail.com <monthergholam@gmail.com>
2018-06-26 00:00:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+//first
+function add(a,b){
+  return a+b
+}
+//Second
+function subtract(a,b){
+  return a-b
+}
+//Third
+function multiply(a,b){
+  return a*b
+}
+//Fourth
+function divide(a,b){
+  return a/b
+}
+//sixth
+function inc(n){
+  return ++n
+}
+//seventh
+function dec(n){
+  return --n
+}
+//eighth
+function makeInt(n){
+  return parseInt(n,10)
+}
+//ninth
+function  preserveDecimal(n){
+  return parseFloat(n)
+}
