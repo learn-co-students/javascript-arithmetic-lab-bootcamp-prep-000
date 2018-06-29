1f37e4cc6e434f969b0c4015979bacb9fcc86c1c
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-06-29 04:10:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return (a+b)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function multiply(a,b){
+  return (a*b)
+}
+
+function divide(a,b){
+  return (a/b)
+}
+
+function inc(n){
+  result (n+1)
+}
+
+function dec(n){
+  result (n-1)
+}
+
+function makeInt(n){
+  result parseInt(n)
+}
+
+function preserveDecimal(n){
+  result parseFloat(n)
+}
