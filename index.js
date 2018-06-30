dfe6eebcdc6bdb154fe296a38f73b194cb9e2045
jenniferlong8 <jlong8@bu.edu>
2018-06-30 18:46:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function inc(a) {
+  return a + 1
+}
+
+function dec(n) {
+  return n - 1
+}
+
+function makeInt(n) {
+  n = parseInt(n,10)
+  return n
+}
+
+function preserveDecimal(n) {
+  n = parseFloat(n)
+  return n
+}
