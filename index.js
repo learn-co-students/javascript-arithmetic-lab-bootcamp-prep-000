04a259672b076d71111900934e64bcd224b4de64
Rene Santana <renesantana10@gmail.com>
2018-06-30 09:47:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+   return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+   parseInt(n)
+   parseInt('n')
+   return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  parseFloat('n')
+  return parseFloat(n)
+}
