2516e652e7c8fa10dd29482f27bdfc8fa72fa920
Robert Pjetri <rpjetri@hotmail.com>
2018-07-04 13:27:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function add() {
-  a + b
+function add(a, b) {
+  return a + b
 }
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
