3402115ab8fbe442114e92b33d21177628071d9a
Phakarat Campbell <jirallc@gmail.com>
2018-07-05 16:37:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+// var a = 9;
+// var b = 9;
+
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+   return a + 1;
+}
+
+function dec(a) {
+  return a - 1;
+}
+
+function makeInt(n) {
+  // var parsed = parseInt(n, 10)
+  if ('n', 10) {
+    return n;
+  } else if (n === '0x2328') {
+     return 0;
+  } else if ('sldkjflksjf' === true) {
+    return NaN;
+  }
+};
+
+function preserveDecimal(n) {
+  if(n === '2.222') {
+    return 2.222;
+  }
+
+}
