29541fc926325794fad8cbd39dd0574ac53960b6
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 07:48:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return (a+b)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function multiply(a,b){
+  return (a*b)
+}
+
+function divide(a,b){
+  return (a/b)
+}
+
+function inc(n){
+  number = 692
+  number +=n
+  
+}
+
+function dec(n){
+  n -=n
+  
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
