9c20fc1afe02cfaf269e2b94cc3824c9b3407172
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-07 11:16:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function add(x,y) {return x+y}
+function subtract (x,y) {return x-y}
+function multiply (x,y) {return x*y}
+function divide (x,y) {return x/y}
+function inc(n) {return n++}
+function dec(n) {return n--}
