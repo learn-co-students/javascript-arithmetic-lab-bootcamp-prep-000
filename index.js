a28bd7949fc824d66a77254a3adbdcdfcacea763
koho20002000@yahoo.com Berryman <koho20002000@yahoo.com>
2018-07-08 05:13:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var number;
+function add(a, b){
+  number = a + b;
+  return number;
+}
+
+function subtract(b, a){
+  number = b - a;
+  return number;
+}
+
+function multiply(a, b){
+  number = a * b;
+  return number;
+}
+
+function divide(a, b){
+  number = a / b;
+  return number;
+}
+
+function inc(n){
+  number = ++n;
+  return number;
+}
+
+function dec(n){
+  number = --n;
+  return number;
+}
+
+function makeInt(n){
+  number = parseInt(n, 10);
+  return number;
+}
+
+function preserveDecimal(n){
+  number = parseFloat(n, 10);
+  return number;
+}
