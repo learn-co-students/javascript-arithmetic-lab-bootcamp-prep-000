24b30498a61d9bf3fde7a8cda5db1def05f0d3e6
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-08 23:46:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  var number = n;
+  number++;
+  return number;
+}
+
+function dec(n) {
+  var number = n;
+  number--;
+  return number;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
