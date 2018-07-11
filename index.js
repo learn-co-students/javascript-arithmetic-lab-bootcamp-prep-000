3dcf402f13c0007b1d23a73033f2dc96dd5ca6a1
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-11 18:36:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(x, y) {
+  return x + y
+}
+
+add (1, 80)
+
+function subtract(x, y) {
+  return x - y
+}
+
+subtract (60, 40)
+
+function multiply(x, y) {
+  return x * y
+}
+
+multiply(2, 3.4)
+
+function divide(x, y) {
+  return x / y
+}
+
+divide(5.0, 2.5)
+
+function inc(x) {
+  return x + 1
+}
+
+number++
+
+function dec(x) {
+  return x - 1
+}
+
+number--
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
