6bee912aff9fe23aefdbb6958c453f4eaff6fe28
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-11 21:25:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-1+80
-60-40
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return n += 1;
+}
+
+function dec(n){
+  return n -= 1;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
