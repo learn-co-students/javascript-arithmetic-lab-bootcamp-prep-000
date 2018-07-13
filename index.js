3e28767b789a5ef27bc502c3c5cf21d3c9475005
Jose C Alarcon <jose.carlos.alarcon.chacon@gmail.com>
2018-07-13 17:41:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8
+5.0 / 2.5 // 2
+
+var number = 5
+number++ // 5
+number // 5
+number-- // 6
+number // 5
+
+var number = 10 
+function add() {
+  number += 5
+}
+function divideBy3() {
+  number /= 3
+}
+divideBy3()
+console.log(number) //
+3.333333333335
+add5()
+console,lod(number) //
+8.333333333335
+
+// reset number
+number = 10
+add5()
+console.log(number)
+divideBy3()
+console.log(number) // 15
+divideBy3()
+console.log(number) // 15
+
+parseInt('2' , 10) // 2
+parseInt('2.2222' , 10)
