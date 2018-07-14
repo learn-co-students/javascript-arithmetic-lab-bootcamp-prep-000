1dee3865290af3474b58186916e7d7813349c7be
Bakir Sadykov <sbakir@yandex.ru>
2018-07-14 21:41:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  a = parseInt(a)
+  b = parseInt(b)
+return parseInt(a) + parseInt(b)
+}
+
+function subtract(a, b) {
+  a = parseInt(a)
+  b = parseInt(b)
+   return parseInt(a) - parseInt(b)
+}
+
+function multiply (a, b) {
+  return parseInt(a) * parseInt(b)
+   
+}
+
+function divide(a, b) {
+  a = parseInt(a)
+  b = parseInt(b)
+return parseInt(a) / parseInt(b)
+}
