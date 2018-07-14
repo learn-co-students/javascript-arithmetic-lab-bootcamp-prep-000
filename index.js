1d83cdd904a8f45d8fd7cb7aa0a359c64409954e
Christina Tedesco <christdsco@gmail.com>
2018-07-14 16:12:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+//var a = 5
+//var b = 3
+function add(a,b) {
+//a+=b
+return a+=b
+}
+ function subtract(a,b) {
+return a-=b
+}
+ function multiply(a,b) {
+return a*=b
+}
+ function divide(a,b) {
+return a/=b
+}
+function inc(n) {
+return n+=1
+}
+function dec(n) {
+return n-=1
+}
+function makeInt(n) {
+return parseInt(n, 10)
+ 
+}
+function preserveDecimal(n) {
+return parseFloat(n)
+ 
+}
+
+
+
+
+
