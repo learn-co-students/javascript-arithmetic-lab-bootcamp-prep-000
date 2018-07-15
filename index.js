2b29e6c8b117ec96999241883c6e11dbc842641c
Will <will.caulfield@gmail.com>
2018-07-15 05:06:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-add (a,b) {
+function add (a,b) {
   return a + b;
 }
 
-subtract (a,b) {
+function subtract (a,b) {
   return a - b;
 }
 
-multiply (a,b) {
+function multiply (a,b) {
   return a*b;
 }
 
-divide (a,b) {
+function divide (a,b) {
   return a/b;
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
