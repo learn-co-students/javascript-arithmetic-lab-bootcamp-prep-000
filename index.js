299e9b1c669b1887507dac2abdcac27f931d46ce
Edward Castillo <edwardcastillo@me.com>
2018-07-16 20:57:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function add(a, b) {
-  return a + b;
+  return(a + b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return(a - b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return(a * b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return(a / b);
 }
 
 function inc(n) {
-  return n += 1;
+  return(n += 1); // increments n by 1
 }
 
 function dec(n) {
-  return n -= 1;
+  return(n -= 1); // decreases n by 1
 }
 
 function makeInt(n) {
-  return parseInt(n, 10);
+  return parseInt(n, 10); // returns n as whole number
 }
 
 function preserveDecimal(n) {
-  return parseFloat(n, 2.222);
+  return parseFloat(n, 10); // returns argument perserving decimal
 }
