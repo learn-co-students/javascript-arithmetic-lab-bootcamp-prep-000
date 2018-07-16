3d694822b869141a1e254799c8623d7142812854
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 17:46:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+var a = 10
+var b = 5
+var n = 2
+
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n++ 
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  return parseInt( 2, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n)
+}
+
+
+
+
+
