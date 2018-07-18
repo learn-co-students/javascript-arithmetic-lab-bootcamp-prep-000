51458cbb03860e1b79eea35c95848773894a3b0b
Brenda  Almonte <breal0811@gmail.com>
2018-07-18 19:19:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,52 @@
+var a = 900
+var b = 31
+var n = 933
+function add(a, b) {
+  return a + b
+}
+//good above
+
+function subtract(a, b) {
+  return a - b
+}
+//good above
+
+function multiply (a, b) {
+  return a * b 
+}
+//good above
+
+function divide(a, b) {
+  return a / b
+}
+//good above
+
+function inc(n) {
+  n++
+  return n
+}
+//good above
+
+function dec(n) {
+  n--
+  return n
+}
+//good above
+
+function makeInt(n) {
+  return n
+  n = parseInt('0*2328', 10)
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
