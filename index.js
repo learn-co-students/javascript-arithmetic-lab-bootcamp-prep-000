edfc3c03fdb75663f91ba745781e78e83f5ad799
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-20 07:50:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+var a = n
+var b = n
+
+function add(a, b) {
+  return(a += b);
+}
+
+
+function subtract(a, b) {
+  return(a -= b);
+}
+
+
+function multiply(a, b) {
+  return(a * b);
+}
+
+function divide(a, b) {
+  return(a /= b);
+}
+
+function inc(a) {
+  return(a + 1);
+}
+
+function dec(a) {
+  return(a - 1);
+}
+
+function makeInt(a) {
+  return(a.toString());
+}
+
+function makeInt(n) {
+  return (parseInt(n, 10));
+}
+
+function preserveDecimal(n) {
+  return(2.222);
+}
+
+function preserveDecimal(n) {
+  return (parseFloat(n));
+}
