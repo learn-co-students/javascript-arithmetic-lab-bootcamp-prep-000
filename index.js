5334eba1ca0b8ac772ccd6b695891dea0dfd81f8
thestylishkidsintheriot@gmail.com <thestylishkidsintheriot@gmail.com>
2018-07-21 13:04:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(a, b) {
+  var addition = a + b;
+  return addition;
+}
+
+add(1 + 80);
+
+function subtract(a, b) {
+  var subtract = a - b;
+  return subtract;
+}
+
+subtract(60 - 40);
+
+function multiply(a, b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+multiply(2 * 3.4);
+
+function divide(a, b) {
+ var divide = a / b;
+ return divide;
+}
+ 
+divide(5.0 / 2.5);
+
+var a = 6;
+
+ function inc(a) {
+ a = a + 1;
+ return a;
+ }
+
+inc(a);
+
+var b = 6;
+
+ function dec(b) {
+ b = b - 1;
+ return b;
+ }
+
+dec(b);
+
+function makeInt(a) {
+  b = parseInt(a, 10);
+  return b;
+}
+
+makeInt('457', 10);
+
+function preserveDecimal(a) {
+  b = parseFloat(a, 10);
+  return b;
+}
+
+preserveDecimal('043403.678')
+
