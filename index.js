19de2d79716f195579c7786166e379bf102a918e
Emily Asch <e.asch@me.com>
2018-07-21 19:28:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function add (a,b){
+  parseInt(a,10)
+  parseInt(b,10)
+  return (a+b)
+}
+
+function subtract (a,b){
+  parseInt(a,10)
+  parseInt(b,10)
+  return (a-b)
+}
+
+function multiply (a,b){
+  parseInt(a,10)
+  parseInt(b,10)
+  return (a*b)
+}
+
+function divide (a,b){
+  parseInt(a,10)
+  parseInt(b,10)
+  return (a/b)
+}
+
+function inc (n){
+  parseInt(n,10)
+  return (++n)
+}
+
+function dec (n){
+  parseInt(n,10)
+  return (--n)
+}
+
+function makeInt(n){
+ return parseInt(n,10)
+}
+
+function preserveDecimal (n){
+  return parseFloat(n,10)
+}
