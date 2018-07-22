7448d2ff87f1d0253b39230276c54a4f0318f951
Steven Zapata <szapata427@gmail.com>
2018-07-22 20:58:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,66 @@
+function add(number1, number2) {
+ return number1 + number2
+}
+add(1,2)
+
+function subtract(number1, number2) {
+ return number1 - number2
+}
+subtract(1,2)
+
+function multiply(number1, number2) {
+ return number1 * number2
+}
+multiply(1,2)
+
+function divide(number1, number2) {
+ return number1 / number2
+}
+divide(1,2)
+
+function inc(n) {
+  return n += 1
+  
+}
+inc(3)
+
+function dec(n) {
+  return n -= 1
+  
+}
+dec(3)
+
+
+function makeInt(n) {
+  var parsed = parseInt(n)
+  return parsed
+}
+
+makeInt(2)
+
+
+function makeInt(n) {
+  var number = "n"
+  number = parseInt(number)
+  return number
+}
+makeInt(2)
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  }
+
+makeInt(2, 10)
+
+function preserveDecimal(n) {
+return parseFloat(n)
+}
+preserveDecimal(2.33)
+
+
+
+
+
+
+
+
