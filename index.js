b8c76b7a9cec71ad1fef7720005491244c353ac4
Randall Libramonte <libramonte.randall@gmail.com>
2018-07-23 22:25:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add (x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+
+function multiply(x, y) {
+  return x * y
+}
+
+function divide(x, y) {
+  return x / y
+}
+
+function inc(x) {
+  return x + 1
+}
+
+function dec(x) {
+  return x - 1
+}
+
+function makeInt(x) {
+  return parseInt(x, 10) //ParseInt usually have 2 arguments, the second being the base.
+}
+
+function preserveDecimal(x) {
+  return parseFloat(x)
+}
