bdcf562c7152f6621bdf20b821db712386d6f3af
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 22:29:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(x, y) {
+  return x + y 
+}
+console.log(add(4,2))
+
+function subtract(x,y) {
+  return x - y 
+}
+console.log(subtract(60,40))
+
+function multiply(x,y) {
+  return x * y
+}
+console.log(multiply(2,3.4))
+
+function divide(x,y){
+  return x/y
+}
+console.log(divide(5.0,2.5))
+
+function inc(x){
+  return x+=1
+}
+console.log(inc(2))
+
+function dec(x) {
+  return x -=1 
+}
+console.log(dec(2))
+
+function makeInt(x, base) {
+  return parseInt(x, 10);
+
+}
+
+function preserveDecimal(x) {
+  return parseFloat(x)}
