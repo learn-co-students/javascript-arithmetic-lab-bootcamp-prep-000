8b7237c13f70823ed078f48fc5fe3dc4313dd7b1
Marty Lerille <mlerille@tulane.edu>
2018-07-26 17:42:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(num1, num2){
+  return(num1 + num2);
+}
+
+function subtract(num1, num2){
+  return(num1 - num2);
+}
+
+function multiply(num1, num2){
+  return(num1 * num2);
+}
+
+function divide(num1, num2){
+  return(num1 / num2);
+}
+
+function inc(num1){
+  return(num1 += 1);
+}
+
+function dec(num1){
+  return(num1 -+ 1);
+}
+
+function makeInt(n){
+  return(parseInt(n));
+}
+
