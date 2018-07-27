771e32ae0a061be431b03b5f3cd0a9c22eaff5bd
Angela Deen <adeen1221@gmail.com>
2018-07-27 13:28:47 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function multiply(a,b) {return a*b}
 function divide(a,b) {return a/b}
 function inc(n) {return n+=1}
 function dec(n) {return n-=1}
-
+function makeInt(n){return parseInt(n,10)}
+function preserveDecimal(n) {return parseFloat(n)}
 
 
