a5aeef4c27b99520ab3dc05e8ae46bae8d4f092c
Angela Deen <adeen1221@gmail.com>
2018-07-27 05:37:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+learn function add(a,b) {return a+b}
+function subtract(a,b) {return a-b}
+function multiply(a,b) {return a*b}
+function divide(a,b) {return a/b}
+function inc(n) {n+=1}
+function dec(n) {n-=1}
+
+
+
