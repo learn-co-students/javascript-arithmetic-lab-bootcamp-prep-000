dec7ff05889b5dd3d9ccfa1b3d17683f2ad76e49
david m flynn <flynndave1@msn.com>
2018-07-28 19:18:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,77 @@
+var TT
+var TTT
+var sum
+function add ( a , b ) {
+  sum = a + b;
+  return sum;
+}
+ TT = add(54 , 78)
+console.log(TT)
+
+function subtract ( a , b ) {
+  sum = a - b;
+  return sum;
+}
+ TT = subtract(54 , 78)
+console.log(TT)
+
+function multiply ( a , b ) {
+  sum = a * b;
+  return sum;
+}
+ TT = multiply(54 , 78)
+console.log(TT)
+
+function divide ( a , b ) {
+  sum = a / b;
+  return sum;
+}
+ TT = divide(54 , 78)
+console.log(TT)
+
+function inc (n ) {
+ n++
+  return n;
+}
+console.log (54)
+ TT = inc(54 )
+console.log(TT)
+
+function dec(n ) {
+ n--
+  return n;
+}
+console.log(78)
+ TT = dec(78 )
+console.log(TT)
+
+
+
+function makeInt(n ) {
+var xx = parseInt(n,10);
+  return xx;
+}
+var alpha = "34"
+console.log(alpha)
+ TTT = makeInt(alpha);
+console.log(TTT)
+
+
+function preserveDecimal(string) {
+  var yy = parseFloat( string , 10)
+  return yy
+}
+
+var zz = "32.45671"
+var zz2 = preserveDecimal(zz);
+console.log(zz2);
+
+var zzDumb = "Dumbdumb!"
+var zz3 = preserveDecimal(zzDumb);
+console.log(zz3);
+
+
+
+
+
+
