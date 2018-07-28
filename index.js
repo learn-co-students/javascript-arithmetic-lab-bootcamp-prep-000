6e119e16e5e927d21f9dd7e79d6321676da755d2
Hank Liu <hanke.liu@gmail.com>
2018-07-28 02:09:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(a,b) {
+  var result= a + b;
+  return result; 
+}
+
+function subtract(a, b) {
+  var result = a - b;
+  return result;
+}
+
+function multiply(a, b) {
+  var result = a * b; 
+  return result;
+}
+
+function divide(a, b) {
+  var result = a/b;
+  return result; 
+}
+
+
+function inc(n) {
+  n += 1;
+  return n;
+ }
+
+ function dec(n) {
+  n -= 1;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt(n);
+  return n;
+}
+
+function makeInt(n) {
+  var number = parseInt(n, 10);
+  return number;
+}
+
+
+function preserveDecimal(n) {
+  number = parseFloat(n);
+  return number;
+}
+
+function preserveDecimal(n) {
+   nonumber = parseFloat('noanumber');
+   return nonumber;
+ }
+
