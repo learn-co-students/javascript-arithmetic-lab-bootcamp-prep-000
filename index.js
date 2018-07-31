fa515ed2e9b48c557ad686385116e06b928a0f6e
Katie Baltzley <kmbaltzl@gmail.com>
2018-07-31 15:39:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+var x = add(6,4);
+
+function add(a, b) {
+  return a + b;
+}
+
+var x = subtract(6,4);
+
+function subtract(a, b) {
+  return a - b;
+}
+
+var x = multiply(6,4);
+
+function multiply(a, b) {
+  return a * b;
+}
+
+var x = divide(6,4);
+
+function divide(a, b) {
+  return a / b;
+}
+
+var x = inc(6,4);
+
+function inc(a, b) {
+  return a + 1;
+}
+
+var x = dec(6,4);
+
+function dec(a, b) {
+  return a - 1;
+}
+
+var x = makeInt(6,4);
+
+function makeInt(a, b) {
+  return parseInt(a);
+}
+
+var x = makeInt(6,4);
+
+function makeInt(a, b) {
+  return parseInt(a, 10);
+}
+
+var x = preserveDecimal(6,4);
+
+function preserveDecimal(a, b) {
+  return parseFloat(a);
+}
