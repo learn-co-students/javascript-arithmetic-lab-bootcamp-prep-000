2dfdbf7a72535b3aad1debe4be59e73eb30b88d5
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-02 21:03:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-function add(a, b) {
-  return a + b
-}
-
-function subtract(a, b) {
-  return a - b
-}
-
-function multiply(a, b) {
-  return a * b
-}
-
-function divide(a, b) {
-  return a / b
-}
-
-function inc(n) {
-  return `n++`
-}
 /*
-function dec(n){
-  return n--
-}
+functions add(), subtract(), multiply(), divide()
+functions: inc(n) and dec(n)
 
-parseInt('2', 10) // 2
-
-parseFloat()
-
-makeInt(n) 
-
-preserveDecimal(n)
  */
- 
+
 console.log("Starting JavaScript Arithmetic Lab") // string NOT DISPLAYED 
 
-var a = 1 
-var b = 2 
-var n = 3 
-
 console.log("Finished JavaScript Arithmetic Lab") // string NOT DISPLAYED 
+
