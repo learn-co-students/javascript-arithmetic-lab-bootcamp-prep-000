61144ac2475c7ac9b9932e855d86cbd7ac340359
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-02 20:50:27 +0000

    Done.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b
 }
-/*
-function divide(a, b) {
-  
-}
 
+function divide(a, b) {
+  return a / b
+}
+/*
 function inc(n) {
   return n++
 }
