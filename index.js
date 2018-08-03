fe522097af667b21060572d21e699481283c0215
Michael Barbone <michael.barbone@gmail.com>
2018-08-03 16:51:12 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,18 @@ function divide (a, b) {
   return a / b
 }
 
-function inc (a) {
-  return (a+=)
+function inc(n) {
+  return n+=1
 }
 
-function dec(a) {
-  return a--
+function dec(n) {
+  return n-=1
+}
+
+function makeInt(n) {
+ return parseInt (n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n)
 }
