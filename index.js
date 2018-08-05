7878bb1d89161a5334feead077433785fbc5180d
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-05 13:54:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 function add(a, b){
-  return(a + b)
+  return a + b
 }
 function subtract(a, b){
-  return(a - b)
+  return a - b
 }
 function multiply(a, b){
-  return(a * b)
+  return a * b 
 }
 function divide(a, b){
-  return (a / b)
+  return a / b
 }
-function inc(a){
-  return (a + 1)
+function inc(n){
+  return n += 1
 }
-function dec(a){
-  return(a - 1)
+function dec(n){
+  return n -= 1
 }
-function makeInt(a){
-  return(a)
+function makeInt(n){
+  return parseInt(n, 10)
 }
-function makeInt(a){
-  return(0 * 2328)
-}
-function isNaN(sldkjflksjf){
-  return(true)
-}
-function preserveDecimal(a){
-  return(2.222)
+function preserveDecimal(n){
+  return parseFloat(n)
 }
