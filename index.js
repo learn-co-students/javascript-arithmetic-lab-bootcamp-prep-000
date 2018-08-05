87254b970079ec4b5fea09cef0ace8bf6d1a39b4
Roundabout Rider <ntm.email@icloud.com>
2018-08-05 10:10:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a , b) {
+  return parseInt(a , 10) + parseInt(b , 10)
+}
+
+function subtract(a , b) {
+  return parseInt(a , 10) - parseInt(b , 10)
+}
+
+function multiply(a , b) {
+  return parseInt(a , 10) * parseInt(b , 10)
+}
+
+function divide(a , b) {
+  return parseInt(a , 10) / parseInt(b , 10)
+}
+
+function inc(a) {
+  return parseInt(++a , 10)
+}
+
+function dec(a) {
+  return parseInt(--a , 10)
+}
+
+function makeInt(n) {
+  return parseInt(n , 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n , 10)
+}
