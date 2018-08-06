e537100ec39fb0fd75b5dbd7e03cce0cbcc10650
Robert Pjetri <rpjetri@hotmail.com>
2018-08-06 23:12:03 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function divide(a, b) {
   return a / b
 }
 
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
