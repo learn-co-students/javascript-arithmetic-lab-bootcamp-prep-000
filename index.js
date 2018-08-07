5e84bc6dda8bbba322845965b24d31978a7778f5
Jennifer Bishop <jbishop@gmail.com>
2018-08-07 02:35:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b){
+  return a + b;
+}
+function subtract(a,b){
+  return a - b;
+}
+function multiply(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+function inc(n){
+  return n+=1;
+}
+function dec(n){
+  return n-=1;
+}
+function makeInt(n){
+  n = parseInt(n, 10);
+  return n;
+}
+function preserveDecimal(n){
+  n = parseFloat(n);
+  return n;
+}
