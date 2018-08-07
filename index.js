1889fa1da17ebfdf309db6d6ec2b3f8d6edf127a
Mark Ehler <ehlerm42@gmail.com>
2018-08-07 03:57:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var add(1, 3){
+  return 1+2
+}
+  
+
+subtract(2, 1){
+  2 -1
+}
+multiply(6000, 7){
+  6000 * 7
+}
+divide(7, 7){
+  7 * 7
+}
+inc(1){
+  1 += 1
+}
+dec(1) ;
+makeInt(2, 10);
+makeInt(2.2222, 10);
+makeInt("Same Stuff", 10);
+preserveDecimal(3.1415);
+preserveDecimal("said I like it like that");
