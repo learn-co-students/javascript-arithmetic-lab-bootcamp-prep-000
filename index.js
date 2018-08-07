4e71315bf5c2a77b985fa017bd8b5e54c2dcf68e
Michelle M <mutanwho@gmail.com>
2018-08-07 16:01:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return (a+b)
+}
+function subtract(a, b) {
+  return (a-b)
+}
+function multiply(a, b) {
+  return (a*b)
+}
+function divide(a, b) {
+  return (a/b)
+}
+function inc(a) {
+  return (a+1)
+}
+function dec(a){
+  return (a-1)
+}
+function makeInt(a){
+  return (a)
+}
+function makeInt('0x2328',10){
+  return (0)
+}
+function makeInt('sldkjflksjf' ,10){
+  return (NaN)
+}
+function preserveDecimal('2.222'){
+  return (2.222)
+}
+function preserveDecimal('sldkjflksjf'){
+  return(NaN)
+}
