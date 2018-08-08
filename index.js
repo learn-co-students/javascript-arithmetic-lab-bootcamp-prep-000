2efc5c6f89cc08fa923d85dff1cbc2ff363bb3ab
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 03:21:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(a) {
+  return parseInt(a, 10);
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n)
+}
