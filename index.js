70bf53962ff2344c793bfd8de901968b5dfaf186
Ashley Tolen <tolenashley7@gmail.com>
2018-08-09 02:36:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+// Add
+function add(a, b) {
+  return a + b
+}
+// Subtract
+function subtract(a, b) {
+  return a - b
+}
+// Multiply
+function multiply(a, b) {
+  return a * b
+}
+// Divide
+function divide(a, b) {
+  return a / b
+}
+// Increment
+function inc(n) {
+  return n += 1
+}
+// Decrement
+function dec(n) {
+  return n -= 1
+}
+// Make Integer
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+// Preserve Decimal
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
