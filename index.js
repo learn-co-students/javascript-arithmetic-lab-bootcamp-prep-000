a591bd6ffc177392dfc53be3ed4fe42522195b21
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-09 14:29:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-function add(a,b){
-  return a+b
-}
-function subtract(a,b){
-  return a-b
-}
-function multiply(a,b){
-  return a*b
-}
-function divide(a,b){
-  return a/b
-}
-function inc(a){
-  return a+=1
-}
-function dec(a){
-  return a-=1
-}
-function makeInt(n){
-  return parseInt(n,10)
-}
-function preserveDecimal(n){
-  return parseFloat(n)
-}
+function add(a, b) {
+  return a + b
+};
+
+function substract(a, b) {
+  return a - b
+};
+
+function multiply(a, b) {
+  return a * b
+};
+
+function divide(a, b) {
+  return a / b
+};
+
+function inc(n) {
+  return n +=
+};
+
+function dec(n) {
+  return n -=
+};
