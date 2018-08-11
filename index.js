c8fee4b8b96a4163adc92773538a2333a5a96f2b
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-11 01:39:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+var n = "3.4"
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return ++a
+}
+
+function dec(a) {
+  return --a
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
+}
+
+function preseveDecimal(n){
+  return parseFloat("n")
+}
