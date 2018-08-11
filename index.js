32ac9aa38439c70e983693de5ad6a8c5c34107ac
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-11 07:10:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b) {
+  var sum = a + b;
+  return sum;
+}
+
+function subtract(a,b) {
+  var sum = a - b;
+  return sum;
+}
+
+function multiply(a,b) {
+  var sum = a * b;
+  return sum;
+}
+
+function divide(a,b) {
+  var sum = a / b;
+  return sum;
+}
+
+function inc(n) {
+  n += 1;
+  return n;
+}
+
+function dec(n) {
+  n -= 1;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
