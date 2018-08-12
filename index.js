6468057cf57f3ae409b42d1cd0dad0ba0e618323
Ryan Parker <ryan.parker3@outlook.com>
2018-08-12 22:32:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x,y){
+  return x + y
+}
+
+function subtract(x,y){
+  return x - y
+}
+
+function multiply(x,y){
+  return x * y
+}
+
+function divide(x,y){
+  return x/y
+}
+
+function inc(x){
+  return x += 1
+}
+
+function dec(x){
+  return x -= 1
+}
+
+function makeInt(x){
+  return parseInt(x,10)
+}
+
+function preserveDecimal(x){
+  return parseFloat(x)
+}
