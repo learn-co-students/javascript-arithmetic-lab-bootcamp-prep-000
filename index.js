6434aaf2f34cdd227fffe6e23ee0dc45966d6402
Maia Coghlan <maiaccog@gmail.com>
2018-08-12 04:21:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+  var result = a + b;
+  return result;
+}
+
+function subtract(a, b) {
+  var result = a - b;
+  return result;
+}
+
+function multiply(a, b) {
+  var result = a * b;
+  return result;
+}
+
+function divide(a,b) {
+  var result = a / b;
+  return result;
+}
+
+function inc(n) {
+  var result = n += 1;
+  return result;
+}
+
+function dec(n) {
+  var result = n -= 1;
+  return result;
+}
+
+function makeInt(n) {
+  var result = parseInt(n, 10);
+  return result;
+}
+
+function preserveDecimal(n) {
+  var result = parseFloat(n);
+  return result;
+}
+
+
+
+
+
