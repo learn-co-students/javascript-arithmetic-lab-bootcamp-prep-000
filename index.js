55590b1f22795e06310e564e66dc0247731c9d05
Marcus Richard <marcus.a.richard@gmail.com>
2018-08-14 14:51:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return n
+}
+
+function makeInt(n, base) {
+  var parsed = parseInt(n, 10);
+  return parsed
+}
+
+function preserveDecimal(n) {
+  var parsed = parseFloat(n);
+  return parsed
+}
