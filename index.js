49d2a75c566934bb1ad46892ff9def5963fdd9e0
Keshia White <keshiamwhite@gmail.com>
2018-08-14 02:56:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b) {
+  var add = a + b;
+  return add;
+}
+
+function subtract(a,b) {
+  var subtract = a - b;
+  return subtract;
+}
+
+function multiply(a,b) {
+ var multiply = a * b; 
+ return multiply;
+}
+
+function divide(a,b) {
+  var divide = a/b;
+  return divide;
+}
+
+var n = 0;
+
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+ n--;
+ return n;
+}
+
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+
