aec2c719db465ff43c7df2017b929e343a8c2dcc
Ismail Jalloh <ijalloh21@gmail.com>
2018-08-15 02:29:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return ( a * b)
+}
+
+function divide(a, b) {
+  return (a/b)
+}
+
+function inc(n) {
+  n++
+  return (n)
+}
+
+function dec(n) {
+  n--
+  return (n)
+}
+
+function makeInt(n) {
+  var num = parseInt(n, 10)
+  return num
+}
+
+function preserveDecimal(n) {
+  var num = parseFloat(n)
+  return num
+}
+
