5cfa83f36f587429618c90764e24eece96b56cc9
agchesspub@gmail.com <agchesspub@gmail.com>
2018-08-20 09:50:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b; 
+}
+
+function inc (a) {
+  return ++a;
+}
+
+function dec (a) {
+  return --a;
+}
+
+function makeInt (string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal (string) {
+  return parseFloat(string);
+}
