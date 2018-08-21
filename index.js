7b32aa7df46a995cbdadb08faf199d6afd7b092d
Andrew <andrew.shillito88@gmail.com>
2018-08-21 14:13:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function divide(a, b){
+  return a/b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+
+function inc(num){
+  return num+=1;
+}
+
+function dec(num){
+  return num-=1;
+}
+
+function makeInt(str){
+  return parseInt(str, 10);
+}
+
+function preserveDecimal(num){
+  return parseFloat(num);
+}
