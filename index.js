80cd7474b38d7fb6b21d64893c1ccd3076ae3af3
Brian McNeil <bmcneil@gmail.com>
2018-08-21 20:10:15 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function inc(n) {
+  return ++n;
+}
+
+function dec(n) {
+  return --n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
