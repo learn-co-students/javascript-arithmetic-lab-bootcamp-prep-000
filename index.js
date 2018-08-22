af4f6d355fd2766b910dc7b2451dd7e35a50565d
Jamey Atkerson <atkersonohana@gmail.com>
2018-08-22 20:20:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return (a+b)
+}
+function subtract(a,b){
+  return(a-b)
+}
+function multiply(a,b){
+  return(a*b)
+}
+function divide(a,b){
+  return(a/b)
+}
+var number=10
+function inc(n){
+  return++ n}
+var number=10
+function dec(n){
+  return --n
+}
+function makeInt(n){
+return (n)}
+function makeInt(n){
+  parseInt(n)
+  return (n)
+}
+function preserveDecimal(n){
+  parseFloat(n)
+  return(n)
+}
+
+
