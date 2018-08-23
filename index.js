5db96c40a7423642597825e1573193972211d8b5
Sarah Pai <pai.sarah89@gmail.com>
2018-08-23 04:40:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-function add(a, b) {
+function add(a,b){
   return a+b
 }
 
-function subtract(a, b) {
+function subtract(a,b){
   return a-b
 }
 
-function multiply(a, b) {
+function multiply(a,b){
   return a*b
 }
 
-function divide(a, b) {
+function divide(a,b){
   return a/b
 }
 
-function inc(n) {
-  return n +=1
+function inc(n){
+  return ++n;
 }
-
-function dec(n) {
-  return n -=1
-}
-
-function makeInt(n) {
-  return parseInt(n, 10)
-}
-
-function preserveDecimal(n) {
-  return parseFloat(n)
-}
+  
+  function dec(n){
+    return --n;
+  }
+  
+  function makeInt(n) {
+    parseInt('a')
+    return parseInt(n,10)
+  }
+  
+  function preserveDecimal(n){
+    parseFloat('n')
+    parseInt('nonsense!',10)
+    return parseFloat(n)
+  }
+  
