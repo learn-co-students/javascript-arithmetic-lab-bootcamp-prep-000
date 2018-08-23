fa6e9659572193e3b3ef3c6266f2b5440639d598
bkalk08@yahoo.com <brook.kalkhoven@gmail.com>
2018-08-23 14:21:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function add(a,b) {
+  return (a+b)
+}
+function subtract(a,b) {
+  return (a-b)
+}
+function multiply(a,b) {
+  return (a*b)
+}
+function divide(a,b) {
+  return (a/b)
+}
+var n=1
+function inc(a) {
+  return (a+n)
+}
+function dec(a) {
+  return (a-n)
+}
+
+function makeInt(a) {
+return parseInt(a,10)
+
+}
+
+function preserveDecimal(n) {
+if (isNaN(n)) {
+  return 'True'
+}
+return parseFloat('2.222')
+}
