47074e48b01bc93b9122ff163dcdf00c077ae79b
Justine Odegbami <justineodegbami@outlook.com>
2018-08-24 13:46:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+ a+= b
+  return a
+}
+
+function subtract(a, b) {
+ a-= b
+  return a
+}
+
+function multiply(a, b) {
+ a*= b
+  return a
+}
+
+function divide(a, b) {
+ a/= b
+  return a
+}
+function inc(a) {
+ a+= 1
+  return a
+}
+
+function dec(a) {
+ a-= 1
+  return a
+}
+
+function inc(a) {
+ a+= 1
+  return a
+}
+
+function makeInt(a) {
+ a = parseInt(a, 10)
+  return a
+}
+
+function preserveDecimal(a) {
+ a = parseFloat(a)
+  return a
+}
