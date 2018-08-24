0f0a2d075d275f248ae245c96cbc95787ef745c6
Suheyl Turac <skbsturac@live.com>
2018-08-24 22:53:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+ return (a+b)
+}
+
+function subtract (a,b) {
+  return (a-b)
+}
+
+function multiply(a,b) {
+ return (a*b)
+}
+
+function divide(a,b) {
+ return (a/b)
+}
+
+function inc(a) {
+ return (++a)
+}
+
+function dec(a) {
+ return (--a)
+}
+
+function makeInt(n) {
+  return (parseInt)
+}
+
+function  makeInt(n) {
+  return (parseInt ('n',100))
+}
+
+function makeInt(n) {
+  return (parseInt(n,10))
+}
+
+function preserveDecimal(n) {
+  return (parseFloat(n))
+}
+
