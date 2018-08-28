94a519d010813eb505d7fd4fb8f0046230db8132
cyu8110@gmail.com <cyu8110@gmail.com>
2018-08-28 19:28:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  var sum = a + b 
+  return sum
+}
+
+function subtract(a,b) {
+  var difference = a - b
+  return difference
+}
+
+function multiply(a,b) {
+  var product = a * b
+  return product
+}
+
+function divide(a,b) {
+  var quotient = a / b
+  return quotient
+}
+
+function inc(n) {
+  n++
+  return n 
+}
+
+function dec(n) {
+  n--
+  return n 
+}
+
+function makeInt(n) {
+  n = parseInt(n,10)
+  return n 
+}
+
+function preserveDecimal(n) {
+  n = parseFloat(n)
+  return n 
+}
