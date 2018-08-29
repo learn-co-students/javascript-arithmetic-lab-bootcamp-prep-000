929e4a7da7f5c3a6d714bcbdbe54c647241c0d17
Samantha Hyatt <shhyatt@gmail.com>
2018-08-29 21:44:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,65 @@
+var number;
+function add(a, b) {
+  number = a + b;
+  return number; 
+  }
+  
+  var number;
+  function subtract(a, b) {
+    number = a - b;
+    return number;
+  }
+  
+  var number;
+  function multiply(a, b) {
+    number = a * b;
+    return number;
+  }
+  
+  var number;
+  function divide(a, b) {
+    number = a / b;
+    return number;
+  }
+  
+  function inc(n) {
+    number = n +1;
+    return number;
+  }
+      
+  
+  function dec(n) {
+    number = n -1;
+    return number;
+   }
+   
+   function makeInt(n) {
+     number = parseInt(n, 10);
+     return number;
+   }
+   
+   function preserveDecimal(n) {
+     number = parseFloat(n);
+     return number;
+   }
+  
+  
+      
+  
+  
+  
+      
+      
+    
+  
+  
+
+
+
+
+  
+
+
+
+
+
