8454d68ebecf6542cbabe740dccaddb42f3684d4
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-29 08:40:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-var number = 10
- 
-function add5() {
-  number += 5
+function add (a,b) {
+  return 'a + b'
 }
- 
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
