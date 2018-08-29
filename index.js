8bf2d33dec38796b63a0fac01fd71cf655b9b553
Arti M <artismarti@gmail.com>
2018-08-29 21:52:21 +0000

    Done.

--- index.js
+++ index.js
@@ -22,20 +22,3 @@ function makeInt(n) {
 function preserveDecimal(n) {
   return parseFloat(n)
 }
-/*
-
-
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-*/
