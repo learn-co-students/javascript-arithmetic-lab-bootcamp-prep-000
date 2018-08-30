285cfc08df91f99be70ec8b197bfaea56dd8de44
Dorothy Davenport <dorothympdx@gmail.com>
2018-08-30 02:11:23 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ function multiply(a,b) {
 function divide(a,b) {
   return a / b;
 }
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt('n', 10);
+  return n;
+}
+
+function preseveDecimal(n) {
+  parseFloat('n')
+  return n;
+}
