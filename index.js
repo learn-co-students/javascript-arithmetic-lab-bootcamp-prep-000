a93e98e67657a9048d41b848a425a277e45340a9
Sam J Jimenez <sam.jimenez.27@hotmail.com>
2018-08-30 04:36:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+var a = 9
+var b = 8
+
+
+
+function add(a, b){
+  var addition = a + b
+  return addition
+}
+
+function subtract(a, b){
+  var subtraction = a - b
+  return subtraction
+}
+function divide(a, b){
+  var division = a / b
+  return division
+}
+
+function multiply(a, b){
+  var multiply =  a * b
+  return multiply
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+  
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+  
+}
+
