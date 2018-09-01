4b5caf4ce55ba4066f84d88f0f6e90573ed67fc1
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 19:48:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+let a = Math.floor(Math.random() * 1000);
+let b = Math.floor(Math.random() * 1000);
+function add(a, b){
+return a+b;
+}
+add(a, b);
+
+function subtract(a, b){
+  return a-b;
+}
+subtract(a, b);
+
+function multiply(a, b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(a){
+  return a+1;
+}
+
+function dec(a){
+  return a-1;
+}
+function makeInt(a){
+  return parseInt(a, 10);
+}
+
+function preserveDecimal(a){
+  return parseFloat(a,10);
+}
