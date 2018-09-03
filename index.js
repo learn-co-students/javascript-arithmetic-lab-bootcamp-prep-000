0125ebdd194f4bcb0802af9fe2bfb0963a677d11
Jubair Hossain <jubair.hossain28@gmail.com>
2018-09-03 13:40:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(num1, num2) { 
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+function inc(n) {
+  return n + 1 
+}
+
+function dec(n) {
+  return n - 1 
+}
+function  makeInt(n) {
+  return makeInt.toString()
+}
+
+function makeInt(0 * 2358) {
+  return 
+}
