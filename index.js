02302aa5441b2f730122c35ff93cdd0b1a5c11c8
Martins Aloba <aloba.martins@gmail.com>
2018-09-03 18:54:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+var a = 10;
+var b = 5;
+var n = 85;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  return result
+}
+
+function makeInt(n) {
+  return n
+}
+
+function preserveDecimal(n) {
+  return n -= 1;
+}
+
+function preserveDecimal(n) {
+  return n -= 1;
+}
