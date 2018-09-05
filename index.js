e1cacb36e6d87e98d16d53c32ce20e2a4751669c
Mielyn Acosta <mielyndl@gmail.com>
2018-09-05 00:59:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+var number;
+function add(a, b){
+  number = a + b ; 
+  return number ; 
+}
+
+function subtract(a,b){
+  number = a - b;
+  return number; 
+
+}
+function multiply(a,b){
+  number = a * b;
+  return number; 
+
+}
+function divide(a,b) {
+  number = a / b ;
+  return number; 
+}
+
+function inc(a){
+  number = a + 1;
+  return number;
+}
+
+function dec(b) {
+  number = b - 1; 
+  return number; 
+}
+
+function makeInt(a){ 
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ 
+  number = parseFloat(a, 10);
+  return number;
+}
