7d4046edddc369974a4508e5e6bcdaa522c7502f
zaid85j@gmail.com <zaid85j@gmail.com>
2018-09-08 14:11:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+add(){
+return  1 + 80
+} // 81
+ subtract(){
+   return 60 - 40
+ } // 20
+multiply(){
+return 2 * 3.4
+}// 6.8 (there's that floating-point arithmetic again...)
+divide(){
+  return 5.0 / 2.5 }// 2
