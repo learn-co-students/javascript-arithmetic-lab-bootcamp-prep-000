928c1fdac37e999c8a3f9ef99918d4523c80e944
johnnyhayes7@gmail.com <johnnyhayes7@gmail.com>
2018-09-09 16:21:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add (a,b){
+  return a + b;
+}
+
+function subtract (a, b){
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function inc (a) {
+  return a + 1;
+}
+
+function dec (a) {
+  return a - 1;
+}
+
+function makeInt(n, base){
+  var number = parseInt(n, 10);
+  return number
+}
+
+function preserveDecimal (n){
+  var decimal = parseFloat(n);
+  return decimal
+}
+
