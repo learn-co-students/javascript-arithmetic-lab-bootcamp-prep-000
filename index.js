5860685ba77d7211951b35f8d0d53302df007860
Jarrod Dyer <jarrod@gasganno.com>
2018-09-11 21:02:48 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,39 @@ function divide(a, b) {
 }
 
 function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+
+/*function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
   return n+= 1
 }
 
@@ -29,3 +62,4 @@ function makeInt(n) {
 function preserveDecimal(n) {
   return parseFloat(n)
 }
+*/
