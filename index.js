fa6de367ac8089f1078bb60a781f92d65d4b909d
Virginia Dooley <vcdooley@gmail.com>
2018-09-11 15:22:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return (a + b);
+}
+
+function subtract(b, a) {
+  return (b - a);
+}
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+function divide(a, b) {
+  return (a/b);
+}
+
+
+
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+5.0 / 2.5 // 2ß
