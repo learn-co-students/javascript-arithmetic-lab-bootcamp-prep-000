ba244b597e72d15bf84dac14cd7b7f7b27732c61
Gianluca Minucci <glminucci@gmail.com>
2018-09-12 23:56:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b)
+{
+  return a + b
+}
+add(700, 75)
+
+function subtract(a, b)
+{
+  return a - b
+}
+subtract(700, 75)
+
+function multiply(a, b)
+{
+  return a * b 
+}
+multiply(50, 3)
+
+function divide(a, b)
+{
+  return a / b 
+}
+divide(4, 2)
+
+function inc(n)
+{
+  return ++n
+}
+inc(476)
+
+function dec(n)
+{
+  return --n
+}
+dec(974)
+
+function makeInt(n)
+{
+  return parseInt(n, 10)
+}
+makeInt(290)
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
+}
+preserveDecimal(80.12399)
