0c3ee63293ee6ff6d94ae450ede49224bf196086
jordancortes22@gmail.com <jordancortes22@gmail.com>
2018-09-13 15:46:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+function multiply(a,b){
+  return a * b
+}
+function divide(a,b){
+  return a / b
+}
+function inc(n){
+return ++n
+}
+function dec(n){
+  return --n
+}
+function makeInt(n){
+  return parseInt(n)
+}
+var n = 10;
+function makeInt(n){
+  return parseInt(n,10)
+} 
+function makeString(n){
+  return parseInt('0')
+}
+function preserveDecimal(n){
+  return parseFloat(2.222)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
