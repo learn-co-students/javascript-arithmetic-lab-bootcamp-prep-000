307143e579653412402adaac9669696dd25db3d1
Brian <briandsalemi@gmail.com>
2018-09-15 17:59:17 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function dec(n) { return n - 1 }
 function makeInt(n) { return parseInt(n) }
 
 function makeInt(n) { return parseInt(n, 10) }
+
+function preserveDecimal(n) { console.log (parseFloat(n))}
+
+function preserveDecimal(n) { return parseFloat(n) }
