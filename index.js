d0bd2e717ad98f997e4dcd6df3216ecd7a41b057
Blair Stuart <blaircstuart28@gmail.com>
2018-09-17 19:28:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function add (a,b) {
+ return a+=b
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function subtract (a,b) {
+  return a-=b
+}
+
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)
+function multiply (a,b){
+  return a*=b
+}
+
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)
+function divide (a,b){
+  return a/=b
+}
+
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)
+function inc(a){
+  a++
+  return a
+}
+
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)
+function dec(a){
+   a--
+   return a
+}
+
+function makeInt(n){
+return parseInt( n , 10 )
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
