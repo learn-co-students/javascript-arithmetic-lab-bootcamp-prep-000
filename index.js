75da4a22e8c40bac08f4a987009e94f30004f436
Breaun <pynkylynn@gmail.com>
2018-09-18 23:56:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,57 @@
+var number= 5
+
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8
+5.0/2.5 // 2
+
+add()
+sutract()
+multiply()
+divide()
+ 
+var number =10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3 () {
+  number /= 3
+}
+
+console.log(number) 
+// 3.333333335
+
+add5()
+
+console.log(number)
+// 8.3333333335
+
+// reset number 
+
+number = 10
+
+add5()
+
+console.log(number)
+// 15
+
+divideBy3()
+
+console.log(number)
+// 5
+
+inc(n)
+dec(n)
+
+parseInt (’nonsense!’ , 10)
+
+parseFloat(‘80.123999’) // 80.123999
+
+makeInt(
+
+preserveDecimal()
+
+
+
