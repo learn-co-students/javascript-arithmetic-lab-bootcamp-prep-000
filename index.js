8c6c93f99c68b9fc5b64524411bec73727d145a6
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-09-18 11:18:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var number = 10
+
+function functionadd5(){
+  number += 5
+}
+function divideBy3() {
+  number /= 3
+}
+function subtract(){
+  
+}
+function multiply(){
+  
+}
+function divide(){
+  
+}
+divideBy3()
+ 
+console.log(number)
+ 
+add5()
+ 
+console.log(number)
+ 
+number = 10
+ 
+add5()
+ 
+console.log(number)
+ 
+divideBy3()
+ 
+console.log(number)
