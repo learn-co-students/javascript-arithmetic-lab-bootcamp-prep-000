7518e5f86b6201156d2361e4a6fc8459f37cb412
kdnguyen9088@gmail.com <kdnguyen9088@gmail.com>
2018-09-24 03:09:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function dec(a){
+  a--
+  return a
+}
+
+function inc(a){
+  a++
+  return a
+}
+
+function makeInt(n){
+  n=parseInt(n,10)
+  return n
+}
+
+function preserveDecimal(n){
+  n=parseFloat(n,10)
+  return n
+}
