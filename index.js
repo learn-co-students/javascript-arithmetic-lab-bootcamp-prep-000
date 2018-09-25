2c2d0f5115ac16814a2d8d4e8e4753d32c2fdfe3
dwphilbrick <dwphilbrick@gmail.com>
2018-09-25 17:29:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-def add(x, y):
+function add(x, y) {
   return x+y
+}
 
-def subtract(x, y):
+function subtract(x, y){
   return x-y
+}
 
-def multiply(x, y):
+function multiply(x, y){
   return x*y
+}
 
-def divide(x, y):
+function divide(x, y) {
   return x/y
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
