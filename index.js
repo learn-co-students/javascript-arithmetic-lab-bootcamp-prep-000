b6f1e1632f90b17e0f35390a0da553643e685261
Tona Morales-Calkins <tona.mc@gmail.com>
2018-09-26 19:25:59 +0000

    Done.

--- index.js
+++ index.js
@@ -15,44 +15,19 @@ function divide(a,b) {
 return a/b; 
  }
 
-
- function numberInc(a) {
- a+=1;
- return a;
- }
- 
- function numberDec(a) {
- a--;
- return a;
-  }
- 
-var number = 10;
-function add5() {
-  number+=5;
- }
- 
- function divideBy3() {
-   number/=3;
- }
- 
- divideBy3();
- console.log(number);
- 
- add5();
- console.log(number);
- 
-number = 10;
-add5();
-console.log(number);
-divideBy3();
-console.log(number);
-
 function inc(n) {
- a+=1;
-  return n;
+return  n+=1;
+  
 }
 
 function dec(n) {
-  a-=1;
-  return n;
+return  n-=1;
+}
+
+function makeInt(n) {
+   return parseInt(n, 10);
+ }
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
