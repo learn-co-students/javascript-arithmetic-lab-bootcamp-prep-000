3bf6319a07eb21b8a7039877b2f6c44a2cbe692f
Juan Ortega <juanakin0108@gmail.com>
2018-09-26 17:32:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,68 @@
+
+
+function add(a, b) {
+  
+  return a + b
+  
+}
+
+add(1,3)
+
+function subtract(a, b) {
+  
+  return a - b
+  
+}
+
+subtract(5, 3)
+
+function multiply(a , b) {
+  
+  return a * b
+}
+
+multiply(5, 4)
+
+
+function divide(a , b) {
+  
+  return a / b
+  
+}
+
+divide(10, 2)
+
+
+function inc(a) {
+  
+  return a + 1
+  
+}
+
+inc(1)
+
+
+function dec(a) {
+  
+  return a - 1
+  
+}
+
+dec(2)
+
+function makeInt(n) {
+ 
+  
+  return parseInt(n, 10)
+  
+}
+
+
+function preserveDecimal(n) {
+ 
+  
+  return parseFloat(n)
+  
+}
+
+
