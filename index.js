ed70a7bae2e48bf74ffcd203c70ef868fd7dbd73
Lucy Suddenly <suddenlylucy@gmail.com>
2018-09-27 23:09:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+  return parseInt(a, 10) + parseInt(b, 10)
+}
+function subtract(a,b) {
+  return parseInt(a, 10) - parseInt(b, 10)
+}
+function multiply(a,b) {
+  return parseInt(a, 10) * parseInt(b, 10)
+}
+function divide(a,b) {
+  return parseInt(a, 10) / parseInt(b, 10)
+}
+function inc(n) {
+  n++
+  return n
+}
+function dec(n) {
+  n--
+  return n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
