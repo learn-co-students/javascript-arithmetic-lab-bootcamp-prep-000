7247dacef4d8f7aff047946256de11f737ad5598
Jonathan Koss <jonathan_koss@yahoo.com>
2018-09-30 01:12:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 function add(a, b) {
-  return a + b
+  return a + b;  
 }
-
-add(51, 7)
-
 
 function subtract(a, b) {
- return a - b 
+  return a - b;
 }
-
-subtract(90, 50)
 
 function multiply(a, b) {
- return a * b
+  return a * b;
 }
 
-multiply(7, 8)
-
-function divide(a, b) {
-  return a / b
+function divide(a,b) {
+  return a / b;
 }
 
 function inc(a) {
-  return a + 1
+  return a += 1;  
 }
 
 function dec(a) {
-  return a - 1
+  return a -= 1;
 }
 
 function makeInt(a) {
- return parseInt(a, 10)
+ return parseInt(a, 10);
 }
 
-function preserveDecimal(g) {
-  return parseFloat(g)
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
 
+/*
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
@@ -45,4 +39,5 @@ describe('preserveDecimal(n)', function() {
   it('returns NaN as appropriate', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
-})
+}) */
+
