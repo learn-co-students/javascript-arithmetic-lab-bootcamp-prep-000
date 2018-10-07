55707d6e689aa6559ee0138ba67a524e42017f8c
florah001 <florah001@yahoo.co.uk>
2018-10-07 20:30:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var a, b;
+function add(a, b) {
+  return (a + b);
+}
+function subtract(a, b) {
+  return (a - b);
+}
+function multiply(a, b) {
+  return (a * b);
+}
+function divide(a, b) {
+  return (a / b);
+}
+function inc(a) {
+  return (a + 1);
+}
+function dec(a) {
+  return (a - 1);
+}
+function makeInt(a) {
+  return (a.toString());
+}
+function makeInt(a) {
+  return parseFloat(a);
+}
+function preserveDecimal(a) {
+  return parseFloat(a);
+}
