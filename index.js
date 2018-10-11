e6d8020fc140dd0d1b6103698e0cf3369f385e7b
Sean Gray <sean.a.gray@hotmail.co.uk>
2018-10-11 17:17:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var a, b
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+  return a += b;
+}
+
+function subtract(a, b) {
+  return a -= b;
+}
+
+function multiply(a, b) {
+  return a *= b;
+}
+
+function divide(a, b) {
+  return a /= b;
+}
+
+function inc(a) {
+  return ++a
+}
+
+function dec(a) {
+  return --a
+}
+
+function makeInt(a) {
+  return a
+}
+
+parseInt('0x2328', 10); {
+  
+}
+
