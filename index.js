a6c6460f42dfb31822a3aff6efb1e104cdb86d83
Shawanda Davis <shawandadavis93@gmail.com>
2018-10-18 16:18:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+// add a and b 
+var a = Math.floor(Math.random() * 1000); 
+var b = Math.floor(Math.random() * 1000); 
+
+function add(a,b) {return (a+b)}
+
+//subtract a and b 
+var a = Math.floor(Math.random() * 1000); 
+var b = Math.floor(Math.random() * 1000); 
+
+function subtract(a,b) {return (a-b)}
+
+// multiply
+var a = Math.floor(Math.random() * 1000); 
+var b = Math.floor(Math.random() * 1000); 
+
+function multiply(a,b) {return (a*=b)}
+
+//divide
+var a = Math.floor(Math.random() * 1000); 
+var b = Math.floor(Math.random() * 1000); 
+
+function divide(a,b) {return (a/=b)}
+
+//increment
+var a = Math.floor(Math.random() * 1000); 
+
+function inc(a) {return ++a}
+
+//decrement
+var a = Math.floor(Math.random() * 1000); 
+
+function dec(a) {return --a}
+
+// parse Interger
+var a = Math.floor(Math.random() * 1000); 
+a.toString()
+
+function makeInt(a) {return parseInt(a,10)}
+
+//floating decimal
+var n = '2.222'
+
+function preserveDecimal(n) {return parseFloat(n)}
+
+
+
+
+
