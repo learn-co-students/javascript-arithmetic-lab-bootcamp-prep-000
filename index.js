43039fa2813e3b93b7c69136c08f840c2d29fe65
David Sollesse <davidsollesse@gmail.com>
2018-10-22 11:59:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+
+function add(a, b) {
+      return(a + b)
+}
+
+function subtract(a, b) {
+      return(a - b)
+}
+
+function multiply(a, b) {
+      return(a * b)
+}
+
+function divide(a, b) {
+      return(a / b)
+}
+
+function inc(n) {
+      return(n++, n)
+}
+
+function dec(n) {
+      return(n--, n)
+}
+
+function makeInt(n) {
+      return(parseInt(n), n)
+}
+
+function makeInt(n) {
+      return(parseInt(n, 10))
+}
+
+function preserveDecimal(n) {
+      return(parseFloat(n))
+}
+
+var bananas = inf
+function preserveDecimal(n) {
+      return(parseFloat(n+bananas), n)
+}
