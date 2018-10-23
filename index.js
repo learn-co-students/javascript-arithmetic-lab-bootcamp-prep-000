aae0510bd724fced346290e9c9ca522db341901f
Marc <marc.troughton@gmail.com>
2018-10-23 02:33:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(a,b) {
+  var number
+  number = a+b
+  return number
+}
+function subtract(a,b) {
+  var number
+  number = a-b 
+  return number
+}
+function multiply(a,b) {
+  var number
+  number = a*b
+  return number
+}
+function divide(a,b) {
+  var number
+  number = a/b 
+  return number
+}
+function inc(n) {
+  n++
+  return n
+}
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
