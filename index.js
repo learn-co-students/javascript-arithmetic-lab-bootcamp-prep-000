79214fa87c47330a4271edd896884fc9c13a7e21
OSCAR RIOS <oscr.rss@gmail.com>
2018-10-30 01:24:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+var number
+
+function add (a, b) {
+  number = a + b ;
+  return number ;
+}
+function subtract (a, b)  {
+  number = a - b 
+  return number ;
+}
+function multiply(a, b) {
+  number = a * b
+  return number 
+}
+function divide(a, b) {
+  number = a / b
+  return number ;
+}
+function inc(a) {
+  number = a + 1
+  return number ;
+}
+function dec(a) {
+  number = a - 1 
+  return number ; 
+}
+function makeInt(n) {
+  return parseInt(n)
+}
+function makeInt(n) {
+  number = parseInt(n,10)
+  return number;
+}
+function preserveDecimal(n) {
+  number = parseFloat(n)
+  return number ;
+}
