4f59851d3e66020e3b2187d9f3cd9f96be0a984f
Sam Chung <words.sam@gmail.com>
2018-11-02 16:28:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var add = (a, b) => {
+  return a + b;
+}
+
+var subtract = (a, b) => {
+  return a - b;
+}
+
+var multiply = (a, b) => {
+  return a * b;
+}
+
+var divide = (a, b) => {
+  return a / b;
+}
+
+var inc = (n) => {
+  return n += 1;
+}
+
+var dec = (n) => {
+  return n -= 1;
+}
+
+var makeInt = (n) => {
+  return parseInt(n, 10);
+}
+
+var preserveDecimal = (n) => {
+  return parseFloat(n);
+}
