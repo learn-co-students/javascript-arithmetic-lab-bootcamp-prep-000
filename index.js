66b3475671e7e1e76c4bbfbe5609d7ebaf1d7176
H B <phajib@gmail.com>
2018-11-02 06:22:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+// add function
+function add(a, b){
+return a + b
+}
+
+//subtract function
+function subtract(a, b){
+  return a - b
+}
+
+//multiply function
+function multiply(a, b){
+  return a * b
+}
+
+//divivde function
+function divide(a, b){
+  return a / b
+}
+
+//increment function
+function inc(n){
+  return n + 1
+}
+
+//decrements function
+function dec(n){
+  return n - 1
+}
+
+//makeInt function
+function makeInt(n){
+  return parseInt(n, 10)
+
+  return parseInt('0x2328')
+
+  return parseInt('sldkjflksjf')
+}
+
+//preserveDecimal function
+function preserveDecimal(n){
+  return parseFloat(n)
+
+  isNan(parseInt(n))//('sldkjflksjf'))
+}
