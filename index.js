fb8b2474ae1c0b721bc2d74e5698fde4cc4f3425
Luis Zabala <luis.zabala076@gmail.com>
2018-11-03 00:38:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-function add(a , b) {
-return a + b
+function add(a,b) {
+  console.log(a+b); // add a & b
+  return a+b;
 }
 
-function subtract(a,b) {
-return a-b
+function multiply (a,b) {
+  console.log(a*b); // multiply a & b
+  return a*b;
 }
 
-function multiply (a, b) {
-  return a*b
-}
-function divide (a,b) {
-  return a/b
+function subtract (a,b) {
+  console.log(a-b); // subtract a & b
+  return a-b;
 }
 
-function inc(n) {
-  return n+1
+function divide(a,b) {
+  console.log(a/b); // divide a & b
+  return a/b;
+}
+
+function inc(n){
+  console.log(n++); // rising inc of n
+  return n;
 }
 
 function dec(n) {
-  return n-1
+  console.log(n--); // dec of n
+  return n;
 }
 
 function makeInt(n) {
-return parseInt(n, 10)
+  console.log(parseInt(n,10)); // parseInt with assumed base of 10
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-return parseFloat(n)
+  return parseFloat(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
