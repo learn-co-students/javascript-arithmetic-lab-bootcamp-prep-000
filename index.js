81f4b7d79c768671cf9f2bdbe7750c6505368c9e
Vanessa Chu <vanessa.chu@outlook.com>
2018-11-03 16:27:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var add = (a, b) => {
+  return a+b;
+}
+var subtract = (a, b) => {
+  return a-b;
+}
+var multiply = (a, b) => {
+  return a*b;
+}
+var divide = (a, b) => {
+  return a/b;
+}
+var inc = (n) => {
+  return ++n;
+}
+var dec = (n) => {
+  return --n;
+}
+var makeInt = (n) => {
+  return parseInt(n, 10);
+}
+var preserveDecimal = (n) => {
+  return parseFloat(n);
+}
