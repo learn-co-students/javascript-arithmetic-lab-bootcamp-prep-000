1eb697c19516379a1e5746d38942b57c530cb78c
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-10 09:05:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b){
+  return a + b;
+}
+function subtract(a, b){
+  return a - b;
+}
+function multiply(a, b){
+  return a * b;
+}
+function divide(a, b){
+  return a / b;
+}
+function inc(a){
+  a++;
+  return a;
+}
+function dec(a){
+  a--;
+  return a;
+}
+function makeInt(n){
+  return parseInt(n, 10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
