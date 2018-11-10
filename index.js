55226e7fbf43871b3681dd054b86be51bb7f1ebe
FX Wood <fxwood2@gmail.com>
2018-11-10 20:11:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b) {
+  return (a + b)
+}
+
+function subtract(a,b) {
+  return (a - b)
+}
+
+function multiply(a,b) {
+  return (a * b)
+}
+function divide(a,b) {
+  return (a / b)
+}
+
+function inc(n, i = 1) {
+  return n += i
+}
+
+function dec(n, i = 1) {
+  return n -= i
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
