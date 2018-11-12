29e3cab6fc8495efb6d7e240bb1d550b9eff5e58
Naenae L <oneprincesslea@gmail.com>
2018-11-12 22:56:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-var number = 10;
-function add5(){
-  number += 5;
-}
-function divideBy3(){
-  number/= 3;
-}
-
-divideBy3() ;
-console.log(number) ;
-
-add5();
-console.log(number);
-
-divideBy3();
-console.log(number);
+console.log(parseInt("nonsense!", 10) ) ;
