31359c80c57fc317d1345ba92f8e47f095d3e85c
Naenae L <oneprincesslea@gmail.com>
2018-11-12 22:50:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var number = 5;
-number ++;
-number
-number--
-number
-console.log(number+=3) ;
-console.log(number-=2);
-console.log(number*=10) ;
-console.log(number/=5) ;
+var number = 10;
+function add5(){
+  number += 5;
+}
+function divideBy3(){
+  number/= 3;
+}
+
+divideBy3() ;
+console.log(number) ;
