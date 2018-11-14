eb3338945d406394f3c3e9b91d47036d991b4aca
Paul Rosero <paul_rosero@yahoo.com>
2018-11-14 06:12:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+  return (a + b)
+}
+function subtract(a, b){
+  return (a - b)
+}
+function multiply(a, b){
+  return (a * b)
+}
+function divide(a, b){
+  return (a /b)
+}
+function inc(n){
+  return (++n)
+}
+function dec(n){
+  return (--n)
+}
+function makeInt(n){
+  return ('n')
+}
+function preserveDecimal(n){
+  return ('n')
+}
+function makeInt(n){
+  parseInt("n", 16)
+  return (n)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+function makeInt(n){
+  return parseInt(n, 10)
+}
