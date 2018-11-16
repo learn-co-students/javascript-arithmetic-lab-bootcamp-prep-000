51463082c2d2efeaa1c6ce79376e3ee10c49de6f
Shemar Dacosta <dshemar6@gmail.com>
2018-11-16 20:11:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  return a + b
+}
+console.log(add(2,2))
+
+function subtract(a,b){
+  return a - b
+}
+console.log(subtract(2,1))
+
+function multiply(a,b){
+  return a * b
+}
+console.log(multply(2,1))
+
+function divide(a,b){
+  return a / b
+}
+console.log(divide(2,1))
+
+
+function inc(n){
+  n++
+  return n
+}
+
+
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
