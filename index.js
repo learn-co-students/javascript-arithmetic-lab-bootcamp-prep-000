547972f6283b69fc49b4d68df418bffafe66d96f
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-19 19:42:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// add
+function add (a, b) {
+  return a + b
+}
+
+//subtract
+
+function subtract (a, b) {
+  return a - b
+}
+
+//multiply
+
+function multiply (a, b) {
+  return a * b
+}
+
+//divide
+
+function divide (a, b) {
+  return a / b
+}
+
+//increment
+
+function inc (n) {
+  n++
+  return n
+}
+
+//decrements
+
+function dec (n) {
+  n--
+  return n
+}
+
+//parsing n
+
+function makeInt (n) {
+  return n
+}
+
+//parsing with base 10
+
+function makeInt (n) {
+  return parseInt (n, 10);
+}
+
+//parsing n as a floating point
+
+function preserveDecimal (n) {
+  return n
+}
+
+//parsing n as a floating point
+
+function preserveDecimal (n) {
+  return parseFloat ('2.222')
+}
+
+//returns NaN as appropriate
+
+function preserveDecimal (n) {
+  return parseFloat (n)
+}
