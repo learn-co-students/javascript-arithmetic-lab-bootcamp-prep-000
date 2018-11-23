8c72d69a5733de3fc4b65ff8a48a2afddc1ce895
Elaina <elainaseki@gmail.com>
2018-11-23 06:00:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var a,b;
+function beforeEach(){
+  a = Math.floor(Math.random()*1000);
+  b = Math.floor(Math.random()*1000);
+}
+
+function add(a,b){
+  return a+b;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(a){
+  return a+=1;
+}
+
+function dec(a){
+  return a-=1;
+}
+
+function makeInt(a){
+  return parseInt("a",10);
+}
+
