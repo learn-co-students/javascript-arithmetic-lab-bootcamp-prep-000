45d2ac96ab2544037a14ceb7ead6452c62577dce
Andrew Turner <andrewturner227@gmail.com>
2018-12-11 21:23:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b)
+{
+    return a + b;
+}
+
+function subtract(a, b)
+{
+    return a - b;
+}
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+function divide(a, b)
+{
+    return a / b;
+}
+
+function inc(n)
+{
+    return n += 1;
+}
+
+function dec(n)
+{
+    return n -= 1;
+}
+
+function makeInt(n)
+{
+    if (isNan(parseInt(n, 10))
+    {
+        return 'NaN';
+    } else {
+        return parseInt(n, 10);
+    }
+}
+
+function preserveDecimal(n)
+{
+    if (isNan(parseFloat(n))
+    {
+        return 'NaN';
+    } else {
+        return parseFloat(n);
+    }
+}
