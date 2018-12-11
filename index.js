938644be2ac48d3605c2aaac0a40f8684d79a9a3
Samidh Patel <spatel13456@gmail.com>
2018-12-11 04:46:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+add(5, 2);
+
+function subtract(b, a) {
+  return b - a;
+}
+subtract(10, 5);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(5, 5);
+
+function divide(a, b) {
+  return a / b;
+}
+divide(6, 3);
+
+function inc(n) {
+  return n + 1;
+}
+inc(10);
+
+function dec(n) {
+  return n - 1;
+}
+dec(5);
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+makeInt("7", 10);
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal("2.22222", "dhdhsjhdj");
+
