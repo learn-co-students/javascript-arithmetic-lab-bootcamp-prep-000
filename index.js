a6228f1dcfe98b11f813642292a473b21939cb58
Claire Muller <clairermuller@gmail.com>
2018-12-18 06:10:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a, 10)
+}
+
+/*
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+*/
