4f24583297fa7161c4628346620a7884f5aad378
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-21 04:42:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var number = a
+var number = b
+
+
+function add(a,b)
+{return a + b}
+
+function subtract(a,b)
+{return a - b }
+
+function multiply(a,b)
+{return a * b }
+
+function divide(a,b)
+{return a/b}
+
+function inc(n)
+{ return n + 1}
+
+number++
+
+function dec(number)
+{return number - 1 } 
+
+function makeInt(n)
+{return parseInt('n') }
+
+function makeInt(c)
+{return parseInt(c, 10) }
+
+function preserveDecimal(n)
+{return parseFloat('n') }
+
+function preserveDecimal(c)
+{return parseFloat(c) }
+
