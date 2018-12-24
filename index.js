73eb490017c88b2546196c9d3d602a2dfcb0fbdd
rnavedojr <rnavedojr@outlook.com>
2018-12-24 19:20:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return (a += b);
+}
+
+function subtract(a, b) {
+  return (a -= b);
+}
+
+function multiply(a, b) {
+  return (a *= b);
+}
+
+function divide(a, b) {
+  return (a /= b);
+}
+
+function inc(n) {
+  return n+=1;
+}
+
+function dec(n) {
+  return n-=1;
+}
+
+function makeInt(n) {
+  return parseInt("10");
+}
+
+function makeInt(n) {
+  return parseFloat(0.0152);
+}
+
+function makeInt(n) {
+  return parseInt("Hello");
+}
