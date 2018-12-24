926924114a149438112048601687411d1bfeba62
rnavedojr <rnavedojr@outlook.com>
2018-12-24 20:05:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 function add(a, b) {
-  return (a += b);
+  return a + b
 }
 
 function subtract(a, b) {
-  return (a -= b);
+  return a - b
 }
 
 function multiply(a, b) {
-  return (a *= b);
+  return a * b
 }
 
 function divide(a, b) {
-  return (a /= b);
+  return a / b
 }
 
 function inc(n) {
-  return n+=1;
+  return n += 1
 }
 
 function dec(n) {
-  return n-=1;
+  return n -= 1
 }
 
-function makeInt (n) {
-  return parseInt("225", 10);
+function makeInt(n) {
+  return parseInt(n, 10)
 }
 
-function makeInt (n) {
-  return parseInt('0', 0);
-}
-
-function makeInt (n) {
-  return parseInt("Hello");
-}
-
-function preserveDecimal (n) {
-  return parseFloat('2.222');
-}
-
-function preserveDecimal (n) {
-  return parseFloat('Hello');
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
