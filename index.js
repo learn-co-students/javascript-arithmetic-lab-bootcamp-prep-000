464df41ab7eb8645cd41fcf9f9b197f8b933a902
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-02 17:17:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+var number = 5
+
+function add(a,b){
+  return a+b
+}
+
+function subtract (b,a){
+  return b-a
+}
+
+function multiply (a,b){
+return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+var n = 5 
+
+function inc(n){
+  
+++n;
+return n;
+}
+
+var n = 7 
+
+function dec(n){
+  
+  --n;
+  return n;
+}
+
+var n = 5
+function makeInt(n){
+  return parseInt(n)
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+return parseInt(n)
+}
+
+var n = 6 
+
+function preserveDecimal(n){
+parseFloat(n)
+return 'n'
+}
+
+var n = 2.222
+
+function preserveDecimal(n){
+parseFloat('n')
+return parseFloat(2.222)
+}
+
+function preserveDecima(n){
+parseFloat ('n')
+return n
+}
+
+function preserveDecimal(n){
+  return parseFloat(n, 10);
+}
