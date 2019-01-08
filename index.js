4efe3da9c17c4d1c232dbe68bb1dd511e48d0c98
Josephine Yao <yao.josephine@yahoo.com>
2019-01-08 22:13:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,44 +1,87 @@
+/*var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+it('add(a, b) adds two numbers and returns the result', function() {
+  expect(add(a, b)).toEqual(a + b)
+})*/
+
 function add(a, b){
-  var sum = (a + b);
-  return sum;
+  return a + b;
 }
+
+/*it('subtract(a, b) subtracts b from a and returns the result', function() {
+  expect(subtract(a, b)).toEqual(a - b)
+})*/
 
 function subtract(a,b){
-  var subtracted = (a - b);
-  return subtracted;
+  return a - b;
 }
 
+/*it('multiply(a, b) multiplies two numbers and returns the result', function() {
+  expect(multiply(a, b)).toEqual(a * b)
+})*/
+
 function multiply(a,b){
-  var product = (a * b);
-  return product;
+  return a * b;
 }
+
+/*it('divide(a, b) divides a by b and returns the result', function() {
+  expect(divide(a, b)).toEqual(a / b)
+})*/
 
 function divide(a,b){
   var divided = (a / b);
   return divided;
 }
 
+/*it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})*/
+
 function inc(a){
-  var n = parseInt(a + 1);
-  return n; 
+  return a += 1;
 }
+
+/*it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})*/
 
 function dec(a){
-  var n = parseInt(a - 1);
-  return n;
+  return a -= 1;
 }
 
-function makeInt(a){
-  var n = parseInt(a);
-  return n;
-}
+/*describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})*/
 
 function makeInt(a){
-  var n = parseInt(a, 10);
-  return n;
+  return parseInt(a, 10);
 }
+
+/*describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})*/
 
 function preserveDecimal(a){
-  var n = parseFloat(a, 10);
-  return n;
+  return parseFloat(a);
 }
