09926afccc26b415864a51d47e36cccfa01714f4
Peter Choi <jc4883@columbia.edu>
2019-01-15 03:54:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(first, seconde) {
+  return first + seconde
+}
+
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a/b
+}
+function dec(a) {
+  return a - 1
+}
+function inc(a) {
+  return a + 1
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
