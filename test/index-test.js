e67033410b04c427b8e164166154bb73e04ff94f
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-15 04:09:57 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -52,3 +52,37 @@ describe('preserveDecimal(n)', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+
+function add(a, b) {
+  return a + b
+}
+  
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b 
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+   return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+  
+  
