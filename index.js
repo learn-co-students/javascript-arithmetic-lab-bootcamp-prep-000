e817f971e0d7131d443f8eb82a0dd3a35703a6e2
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-24 06:32:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  var x = ++n
+  return x
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
