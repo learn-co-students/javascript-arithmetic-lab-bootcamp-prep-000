f1ce91f5ebbd957e13a6ad142050a86c12daf1b8
Carlos Nah <cnah27@gmail.com>
2019-01-25 13:24:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+  let sum = a + b 
+  return sum
+}
+
+function subtract(a,b) {
+  let difference = a - b 
+  return difference
+}
+
+function multiply(a,b) {
+  return a * b 
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n = n + 1
+  return n
+}
+
+function dec(n) {
+  n = n - 1 
+  return n
+}
+
+function makeInt(n) {
+ return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
