0ac6371a046c7082b9b5963746bd6f49d5a0a710
Dave K <dkorff12@gmail.com>
2019-01-26 06:35:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+  return x + y 
+}
+
+function subtract(x, y) {
+  return x - y 
+}
+
+function multiply(x, y) {
+  return x * y 
+}
+
+function divide(x, y) {
+  return x / y 
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
