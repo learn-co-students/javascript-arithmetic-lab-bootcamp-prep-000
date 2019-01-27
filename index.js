508af1eb7d08c1c1ec931fb147381416fedd2537
Alex Romo <ayromo08@gmail.com>
2019-01-27 17:47:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+
+}
+
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+
+}
+
+function divide(a, b) {
+
+}
+
+function inc(a) {
+
+}
+
+function dec(a) {
+
+}
+
+function makeInt(a) {
+
+}
+
+function preserveDecimal(a) {
+
+}
