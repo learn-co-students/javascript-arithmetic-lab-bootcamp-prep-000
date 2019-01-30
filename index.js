9f3340c03e8d95b68aa38344edfc57560e96d079
m.b.kubacki <m.b.kubacki@gmail.com>
2019-01-30 12:40:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add (a, b) {
+  return (a + b)
+}
+
+function subtract (a, b) {
+  return (a - b)
+}
+
+function multiply (a, b) {
+  return (a*b)
+}
+
+function divide (a, b) {
+  return (a/b)
+}
+
+function inc (n) {
+  n++
+  return (n)
+}
+
+function dec (n) {
+  n--
+  return (n)
+}
+
+function makeInt (n) {
+  n=parseInt(n, 10)
+  return n
+}
+
+function preserveDecimal (n) {
+  n=parseFloat(n)
+  return n
+}
