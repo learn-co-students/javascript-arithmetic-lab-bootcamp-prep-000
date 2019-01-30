09e9edc29448f7243375f6a42883cae5df7de29a
remi mendoza <mendozaremi@gmail.com>
2019-01-30 17:42:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var number = 10
+function add5() {
+  number += 5
+}
+
+function divideby3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
