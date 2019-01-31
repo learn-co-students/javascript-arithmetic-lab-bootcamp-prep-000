599bcbdd5001b50bce38dbbe17dc2a2779ab4d8c
Daniel J Trifaro <djtrifaro@gmail.com>
2019-01-31 18:44:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b) {
+  return a + b;
+}
+
+add();
+
+function subtract(a, b) {
+  return a - b;
+}
+
+subtract();
+
+function multiply(a, b) {
+  return a * b;
+}
+
+multiply();
+
+function divide(a, b) {
+  return a / b;
+}
+
+divide();
+
+function inc(n) {
+  return n +1;
+}
+
+inc();
+
+function dec(n) {
+  return n -1;
+}
+
+dec();
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+makeInt();
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+preserveDecimal();
