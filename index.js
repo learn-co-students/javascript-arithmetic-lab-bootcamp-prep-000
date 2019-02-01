31a28576295e7f3011ba5a39bfe6cfe243872f96
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-01 11:25:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(a) {
+  return a += 1;
+}
+
+function dec(a) {
+  return a -= 1;
+}
+
+function makeInt(a){
+  var number = parseInt(a, 10);
+  return number;
+}
+
+function preserveDecimal(a){ 
+  var number = parseFloat(a);
+  return number;
+}
