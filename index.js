e65185f857703133a9e6bc8ac025bf3a5ed124fc
Julian Brown <julianxbrown@gmail.com>
2019-02-03 00:47:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const add = (a,b) => {
+  return a + b;
+};
+
+const subtract = (a,b) => {
+  return a - b;
+};
+
+const multiply = (a,b) => {
+  return a * b;
+};
+
+const divide = (a,b) => {
+  return a / b;
+};
+
+const inc = (n) => {
+  return ++n;
+};
+
+const dec = (n) =>{
+  return --n;
+};
+
+const makeInt = (n) => {
+  return parseInt(n,10);
+};
+
+const perserveDecimal = (n) => {
+  return parseFloat(n);
+};
