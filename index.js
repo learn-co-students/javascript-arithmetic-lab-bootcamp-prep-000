0d3f8fb0e8933ac8c051da89cdaa2574f48ac758
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-03 18:52:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-function add(a, b){
-  return a + b
+function add(a,b){
+  return a+b;
 }
 
-function subtract(a, b){
-  return a - b
+function subtract(a,b){
+  return a-b;
 }
 
-function multiply(a, b){
-  return a * b
+function multiply(a,b){
+  return a*b;
 }
 
-function divide(a, b){
-  return a / b
+function divide(a,b){
+  return a/b;
 }
 
 function inc(n){
-  return ++n
+  return n += 1;
 }
 
 function dec(n){
-  return --n
+  return n -= 1;
 }
 
 function makeInt(n){
-  return parseInt('135.5', 10)
+  return parseInt(n,10);
 }
 
 function preserveDecimal(n){
-  return parseInt('2.22222', 10)
-}
-
-function preserveDecimal(n){
-  return parseInt('nonsense', 10)
+  return parseFloat(n,10);
 }
