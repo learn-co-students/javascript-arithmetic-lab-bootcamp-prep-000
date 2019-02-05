94e378f1eae9d295d6045b9d910738cefb0a1fc6
Rufus <ruufman@gmail.com>
2019-02-05 19:24:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b){
+  return a + b
+}
+console.log(2, 3);
+
+
+function subtract(a, b) {
+  return a - b
+}
+console.log(6, 3);
+
+
+function multiply(a, b) {
+  return a * b
+}
+console.log(2, 1);
+
+
+function divide(a, b) {
+  return a / b
+}
+console.log(6, 3);
+
+
+var n = 0;
+function inc(n){
+  n++;
+  return n;
+}
+
+var n = 0;
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt(15);
+  return n;
+}
+
+
+ function makeInt(n) {
+   return parseInt(n, 10);
+ }
+
+ function preserveDecimal(n) {
+   return parseFloat(n);
+ }
