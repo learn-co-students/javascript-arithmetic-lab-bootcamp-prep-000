7061e6646346149a2dc437a17f915760dd161b0f
juan siu <juan.siu@gmail.com>
2019-02-07 22:14:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
+
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc(n){
+  return n+= 1
+}
+
+function dec(n){
+  return n-= 1
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
