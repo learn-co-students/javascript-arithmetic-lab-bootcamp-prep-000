642375dfb4ed3ba621995f719f963ec0d07896e8
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 20:14:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+  return a+b
+}
+function subtract(b,a){
+  return b-a
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  return n+1
+}
+function dec(n){
+  return n-=1
+}
+function makeInt(n) {
+  return n
+}
+function preserveDecimal(n){
+  return n
+}
+function makeInt(n){
+  return parseFloat(n)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
