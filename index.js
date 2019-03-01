6f6adff2663d739368cc529473280ebe7a79291d
Francisco Andaur <francisco.andaur@icloud.com>
2019-03-01 18:29:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 function subtract(a, b) {
   return a - b;
 }
+
 function multiply(a, b) {
   return a * b;
 }
-function divide(a,b) {
+
+function divide(a, b) {
   return a / b;
 }
-function inc(n){
+
+function inc(n) {
   return ++n;
 }
 
-function dec(n){
+function dec(n) {
   return --n;
 }
 
-function makeInt(n){
+function makeInt(n) {
   return parseInt(n, 10);
 }
 
-function preserveDecimal(n){
+function preserveDecimal (n) {
   return parseFloat(n);
 }
