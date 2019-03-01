0e0ecd3f7a690eb6ecf9138ec7c13f41ae7e76bc
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-01 04:59:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+n = 5
+function inc(n) {
+  return n += 1
+
+}
+
+n = 5
+function dec(n) {
+  return n -= 1
+}
+
+n = "2"
+function makeInt(n) {
+return parseInt(n, 10)
+
+
+}
+
+n = "2.222"
+function preserveDecimal(n) {
+  return parseFloat(n)
+
+}
