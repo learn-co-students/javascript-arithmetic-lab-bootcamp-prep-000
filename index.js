7271fe12dd5b7cf80d4fc47a7e4eac6909823b29
Annia Forney <anniaforney@gmail.com>
2019-03-04 03:13:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-add (1, 80)
-subtract(60, 40)
-multiply(2, 3.4)
-divide(5.0, 2.5)
+var number = (a, b)
+
+function add (a,b) {
+  return  a+b
+}
+function subtract (a,b) {
+  return  a-b
+}
+function multiply (a,b) {
+  return  a*b
+}
+function divide(a,b) {
+  return  a/b
+}
+function inc(n) {
+  return  ++n
+}
+function inc(n) {
+  return  --n
+}
+function makeINT(2) {
+  
+  return parseInt(2, 10)
+}
+function makeINT(n) {
+  
+  return  preserveDecimal (2, 10)
+}
