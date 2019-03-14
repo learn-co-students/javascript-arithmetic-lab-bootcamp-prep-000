f45a1342281b8583fbbadb2cde1849c68226e6c9
Nekwan Luke <contact@nekwanluke.com>
2019-03-14 16:51:20 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ function dec(n) {
   return --n
 }
 
-function makeInt(n) {
-  return parseInt(n)
+function makeInt(n)
+  {return parseInt(n, 10)
 }
 
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
