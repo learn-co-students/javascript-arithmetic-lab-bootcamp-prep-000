530bfdd1738e6ffa8d594d3be9bc920172f73c1c
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-19 21:52:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+var add = (a,b) => a+b;
+var subtract = (a,b) => a-b;
+var multiply = (a,b) => a*b;
+var divide = (a,b) => a/b;
+var inc = n => ++n;
+var dec = n => --n;
+var makeInt = n => parseInt(n,10);
+var preserveDecimal = n => parseFloat(n);
