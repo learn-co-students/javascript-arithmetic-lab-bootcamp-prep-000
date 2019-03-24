9e8e935e16f3302060f280d51b44a606ed431077
Marcal Ramos <suncastelo@gmail.com>
2019-03-24 19:44:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+function add( a, b){
+  return a + b
+}
+
+add(2, 2 )
+
+function subtract( a, b ){
+  return a - b
+}
+
+subtract( 5, 6)
+
+function multiply( a, b){
+  return a * b
+}
+
+multiply(10,2)
+
+function divide( a, b){
+  return a / b 
+}
+ divide(8,5)
+ 
+ function inc(n){
+  
+   return n + 1
+ }
+ 
+ inc(10)
+ 
+ function inc(n){
+   return n + 1
+ }
+ 
+ 
+ 
+  function dec(n){
+   return n - 1
+ }
+ 
+ function makeInt(n){
+   return parseInt(n, 10)
+ }
+ 
+
+ 
+ function preserveDecimal(n){
+   return parseFloat(n)
+ }
+ 
+ 
