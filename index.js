04c956fba16ef5469ed6cae1b9350aa46dcc0c00
Joe Jamsky <cba1067950@yahoo.com>
2019-03-26 23:17:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+
+function divide(a, b){
+  return a/b;
+}
+
+function inc(n){
+  return ++n;
+}
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+  var nInt = parseInt(n, 10);
+  return nInt;
+}
+
+function preserveDecimal(n){
+  var nFloat = parseFloat(n, 10);
+  return nFloat;
+}
