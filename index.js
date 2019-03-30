a8d8e74766778e5751e3e425567cd6a4e64eb2ef
Adella <adellakenn@gmail.com>
2019-03-30 14:58:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+//var a, b 
+
+//beforeEach(function() {
+//  a = Math.floor(Math.random() * 1000)
+//  b = Math.floor(Math.random() * 1000)
+// })
+
+//add
+function add(a, b) {
+  return a + b
+}
+
+//subtract (a,b)
+function subtract(a, b) {
+  return a - b
+}
+
+//multiply (a,b)
+function multiply(a, b) {
+  return a * b
+}
+
+//divide (a,b)
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n++
+  return n 
+}
+
+//decrement
+function dec(n) {
+  n--
+  return n
+}
+
+//makeInt
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+//preserveDecimal(n)
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
