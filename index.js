f91c34ca7e60c2768b75cefa23c99b2ba4747c1b
Anthony Wan <anthonywan00@gmail.com>
2019-04-08 23:24:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function inc (n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  if (n === int) {
+    return NaN
+}
+  else return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  if (parseFloat(n) === isNaN) {
+    return NaN;
+    }
+    else return parseFloat(n);
+}
+
