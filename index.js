747af70d1c4d530ac7a1eaf25776e9d8bdc2a5a5
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 13:56:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const add = (a,b) => {
+  return a + b;
+}
+
+const subtract = (a,b) => {
+  return a - b;
+}
+
+const multiply = (a,b) => {
+  return a * b;
+}
+
+const divide = (a,b) => {
+  return a / b;
+}
+
+const inc = n => {
+  return n+1;
+}
+
+const dec = n => {
+  return n-1;
+}
+
+const makeInt = n => {
+  return parseInt(n, 10);
+}
+
+const preserveDecimal = n => {
+  return parseFloat(n);
+}
