aa903e471131fbf117ef8471c636c9ce3eef4b3d
Michael Adair <riada125@hotmail.com>
2019-04-23 19:36:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+return(a + b);
+}
+
+function subtract(a,b){
+  return(a-b)
+}
+
+function multiply(a,b){
+  return(a*b)
+}
+
+function divide(a,b){
+  return(a/b)
+}
+
+function inc(a){
+}
+function inc(n){
+  return(n+=1)
+}
+console.log(inc)
+function dec(n){
+  return(n-=1)
+}
+console.log(dec);
+
+function makeInt(n){
+return parseInt(n, 10);
+}
+console.log(makeInt)
+
+function preserveDecimal(n){
+ return parseFloat(n)
+}
+
+
+
