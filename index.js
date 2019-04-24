b8f72232af1843f46f2481fc4ae50f820c6151b7
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-24 10:13:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+var n = 0;
+
+function inc(n) {
+  return n +=1
+}
+
+function dec(n) {
+  return n-=1
+}
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
