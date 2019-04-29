2671ec9fa690cbea55febc1859a9e958f722ba01
Gufwat Antiphas <antiphas4christ888@gmail.com>
2019-04-29 03:04:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var a, b
+function add(a,b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function multiply(a, b ){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+function inc(n){
+  return ++n
+}
+function dec(n){
+  return --n
+}
+function makeInt(n){
+   var number=parseInt(n, 10)
+   return number
+}
+
+function preserveDecimal(n){
+  var number = parseFloat(n)
+  return number
+}
+
