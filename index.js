1672ce1aa7649850bec308c149ea412355cc1157
Ellyn Anderson <eka249@nyu.edu>
2019-05-08 07:20:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-var n= 4.5
-makeInt(n, 10)
+var n= '4.5'
+var a = 1
+var b= 3
 
-preserveDecimal(n)
+function add(a,b){
+  return a+b
+  }
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+
+function inc(n){
+  return n+=1
+}
+
+function dec(n){
+  return n-+1
+}
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
