35d900583e2ba3f68f769b0d1b24e16eef672099
Yooni Park <yoonikj@gmail.com>
2019-05-16 10:59:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function inc(n) {
+  var value = n+1;
+  return value;
+}
+
+function dec(n) {
+  var value = n-1;
+  return value;
+}
+
