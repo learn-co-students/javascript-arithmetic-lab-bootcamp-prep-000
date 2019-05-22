e0784cc90623a3a857af444c51b4ec5b37de5fb7
Andnrew Cheon <cheonandrew113@gmail.com>
2019-05-22 17:53:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+  return (a+b)
+}
+
+function subtract(a, b){
+  return (a-b)
+}
+
+function divide(a, b){
+  return (a/b)
+}
+
+function inc(n){
+  return n+=1
+}
+
+function dec(n){
+  return n-=1
+}
+
+function makeInt(n){
+  return parseFloat(n, 10)
+}
+
+function multiply(a, b){
+  return (a*b)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
+
+
