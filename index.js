3c099b059778b5889672f0d8657c15cc540a626b
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-24 03:35:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+   a + b;
+   return a + b;
+ }
+ 
+ function subtract(a, b) {
+   a - b;
+   return a - b;
+ }
+ 
+ function multiply(a, b) {
+   a * b;
+   return a * b;
+ }
+ 
+ function divide(a, b) {
+   a / b; 
+   return a / b;
+ }
+ 
+ function inc(n) {
+   n += 1;
+   return n;
+ }
+ 
+ function dec(n) {
+   n -= 1;
+   return n;
+ }
+ 
+ function makeInt(n) {
+   parseInt(n)
+   return n
+ }
+ 
+ function makeInt(n) {
+    return parseInt(n, 10)
+ }
+ 
+  function preserveDecimal(n) {
+     parseFloat('n');
+     return n;
+ }
+ 
