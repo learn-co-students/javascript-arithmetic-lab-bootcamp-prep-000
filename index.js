0275d1cbfefd180ef239018174571ceda0cbed43
Joshua Coleman <procoleman77@outlook.com>
2019-05-28 20:39:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+let a = 5 ;
+let b = 8 ;
+function add() {
+  let z = a+=b ; 
+}
+
+console.log(z); 
+
+var c = 6 ;
+var d = 7 ;
+function subtract(){
+ let x= c-=d ;
+}
+console.log(x);
+
+var e = 7 ;
+var f = 2 ;
+function multiply(){
+  let w = e*=f ;
+}
+
+console.log(w);
+
+var g = 4 ;
+var h = 10 ;
+function divide (){
+  let k = g/=h ;
+}
+console.log (k) ; 
+
+let n = 5;
+n++;
+++n;
+
+let p = 7 ;
+p--;
+--p;
+
+
+  }
+  let q = 8 ;
+  let r = "5";
+  let makeInt = Number.parseInt(r);
+  console.log()
+
+}
+
+
