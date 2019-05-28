cbc1072326018db4a8284a169199089f584ddd45
aj.artwil <aj.artwil@gmail.com>
2019-05-28 21:08:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var number;
+
+function add(a, b){
+  number = a + b;
+  return number;
+}
+
+function subtract(a, b){
+  number = a - b;
+  return number;
+}
+
+ function multiply(a, b){
+  number = a * b;
+  return number;
+}
+
+  function divide(a, b){
+  number = a / b;
+  return number;
+}
+
+function inc(a){
+  number = a + 1;
+  return number;
+}
+  
+  
+function dec(a){
+  number = a - 1;
+  return number;
+}
+
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
