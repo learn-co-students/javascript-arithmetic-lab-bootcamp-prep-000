75115d4803b6b4ec7c779d96a98f569fa9f9cf22
Carlotta Junin <carlotta.junin@gmail.com>
2019-06-03 12:07:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var a = 5
+var b = 3
+
+function add(a, b) {
+  return a+b
+}
+
+function subtract(a, b){
+return a-b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide (a, b) {
+return a/b
+}
+
+var n = 7
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n)
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
