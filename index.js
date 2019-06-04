3d91d00d69d04d49959ea18dd5ba308b639689af
Jason Shockness <jshock12@gmail.com>
2019-06-04 19:41:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var a = 5
+    b = 10
+ 
+function add (a, b) {
+ return a + b
+}
+
+function subtract (b, a){
+ return b - a
+}
+
+function multiply (a, b ) {
+ return a * b
+}
+
+function divide (b, a) {
+ return b / a
+}
+function inc(a) {
+  return a += 1
+}
+
+function dec(b) {
+  return b -= 1
+}
+
+function makeInt(a) {
+  return parseInt (a, 10)
+}
+
+function preserveDecimal(b) {
+  return parseFloat (b)
+}
