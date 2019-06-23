44dad63861bed4d49c0f304d9e7d17d67833e982
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-23 01:50:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,61 @@
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000), string= 'a',
+n = '0x2328', c='sldkjflksjf', d= '2.222'
+
+function add(a,b) {
+  return (a+b)
+  console.log(add)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function multiply(a,b){
+  return (a*b)
+}
+
+function divide(a,b){
+  return (a/b)
+}
+
+function inc(a){
+a++
+ return (a)
+}
+
+function dec(a){
+a-- 
+ return (a)
+}
+
+function makeInt(string){
+  parseInt (string)
+  return (string)
+}
+n ='sldkjflksjf'
+function preserveDecimal(n){
+    return (n)
+    parseFloat (n)
+
+
+
+}
+
+function makeInt(c){
+parseInt (c)
+return(c)
+}
+
+n ='2.222'
+function preserveDecimal(n){
+   parseFloat (n)
+  return (n)
+}
+
+function makeInt(n){
+return (n)
+parseInt (n, 10)
+
+
+
+}
