19e1a357d25cc9ce37de39977bd710d967b32c5c
Ping Ping <ping.ping.lu@hotmail.com>
2019-06-25 18:51:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,75 @@
+//Q1
+function add(a,b) {
+  return a+b
+}
+
+var a = 715
+var b = 3 
+console.log(add)
+
+
+//Q2
+function subtract (a,b) {
+  return a-b
+}
+
+var a = 715
+var b = 3 
+console.log(subtract)
+
+//Q3
+function multiply(a,b){
+  return a*b
+}
+var a = 2
+var b = 3 
+console.log(multiply)
+
+
+//Q4
+function divide(a,b){
+  return a/b
+}
+
+console.log(divide)
+
+//Q5
+var n = 20
+function inc(n) {
+  n++
+  return n
+}
+console.log(inc)
+
+//Q6
+var n = 5
+function dec(n) {
+  n--
+  return n 
+}
+console.log(dec)
+
+//Q7 
+var n = '0.2328'
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+console.log(makeInt)
+
+
+//Q9
+n = 'nonsense'
+function preserveDecimal(n) {
+  return parseInt('n', 10)
+}
+console.log(preserveDecimal)
+
+//Q8
+var n = '2.222'
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+console.log(preserveDecimal)
+
+
+
