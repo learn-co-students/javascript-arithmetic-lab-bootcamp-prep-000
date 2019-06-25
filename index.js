fca7f663545331a0067abb0837409a90b57a2ceb
Elizabeth Fawcett <elizabethfawcett47@gmail.com>
2019-06-25 18:06:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+var a = parseFloat(a)
+var b = parseFloat (b)
+var c = parseFloat (c)
+a = 1
+b = 80
+function add (a,b,c) {
+ c = a + b
+  return (c)
+}
+
+a = 60
+b = 40
+function subtract (a,b,c) {
+  c = a - b
+  return (c)
+}
+
+a = 2
+b = 3.4 
+function multiply (a,b,c) {
+  c = a * b 
+  return (c)
+}
+
+a = 5.0 
+b = 2.5
+function divide (a,b,c) {
+  c = a / b 
+  return (c)
+}
+
+var n = 5
+function inc(n) {
+  n++
+  return (n)
+}
+
+function dec(n) {
+  n--
+  return (n)
+}
+
+n = 2
+function makeInt (n) {
+  parseInt(n, 10)
+  return (parseInt(n,10))
+}
+
+n = 2.222
+function preserveDecimal (n) {
+  parseFloat(n)
+  return (parseFloat(n))
+}
