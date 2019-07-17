2757e34e9e08efa7470e67fe7306e590c7384557
Carolina Ferreira <carolina.ferreira.16@ucl.ac.uk>
2019-07-17 17:08:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b) {
+  return a + b
+}
+add(2,3)
+
+function subtract(a,b) {
+  return a - b
+}
+subtract(10,2)
+
+function multiply(a,b) {
+  return a*b
+}
+subtract(2,2)
+
+function divide(a,b) {
+  return a / b
+}
+subtract(10,2)
+
+function inc(n) {
+  return n+=1
+}
+ inc(5)
+
+function dec(n) {
+  return n-=1
+}
+ inc(5)
+ 
+ parseInt('1', 10)
+ parseFloat('2.2', 10)
+ 
+function makeInt(n) {
+  return n
+}
+
+makeInt("5")
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+} 
+
+preserveDecimal(2.222)
+
