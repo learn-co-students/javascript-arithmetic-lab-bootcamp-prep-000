c04f1b63f89487db97ee09d38ce1f6bc7551208e
Bailey Wood <woodbmae@gmail.com>
2019-08-03 20:00:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+function add(a, b) {
+  return a + b
+}
+
+add(a, b)
+
+function subtract(a, b) {
+  return a - b
+}
+
+subtract(a, b)
+
+function multiply(a, b) {
+  return a * b
+}
+
+multiply(a, b)
+
+function divide(a, b) {
+  return a / b
+}
+
+divide(a, b)
+
+function inc(n) {
+  return n += 1
+}
+
+inc(n)
+
+function dec(n) {
+  return n -= 1
+}
+
+dec(n)
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+makeInt(n)
+  
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+
