fd59e1f7a80e5540186f4196f4b0672cdd9db573
John Wall <jaywall@gmail.com>
2019-08-03 20:40:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+return a + b
+}
+function subtract(b, a) {
+return b - a
+}
+function multiply(a, b) {
+return a * b
+}
+function divide(a, b) {
+return a / b
+}
+
+function inc(n) {
+n++
+return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  var parsed = parseInt(n, 10)
+  return parsed
+}
+
+function preserveDecimal(n) {
+  var floated = parseFloat(n)
+  return floated
+}
