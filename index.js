c9b0e89be1f2a829e36b1a6b2979cc7bc5bc2847
Connor Meth <connormeth27@gmail.com>
2019-08-04 16:39:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+var a, b 
+
+function add(a, b) {
+  return (a + b);
+}
+console.log(add(1, 80));
+
+function subtract(a, b) {
+  return (a - b);
+}
+console.log(subtract(60, 40));
+
+function multiply(a, b) {
+  return (a * b);
+}
+console.log(multiply(2, 3.4));
+
+function divide(a, b) {
+  return (a / b);
+}
+console.log(divide(5.0, 2.5));
+
+function inc(n) {
+  return (++n);
+}
+console.log(inc(1));
+
+function dec(n) {
+  return (--n);
+}
+console.log(dec(1));
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+console.log(makeInt("5"));
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10);
+}
+
+
+
+
+
+
