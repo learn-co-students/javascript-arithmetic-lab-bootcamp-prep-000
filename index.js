ba619c1b6f2c7dd30f5bff85c73a0ce64bd95136
John Reyes <john.jreye@gmail.com>
2019-08-04 22:33:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var number;
+function add(a,b) {
+  number = a + b;
+  return number;
+}
+
+function subtract(a,b) {
+  number = a - b;
+  return number;
+}
+
+function multiply(a,b) {
+  number = a * b;
+  return number;
+}
+
+function divide(a,b) {
+  number = a / b;
+  return number;
+}
+
+function inc(a) {
+  number = a + 1;
+  return number;
+}
+
+function dec(n) {
+  number = n - 1;
+  return number;
+}
+
+function makeInt(n) {
+  number = parseInt(n, 10);
+  return number;
+}
+
+function preserveDecimal(n) {
+  number = parseFloat(n, 5);
+  return number;
+}
+
+
+
