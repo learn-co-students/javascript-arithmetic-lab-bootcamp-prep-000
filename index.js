dabf95d1b3dcebdb363d5244bddaf7d279d75575
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-13 22:44:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var a, b, c, n
+function add(a,b) {
+  c = a + b
+  return c
+}
+
+function subtract(a,b) {
+  c = a - b
+  return c
+}
+
+function multiply(a,b) {
+  c = a*b
+  return c
+}
+
+function divide(a,b) {
+  c = a/b
+  return c
+}
+
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
