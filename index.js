2fcb9257f579e92dd805131f6390c3cf0650f6be
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-24 03:56:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+const add = (a,b) =>{
+  return a+b
+}
+
+const subtract = (a,b) =>{
+  return a-b
+}
+
+const multiply = (a,b) =>{
+  return a*b
+}
+
+const divide = (a,b) =>{
+  return a/b
+}
+
+const inc = n =>{
+  return parseInt(n,10)+1
+}
+
+const dec = n =>{
+  return parseInt(n,10)-1
+}
+
+const makeInt =n =>{
+  return parseInt(n,10)
+}
+
+const preserveDecimal = n =>{
+  return parseFloat(n)
+}
+
