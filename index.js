982fabc7426149b9243eee368e8882d83691a310
Luis Cintron <l.cintron9966@outlook.com>
2019-09-03 01:33:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var a = 8;
+var b = 10;
+
+function add(a, b) {
+  return (a + b);
+}
+
+function subtract(a, b) {
+  return (a - b);
+}
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+function inc(a, b) {
+  return (a + 1);
+}
+
+function divide(a, b) {
+  return (a / b);
+}
+
+function dec(a, b) {
+  return (a - 1);
+}
+
+function makeInt(a, b) {
+  return parseInt('a', b);
+}
+
+function makeInt(a, b) {
+  return parseInt(a, 10);
+}
+
+function perserveDecimal() {
+  return ('2.222', a);
+}
+
+
+
