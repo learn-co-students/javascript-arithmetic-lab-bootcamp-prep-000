aa2083f20eeeb2ccb134c8010c79a747d160f558
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-03 23:06:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,65 @@
+
+function mathFloorRandom() {
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)
+return a, b;
+}
+
+function add(a, b){
+return a+b;
+}
+add(a, b);
+
+mathFloorRandom();
+// let a = Math.floor(Math.random() * 1000)
+// let b = Math.floor(Math.random() * 1000)
+
+function subtract(a, b){
+return a-b;
+}
+subtract(a, b);
+
+mathFloorRandom();
+// let a = Math.floor(Math.random() * 1000)
+// let b = Math.floor(Math.random() * 1000)
+function multiply(a, b){
+return a*b;
+}
+multiply(a, b);
+
+mathFloorRandom();
+// let a = Math.floor(Math.random() * 1000);
+// let b = Math.floor(Math.random() * 1000);
+function divide(a, b){
+return a/b;
+}
+divide(a, b);
+
+mathFloorRandom();
+
+function inc(a){
+var i = parseInt();
+return a+1;
+}
+inc(i);
+
+mathFloorRandom();
+
+function dec(a){
+  var j = parseInt();
+return a-1;
+}
+dec(j);
+
+mathFloorRandom();
+// let k = Math.floor(Math.random() * 1000);
+function makeInt(k){
+return parseInt(k, 10);
+}
+makeInt(k);
+
+let l = Math.floor(Math.random() * 1000);
+function preserveDecimal(l){
+return parseFloat(l, 10);
+}
+preserveDecimal(l);
