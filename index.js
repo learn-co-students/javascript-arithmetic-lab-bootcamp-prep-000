98cacc360e5817270cc2240588b4b85741ef461c
jeremy perry <jeremy.m.perry@gmail.com>
2019-09-24 18:10:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  var num = a + b;
+  return num;
+}
+
+function subtract(a,b){
+  var newNum = a - b;
+  return newNum;
+}
+
+function multiply(a,b){
+  var multi = a * b;
+  return multi;
+}
+
+function divide(a,b){
+  var div = a / b;
+  return div;
+}
+
+function inc(n){
+ n++;
+ return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+return parseInt(n, 10);
+
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+  
+}
