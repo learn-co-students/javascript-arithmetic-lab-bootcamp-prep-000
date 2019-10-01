f149d47cb31f355527a9ea3908213397e7952bda
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 17:34:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+
+function add(a ,b) {
+  return a + b ;
+}
+
+function subtract(a,b) {
+   return a - b ;
+}
+
+function multiply(a,b) {
+  return a * b ;
+}
+
+function divide(a,b) {
+  return a / b ;
+  
+}
+
+function inc(a) {
+  var number = a ; 
+  number++ ; 
+  return number ; 
+  
+}
+
+function dec(b) {
+  var number = b ; 
+  number-- ; 
+  return number ; 
+  
+}
+
+function makeInt(n) {
+  return parseInt(n, 10) ;
+  
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n) ; 
+  
+}
