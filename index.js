c549de8ff22d5fe8d815b77213adc72fc14acff1
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 10:16:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+return a+b;
+}
+add(4,3);
+function subtract(a, b){
+  return a-b;
+}
+subtract(7,3);
+
+function multiply(a,b){
+return a*b;
+}
+multiply(4,3);
+function divide(a,b){
+return a/b;
+}
+divide(4,3);
+function inc(n){
+n++;
+return n;
+}
+inc(85);
+function dec(n){
+n--;
+return n;
+}
+inc(85);
+
+function makeInt(n){
+var parsed = parseInt(n ,10)
+return parsed;
+}
+makeInt(5)
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+preserveDecimal(4.5);
