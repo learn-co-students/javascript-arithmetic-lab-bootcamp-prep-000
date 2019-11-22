dfb627b58666aa42d382b377f4d5f88dab63f62c
Skyler H Gold <skylergold96@gmail.com>
2019-11-22 23:20:07 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ function multiply (a,b) {
 function divide (a,b) {
   return a/b
 }
+//having issues with why a++ isn't working for this
 function inc(a) {
   return a + 1
 }
 function dec(a) {
   return a - 1
 }
+//what is makeInt(n) mean?!?
