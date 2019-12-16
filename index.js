6b8a4698ff00fc2e056642975f157bed0bfb8e25
Iuliia Saprykina <iuliia.proskurnina@gmail.com>
2019-12-16 03:12:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+var a, b 
+ function add(a, b) {
+  return (a+b) 
+ }
+ 
+ var a, b 
+ function subtract(a, b) {
+  return (a-b) 
+ }
+ 
+ var a, b 
+ function multiply(a, b) {
+  return (a*b) 
+ }
+ 
+ var a, b 
+ function divide(a, b) {
+  return (a/b) 
+ }
+ 
+var n 
+ function inc(n) {
+  return (n+=1) 
+ }
+ 
+ var n 
+ function dec(n) {
+  return (n-=1) 
+ }
+ 
+ var n 
+ function makeInt(n) {
+  return parseInt(n)
+ }
+ 
+function makeInt(n) {
+  return parseInt(n,10)
+ }
+ 
+ function preserveDecimal(n) {
+   return parseFloat (n)
+ }
+ 
+ 
+ 
