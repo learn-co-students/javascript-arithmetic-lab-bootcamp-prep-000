3ed773f9397c436f0c1c85124bca719cf22ae7e3
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:56:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+a = 8
+b = 4
+n = 1
+z = 2.222
+ 
+function add(a, b){
+  return (a + b)
+}
+
+function subtract(a, b){
+  return (a - b)
+}
+
+function multiply(a, b){
+  return (a * b)
+}
+
+function divide(a, b){
+  return (a / b)
+}
+
+function inc(a){
+ return a += 1
+}
+
+function dec(a){
+  return a -= 1
+}
+
+function makeInt(n){
+
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(z){
+ 
+  return parseFloat(z, 'sldkjflksjf')
+  
+}
