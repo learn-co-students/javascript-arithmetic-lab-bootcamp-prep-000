297be0eec2318bd37bed0f6e249fd8cc538dea7c
Brittany Russell <sincerelybr@gmail.com>
2020-01-18 20:16:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+function add (a,b){
+  var addition = a+b;
+  return addition
+}
+console.log(add);
+
+function subtract (a,b){
+  var subtract = a-b;
+  return subtract
+}
+console.log(subtract);
+
+function multiply (a,b){
+  var multiply = a*b;
+  return multiply
+}
+console.log(multiply);
+
+divide
+
+function divide (a,b){
+  var divide = a/b;
+  return divide
+}
+console.log(divide);
+
+function inc(n){
+  var inc = ++n;
+  return inc
+}
+console.log (inc(3));
+
+function dec(n){
+  var dec = --n;
+  return dec
+}
+console.log (dec(3));
+
+function makeInt(n){
+  var num = parseInt(n,10);
+  return num
+}
+
+function preserveDecimal(n){
+  var num = parseFloat(n);
+  return num
+}
+
+
+
+
+
+
+
+
