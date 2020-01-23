fe3afbb6b9b7709ff4d648695f58b049a0720622
Andres Mercado <andresmercado69@gmail.com>
2020-01-23 03:15:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+let x = 10;
+let y = 20;
+
+let z = x + y;
+
+console.log(z);
+
+let b = 20;
+let c = 5;
+
+// Multiply x by y to get the product
+let a = b * c;
+
+console.log(a);
+
+function add(a, b){
+ return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+
+
