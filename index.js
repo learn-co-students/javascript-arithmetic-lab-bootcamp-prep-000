709506f17b9f04c7712b67dbd25a37bbc9cb9c26
Dave Patel <ddp2134@gmail.com>
2020-01-29 03:43:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var a = '10';
+var b = '2';
+
+function add (a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+
+
+var n = 0;
+function increment(){
+  n++;
+  return n;
+}    
