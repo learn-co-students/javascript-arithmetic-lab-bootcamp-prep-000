b6d7032b3e6c13453734d28ffea3819c6269f4f9
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-03 02:12:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+//add(a,b)
+function add(a, b){
+  return a+b
+}
+
+//subtract(a,b)
+function subtract(a, b){
+  return a-b
+}
+
+//multiply(a, b)
+function multiply(a, b){
+  return a*b
+}
+
+//divide(a, b)
+function divide(a, b){
+  return a/b
+}
+
+//inc(n)
+function inc(n){
+  return ++n
+}
+
+//dec(n)
+function dec(n){
+  return --n
+}
+
+//makeInt(n)
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+//preserveDecimal(n)
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
