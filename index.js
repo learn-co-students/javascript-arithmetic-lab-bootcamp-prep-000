f982a99d781c8ac800fb53f4cd704323717e5740
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-05 17:54:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,63 @@
+
+/*var number = 10
+
+function add5() {
+  number += add5
+}
+
+function divideBy3(){
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
+*/
+
+function increment(n){
+  n++
+  return n
+}
+
+function decrement(n){
+  n--
+  return n
+}
+
+function makeInt(string){
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+  return parseFloat(string)
+}
+
+function add(x,y){
+  return x+y
+}
+
+function subtract(x,y){
+  return x-y
+}
+
+function multiply(x,y){
+  return x*y
+}
+
+function divide(x,y){
+  return x/y
+}
