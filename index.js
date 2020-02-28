e542d795f33f2c5d39b38a1c127d3957d5e005d1
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 19:37:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function add(a, b) {
   return a + b;
 }
+
 function subtract(a, b) {
   return a - b;
 }
@@ -16,7 +17,6 @@ function divide(a, b) {
 function inc(n) {
   return ++n;
 }
-
 function dec(n) {
   return --n;
 }
