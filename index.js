1dbf9a0e8af99c3fd3e770cae80dad11e221f541
Shamira Naznin <shaminzn@gmail.com>
2020-03-07 19:40:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+//Addition
+function add(a, b) {
+  return a + b;
+}
+
+add(1, 80);
+//Subtraction
+function subtract(a, b) {
+  return a - b;
+}
+
+subtract(60, 40);
+//Multiplication
+function multiply(a, b) {
+  return a * b;
+}
+
+multiply(2, 3.4);
+//Division
+function divide(a, b) {
+  return a / b;
+}
+
+divide(5.0, 2.5);
+//Increment
+
+function increment(n) {
+
+  return n += 1;
+}
+
+//Decrement
+function decrement(n) {
+  return n -= 1;
+}
+//ParseInt
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+//ParseFloat
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
