43a94fe81548978f7c2590ba2604842caa9ffade
Netaly Ramirez <21netaly@gmail.com>
2020-03-15 04:28:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function makeInt(string){
+  return parseInt(string,10)
+}
+
+function preserveDecimal(string){
+  return parseFloat(string)
+}
+
+function add(){
+
+}
+function subtract(){}
+function multiple(){}
+function divide(){}
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n){
+  return n+=1
+}
+function decrement(n){
+  return n-=1
+}
