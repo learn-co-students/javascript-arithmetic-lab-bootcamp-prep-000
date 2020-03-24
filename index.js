b10b37bc0ff86fad3ca43249880ffa9aeb694f03
danpaslawsky <danpaslawsky@gmail.com>
2020-03-24 00:30:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(n){
+  return (n,10);
+}
+
+function subtract(a,b){
+  return (a-b);
+}
+
+function add(a,b){
+  return (a+b);
+}
+
+function multiply(a,b){
+  return (a*b);
+}
+
+function divide(a,b){
+  return (a/b);
+}
+
+function increment(n){
+  return n+=1;
+}
+
+function decrement(n){
+  return --n;
+}
+
+function makeInt(n){
+  return parseFloat(n);
+}
+
+function preserveDecimal(n){
+  return parseInt(n);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+  
