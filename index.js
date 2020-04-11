239d8358c353e49e0eb5bdd3ecb38e65e04cd518
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-11 02:59:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+  return a + b;
+}
+function subtract(a, b){
+  return a - b;
+}
+function multiply(a, b){
+  return a * b;
+}
+function divide(a, b){
+  return a / b;
+}
+function increment(n){
+  n++;
+  return n;
+}
+function decrement(n){
+  n--;
+  return n;
+}
+function makeInt(string){
+  var x = parseInt(string, 10);
+  return x;
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
