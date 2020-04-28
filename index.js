0b943e58966709be57e70ff22bfc80a83a1a89be
James Mannarino <jamesmannarino@gmail.com>
2020-04-28 21:22:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add() {
+  return 1+80
+}
+
+var number = 10;
+
+function add(a, b) {
+ return a + b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
+function increment (n) {
+  return n += 1;
+}
+
+function decrement (n) {
+  return n -= 1;
+}
+
+function makeInt (n) {
+  return parseInt (n, 10);
+}
+
+function preserveDecimal (n) {
+  return parseFloat (n)
+}
