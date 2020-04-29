c2ff7a9bada02624781a417721dc128ef900f8bf
Paolo Ventura <ventura.paolo@gmail.com>
2020-04-29 11:26:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(x, y) {
+  return x+y
+}
+
+function subtract(x, y) {
+  return x-y
+}
+
+function multiply(x, y) {
+  return x*y
+}
+
+function divide(x, y) {
+  return x/y
+}
+
+var number =10.22
+function increment(number) {
+  return number+=1
+}
+
+function decrement(number) {
+  return number-=1
+}
+
+function makeInt(x, base){
+  parseInt('2',10)
+}
