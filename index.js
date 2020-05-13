40d8219e0697df552014709d9d8a3c0f67c17ccd
Josh Luerssen <joshluer@gmail.com>
2020-05-13 15:37:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+function add() {
+  return 1 + 80;
+}
+
+function subtract() {
+  return 60 - 40;
+}
+
+function multiply() {
+  return 2 * 3.4;
+}
+
+function divide() {
+  return 5.0 / 2.5;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+
+var n = '2';
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+var n = '2.2222';
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
