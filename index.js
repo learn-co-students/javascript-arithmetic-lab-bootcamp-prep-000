acd343ad131a48f00054add0fe26bc602427e0e6
Paolo Ventura <ventura.paolo@gmail.com>
2020-05-13 09:00:21 +0000

    Done.

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function decrement(number) {
   return number-=1
 }
 
-function makeInt(x, base){
-  parseInt('2',10)
+function makeInt(string) {
+  return parseInt(string,10)
+}
+
+function preserveDecimal(string){
+  return parseFloat(string)
 }
