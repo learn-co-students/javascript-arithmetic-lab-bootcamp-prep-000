0313ca9708454fa56fda0170517b6031aea71961
Miten Kashiyani <miten.kashiyani96@gmail.com>
2020-05-24 13:19:27 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ function add(a,b) {
   return a+b
 }
 
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+  return --n;
+}
+
 function makeInt(string) {
-  parseInt('string', 10)
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
 }
