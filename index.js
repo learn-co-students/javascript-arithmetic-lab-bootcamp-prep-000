15a008ae17c6e8767176fadaf09d9068700802f8
Nathan Chung <chung.nathanh@gmail.com>
2020-05-26 23:17:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+add(3, 5)
+subtract(3, 5)
+multiply(10,50)
+
+function increment(a) {
+  return (++a)
+}
+
+function decrement(a) {
+  return (--a)
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
