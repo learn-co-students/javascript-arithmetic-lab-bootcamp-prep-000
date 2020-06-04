2fd50dc482886494fd077827166f4826c2f4dbdc
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-06-04 21:40:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,57 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+
+var number;
+function add(a, b){ 
+  number = a + b;
+  return number;}
+  
+  function subtract(a, b){
+    number= a-b;
+    return number;
+  }
+  
+  function multiply(a, b){
+    number=a*b;
+    return number;
+  }
+  
+  function divide(a,b){
+  number=a/b;
+  return number;}
+  
+  function increment(a){
+  number = a + 1;
+  return number;
+}
+ 
+ function decrement(a){
+   number = a - 1;
+   return number;
+ }
+ function makeInt(a){
+   number = parseInt(a, 10);
+  return number;
+}
+ function preserveDecimal(a){
+   number = parseFloat (a, 10);
+   return number;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
