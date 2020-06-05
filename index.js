d3ec80e6781e3230a4ec6c77c79231ba4806df8f
Cedric Patton <ceddyp117@gmail.com>
2020-06-05 04:59:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+  function add() {return (1 +80)}
+
+subtract(60-40);
+
+multiply(2* 3.4);
+
+divide(5.0 / 2.5);
+
+function add(a,b) { return (a+b)}
+
+function subtract(a, b) { return (a-b)}
+
+function multiply(a, b) { return(a*b)}
+
+function divide(a, b) { return(a/b)}
+
+function increment(a){return (a + 1)}
+
+function decrement(a){return (a - 1)}
+
+function makeInt(n){
+   return n }
+
+
+function preserveDecimal(x){ 
+   x = 0;
+   x = x + 2.222 ;
+   return x}
+    
+
+
+ 
+ 
