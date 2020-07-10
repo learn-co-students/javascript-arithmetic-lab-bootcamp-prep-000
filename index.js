3297ca708bb79f9904cac2ad1daa107e6627691e
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 17:24:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {}
+function divide() {}
+function add(a, b) {
+  return a + b
+}
+function subtract (a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function increment(number) {
+  return number + 1
+}
+function decrement(number) {
+  return number - 1
+}
+function makeInt(number) {
+  return parseInt(number, 10)
+}
+function preserveDecimal(number) {
+  return parseFloat(number)
+}
