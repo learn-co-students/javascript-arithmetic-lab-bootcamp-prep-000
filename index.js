0f1be2568d76e82936fadd7bfa6e9fa51fc786ad
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-13 22:42:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function add(x,y) {
+  return x + y; 
+}
+
+function subtract(x,y) {
+  return x - y;
+}
+
+function multiply(x,y) {
+  return x * y;
+}
+
+function divide(x,y) {
+  return x / y;
+}
+
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+  return --n;
+}
+
+function makeInt(n) {
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+/* 
+function makeInt(string) {
+  return string.parseInt(string,10);
+}
+
+function preserveDecimal(string) {
+  return string.parseFloat(string);
+} 
+*/
