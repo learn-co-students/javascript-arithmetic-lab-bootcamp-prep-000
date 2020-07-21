24f2e14a6b831e6b3cc8727894a52c89c21d994e
Evan Pavone <evanrpavone@gmail.com>
2020-07-21 14:34:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var number
+function add(a, b){
+  number = a + b;
+  return number
+}
+function subtract(a,b) {
+  number = a - b;
+  return number
+}
+function multiply(a,b) {
+  number = a * b;
+  return number
+}
+function divide(a,b) {
+  number = a / b;
+  return number
+}
+function increment(a) {
+  number = a + 1;
+  return number
+}
+function decrement(a){
+  number = a - 1;
+  return number
+}
+function makeInt(a){
+  number = parseInt(a,10);
+  return number
+}
+function preserveDecimal(a){
+  number = parseFloat(a, 10);
+  return number
+}
