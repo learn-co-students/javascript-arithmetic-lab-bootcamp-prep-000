0757006a3075fb3fd073922ec94381b74cd69589
sina ao <electronicnewss@gmail.com>
2020-07-24 23:04:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function makeInt(string){
+  return parseInt(string,10)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
+}
+
+function add(a,b){
+  return a+b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(n){
+  return ++n;
+}
+function decrement(n){
+  return --n
+  ;
+}
