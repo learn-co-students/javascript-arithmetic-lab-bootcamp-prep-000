68aab606f79024549fad818932dc7e4d0b6fbf98
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-08-12 19:19:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function increment(n) {
+  return n += 1
+}
+
+function decrement(n) {
+  return n -= 1
+}
+//having trouble here. ill need to double check on my work and syntax
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
