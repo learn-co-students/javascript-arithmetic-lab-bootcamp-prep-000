b82e0623577d25980dcde18ef253053af1c49633
Brian Franklin <brianfranklinmusic@gmail.com>
2020-08-13 02:03:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+  return a+b
+}
+function subtract(a,b) {
+  return a-b
+}
+function multiply(a,b) {
+  return a*b
+}
+function divide(a,b) {
+  return a/b
+}
+function increments(n) {
+  n += 1
+  return n
+}
+function decrements(n) {
+  n -= 1
+  return n
+}
