1ff22e7e59c33ffd7065932019d75f709d0e6171
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 21:24:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+add(a, b) { return a + b }
+subtract(a, b) { return a - b }
+multiply(a, b) { return a * b }
+divide(a, b) { return a / b }
+
+function increment(n) { return n += 1 }
+function decrement(n) { return n -= 1 }
+
+function makeInt(n,10) { return parseInt(n) }
+function preserveDecimal(n) { return parseFloat(n) }
