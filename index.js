cee97bf0042d790784b9b5ac766a3ce6828143ea
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-02 00:28:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function increment(n) {
+  return n += 1
+}
+
+function decrement(n) {
+  return n -= 1
+}
