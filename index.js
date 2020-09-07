58754ff0c74406f46405fec87bd7ac72bccc5a9c
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-07 14:32:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function increment(a) {
+  a += 1
+  return a
+}
+
+function decrement(a) {
+  a -= 1
+  return a
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a, 10)
+}
+
