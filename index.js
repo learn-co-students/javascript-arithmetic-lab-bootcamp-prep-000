5ad5b237b0db75b9fcae2e0414b91082ae0cd6b2
Chemar Wright <chemarrian@gmail.com>
2020-09-11 19:37:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function add() {}
+function subtract(){}
+function multiply() {}
+funtion divide() {}
+var a = 2
+var b = 1
+function add(a,b) {return a + b };
+function subtract(a,b) {return a - b };
+function multiply (a,b) { return a * b };
+
+function divide (a,b) { return a/b }
+var number = 10
+function add5() {number +=5} console.log (number)
+function subtract2() {number -=2} console.log (number)
