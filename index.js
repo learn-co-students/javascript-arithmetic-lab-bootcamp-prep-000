4789c92c182fe557bce5cae7ab44c2479e407245
Damian Malysza <damian.malysza@gmail.com>
2020-09-20 03:09:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x,y){
+  return x+y
+}
+
+function subtract(x,y){
+  return x-y
+}
+
+function multiply(x,y){
+  return x*y
+}
+
+function divide(x,y){
+  return x/y
+}
+
+function increment(n){
+  return n+=1
+}
+
+function decrement(n){
+  return n-=1
+}
+
+function makeInt(s){
+  return parseInt(s,10)
+}
+
+function preserveDecimal(s){
+  return parseFloat(s)
+}
