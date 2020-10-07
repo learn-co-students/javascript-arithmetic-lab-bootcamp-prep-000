afcaa6ab8c4c6f23c49703bb48d56564313d80f4
energetic-gateway-3088 <geobannyborja@gmail.com>
2020-10-07 07:19:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+var a= 10
+var b=2
+var n=4
+
+function add(a, b){
+return (a+b)
+}
+
+function subtract(a, b){
+return (a-b)
+}
+
+function multiply(a, b){
+return (a*b)
+}
+
+function divide(a, b){
+return (a/b)
+}
+
+function increment(n){
+  n++
+  return (n)
+}
+
+function decrement(n){
+  n--
+  return (n)
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
