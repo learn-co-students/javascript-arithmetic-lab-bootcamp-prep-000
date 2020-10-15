cc513b78baeffe25e31f7a3571bb3f55bbed378e
Jason DiLoreto <jasondiloreto@yahoo.com>
2020-10-15 02:32:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+
+}
+
+function divide() {
+  
+}
+ 
+function add(a, b) {
+ return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b; 
+}
+
+var n = 0;
+
+function increment(n) {
+  n++;
+  return n;
+}
+
+function decrement(n) {
+  n--;
+  return n;
+}
+
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+return parseFloat(n);
+}
