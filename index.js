96e544330fa94d010e260f24df1b311f3d5b27e7
Bill Miles <williammiles201@gmail.com>
2020-10-16 19:22:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function increment() {
+}
+function decrement() {
+}
+function makeInt() {
+}
+function parseInt() {
+}
+function parseFloat() {
+}
+function isNaN() {
+}
+function preserveDecimal() {
+}
+function add(a,b) {
+  return a += b
+}
+function subtract(a,b) {
+  return a -= b
+}
+function multiply(a,b) {
+  return a *= b
+}
+function divide(a,b) {
+  return a /= b
+}
+function increment(a){
+  return a + 1
+}
+function decrement(a) {
+  return a - 1
+}
+function makeInt(a){
+  return parseInt(a, 10)
+}
+function preserveDecimal(a){
+  return parseFloat(a)
+}
