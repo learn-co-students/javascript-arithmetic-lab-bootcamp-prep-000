68c5ca77cbb0094a8307b78176dd722fa54f5484
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-17 18:28:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+  add (1 + 8)
+}
+function subtract() {
+  subtract (60 - 40)
+}
+function multiply () {
+  multiply (2 * 3.4)
+}
+function divide () {
+  divide (5.0 / 2.5)
+}
+var a = 2
+var b = 5
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function increment(n) {
+  return n += 1
+}
+function decrement(n) {
+  return n -= 1
+}
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
