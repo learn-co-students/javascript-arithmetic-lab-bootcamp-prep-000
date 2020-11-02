63680dbb7edd896c9a2d054b0214f1c9665385ba
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-02 12:38:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var number = 5
+
+function add(number) {
+  return number++
+}
+function subtract() {
+  return number -= 3
+}
+function multiply() {
+  return number *= 2
+}
+function divide() {
+  return number /= 2
+}
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+function increment(n) {
+  return n+= 1
+}
+function decrement(n) {
+  return n-= 1
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
