6ab39730855a655c089fae0527bf29018e42616d
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 19:00:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var a
+var b
+var n
+
+function add(a,b) {
+return a+=b
+}
+
+function subtract(a,b) {
+return a-=b
+}
+
+function multiply(a,b) {
+return a*=b
+}
+
+function divide(a,b) {
+return a/+b
+}
+
+function increment(a){
+  return ++a
+}
+
+function decrement(a){
+return  --a
+}
+
+function makeInt(a){
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a)
+  return parseFloat(sldkjflksjf, 10)
+}
