7830313cd3638fc157a1b7787626fccbfa732a5e
k k <kkebaara@yahoo.com>
2021-01-23 09:00:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function add(a, b){
+  var result = a + b
+  return result
+}
+
+function subtract(a, b){
+  var result = a - b
+  return result
+}
+
+function multiply(a, b){
+  var result = a * b
+  return result
+}
+
+function divide(a, b){
+  var result = a / b
+  return result
+}
+
+function increment(n){
+  var result =  n+1
+  return result
+}
+
+function decrement(n){
+  var result = n-1
+  return result
+ }
+
+function makeInt(n){
+  return parseInt(n, 10)
+
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
