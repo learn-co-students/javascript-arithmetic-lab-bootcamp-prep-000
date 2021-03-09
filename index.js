5d341cd154acdb75fa725cf3e7dbed29ea4a9062
decisive-metric-0348 <gmd298@gmail.com>
2021-03-09 19:57:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+var a = 1
+var b = 2
+
+
+function add() {
+
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function increment(a) {
+  return (++a)
+}
+
+function decrement(a) {
+  return (--a)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+
+
