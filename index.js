eb034b6014d9889af589b5052078c3aee49d7fcc
celestial-applet-1261 <zihan991@gmail.com>
2021-03-10 15:16:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var a,b;
+
+function add(a,b) {
+  return a + b;
+}
+function subtract(a,b) {
+  return a - b;
+}
+function multiply(a,b) {
+  return a * b;
+}
+function divide(a,b) {
+  return a / b;
+}
+function increment(n) {
+return  ++n;
+  }
+function decrement(n) {
+return --n;
+}
+function makeInt(n) {
+Number = parseInt(n , 10);  // NaN
+return Number;
+  
+}
+function preserveDecimal(n) {
+ Number = parseFloat(n , 10);  //NaN
+  return Number;
+}
